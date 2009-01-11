40b8bd5de4024f5f40e5bbae5027c641da06781b
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2009-01-11 17:38:33 +0000

    эпсилон замыкание. Несобирается
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@9 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- Source/Pasrser/FinitAutomata.fs
+++ Source/Pasrser/FinitAutomata.fs
@@ -4,13 +4,14 @@ module FinitAutomaton
 
 open IL.Production
 open IL.Source
-
-let state = 
-    let i =ref 0 in
-    let next () = i:=!i+1;!i in
-    next
+open Set
   
-let rec create_NFA regexpr =       
+let rec create_NFA regexpr =
+    let state = 
+        let i =ref 0 in
+        let next () = i:=!i+1;!i in
+        next
+    in   
     match regexpr
     with
     | PSeq (seq,attr) -> ( let new_autom = List.map (fun t -> create_NFA t.rule) seq
@@ -21,19 +22,37 @@ let rec create_NFA regexpr =
                           ) 
                               
     | PAlt (l,r)      -> (match (create_NFA l,create_NFA r)
-                          with
-                              |((lrules,ls,lf),(rrules,rs,rf))-> (let s,f = state(),state() in                                                                
-                                                                List.concat [[(s,None,ls);
+                          with ((lrules,ls,lf),(rrules,rs,rf))-> (let s,f = state(),state() in                                                                
+                                                                  List.concat [[(s,None,ls);
                                                                               (s,None,rs);
                                                                               (lf,None,f);
                                                                               (rf,None,f)];lrules;rrules],s,f)
                             ) 
     | PMany (expr)    ->  (match (create_NFA expr)
-                             with
-                             |(rules,s,f) ->(let ns,nf = state(),state() in                                             
-                                             (List.concat [[ns,None,nf];[nf,None,ns];[ns,None,s];[f,None,nf];rules],ns,nf)
-                                             )
-                                             //(List.concat [[s,'`',f];[f,'`',s];rules],s,f)
-                             )
+                           with (rules,s,f) ->(List.concat [[f,None,s];[s,None,f];rules],s,f)
+                          )
     | PToken(ch)
-    | PLiteral(ch) as t -> (let s,f = state(),state() in ([s,Some(toString ch),f],s,f))
+    | PLiteral(ch) as t -> (let s,f = state(),state() in ([s,Some(t),f],s,f))
+    
+let states rules = List.fold_left (fun set (a,b,c) -> Set.union set (of_list[a;c])) empty rules 
+     
+let exists_e_elt:(int list ref) = ref []
+
+let e_closure rules =    
+    let rec closure stt = 
+        if List.exists ((=)stt) (!exists_e_elt)
+        then (!exists_e_elt)
+        else (exists_e_elt:=stt::!exists_e_elt;
+              let lst = (List.filter (fun(a,b,c)-> a = stt && b = None) rules)
+              in
+              if lst = [] then !exists_e_elt else
+              List.concat (List.map (fun (a,b,c)-> closure c)
+              lst))
+     in   
+     Set.fold_left (fun lst x -> let flg = ref false 
+                                 in 
+                                 let new_lst= List.map (fun y -> if empty = intersect x y then union x y else (flg:=true ;y)) lst
+                                 in
+                                 if !flg then x::new_lst else new_lst) [] (Set.map (fun x -> of_list(closure x)) (states rules))
+
+                                 

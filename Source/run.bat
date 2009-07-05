4d3ca1e594efa0629dd5ae83d9306541a1a44106
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2009-07-05 20:09:23 +0000

    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@64 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- Source/run.bat
+++ Source/run.bat
@@ -1,10 +1,63 @@
 @echo off
 echo get new Generator.dll
-xcopy /y /s Generator\bin\Release\Generator.dll Runner\bin\Release\Generator.dll
+
+if .%1 == .r (
+   xcopy /y /s Generator\bin\Release\Generator.dll Runner\bin\Release\Generator.dll 
+) else       (
+   if .%1 == .d (
+      xcopy /y /s Generator\bin\Debug\Generator.dll Runner\bin\Debug\Generator.dll
+   ) else       (
+      echo Incorrect first argument.
+   )
+)
+
 echo Generating...
-Runner\bin\Release\Runner.exe
+
+if .%1 == .r (
+   Runner\bin\Release\Runner.exe
+) else       (
+   if .%1 == .d (
+      Runner\bin\Debug\Runner.exe
+   ) else       ( 
+      echo Incorrect first argument.
+   )
+)
+
+pause
+
 echo Copy data files...
-xcopy /y  Runner\bin\Release\goto.dta  Pasrser\bin\Release\
-xcopy /y  Runner\bin\Release\items.dta  Pasrser\bin\Release\
+
+if .%1 == .r (
+   xcopy /y  goto.dta  Pasrser\bin\Release\
+   xcopy /y  items.dta  Pasrser\bin\Release\
+) else       (
+   if .%1 == .d (
+      xcopy /y  goto.dta  Pasrser\bin\Debug\
+      xcopy /y  items.dta  Pasrser\bin\Debug\
+   ) else       ( 
+      echo Incorrect first argument.
+   )
+)
+
 echo Parsing...
-Pasrser\bin\Release\pasrser.exe
+
+if .%1 == .r (
+   if .%2 == .o (
+      Pasrser\bin\Release\pasrser.exe >> parser_result_r.txt
+   ) else      (
+      Pasrser\bin\Release\pasrser.exe
+   )
+) else       (
+   if .%1 == .d (
+      if .%2 == .o (
+         Pasrser\bin\Debug\pasrser.exe >> parser_result_d.txt
+      ) else      (
+         Pasrser\bin\Release\pasrser.exe
+      )
+
+   ) else       (
+      echo Incorrect first argument.
+   )
+)
+
+echo Parsing is successfuly finished.

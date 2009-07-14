7c98c4d58732baad55f8793f3f2233acb379b9b8
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2009-07-14 08:32:43 +0000

    Solution refactoring.
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@67 b8160d1e-d411-11dd-a2b5-e5f827957e07

---
+++ Source/Runtime/Grammar.fs
@@ -0,0 +1,20 @@
+﻿#light "off"
+
+module Symbol = //Терминал или нетерминал 
+  struct
+   type t<'a> = 
+    |Terminal    of 'a
+    |Nonterminal of 'a 
+  end  
+  
+module Item = 
+  struct   
+   type t<'a> = {prod_num:int;
+                 prod_name:string;
+                 item_num:int;
+                 symb:Symbol.t<'a> option;
+                 next_num:int option;
+                 s:int;
+                 f:int Set}
+  end  
+
---
+++ Source/Runtime/IL.fs
@@ -0,0 +1,64 @@
+﻿#light "off"
+
+module Source = 
+  struct
+   type t = string * (int * int) 
+   
+   let toString ((r,_):t):string = r
+
+  end
+
+module Production = 
+  struct
+    type elem <'patt,'expr> = 
+    {
+     omit:bool; //Вычитание
+     rule:(t<'patt,'expr>);//Правило
+     binding:'patt option; //замыкание :) f:F ну или f:=F...
+     checker:'expr option //"почти" резольвер
+    }   
+    and
+    t <'patt,'expr> =    
+    |PAlt     of (t <'patt,'expr>) * (t<'patt,'expr>)//Альтернатива
+    |PSeq     of (elem <'patt,'expr>) list * 'expr option //Последовательность * атрибут.(атрибут всегда применяется к последовательности) 
+    |PToken   of Source.t //собственно токен
+    |PRef     of Source.t * 'expr option // Vanilla rule reference with an optional args list.
+    |PMany    of (t <'patt,'expr>) //expr*
+    |PMetaRef of Source.t * 'expr option * 'expr list // Metarule reference like in "a: mr<x> y z"
+    |PLiteral of Source.t //Литерал. Хочется в правилах явно писать ,например, .."if" expr "then" expr...
+    |PRepet   of (t <'patt,'expr>) * int option * int option  //extended regexp repetition, "man egrep" for details
+    |PPerm    of (t <'patt,'expr>) list //permutation (A || B || C)
+    
+/// The following are obsolete and reduction to PRepet should be discussed.
+    |PSome    of (t <'patt,'expr>) //expr+
+    |POpt     of (t <'patt,'expr>) //expr?
+
+  end
+
+module Rule = 
+  struct
+    type t <'patt,'expr> = 
+    { 
+     name    : string;
+     args    : 'patt list;
+     body    : (Production.t <'patt,'expr>);
+     _public : bool; //Стартовый нетерминал (иногда их хочется иметь много...)
+     metaArgs: 'patt list
+    }
+  end
+
+
+module Grammar = 
+  struct
+    type t <'patt,'expr> = (Rule.t<'patt,'expr>) list //грамматика - список правил.
+  end 
+
+module Definition = 
+  struct
+    type ('patt,'expr) t = 
+    { 
+     head    :'expr option; //текст до грамматики, который потом просто копируеться(всякие open-ы)
+     grammar : Grammar.t<'patt,'expr>;//грамматика
+     foot    :'expr option //текст после грамматики
+    }
+end

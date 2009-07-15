67669408f72a8da3a75601362ac9e3c08bdbd15a
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2009-07-15 14:22:59 +0000

    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@70 b8160d1e-d411-11dd-a2b5-e5f827957e07

---
+++ Source/FE/Main.fs
@@ -0,0 +1,21 @@
+﻿// Main.fs
+//
+// Copyright 2009 Semen Grigorev
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation.
+
+
+#light
+
+let run path = 
+    use reader = IO.binary_reader path
+    let p = reader.PeekChar()
+    Parser.file Lexer.main (Microsoft.FSharp.Text.Lexing.LexBuffer.FromBinaryReader reader)
+    
+let main = 
+ run "test001.yrd";//Sys.argv.[1];
+ print_any "end"
+
+

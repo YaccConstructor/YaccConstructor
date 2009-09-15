41516babb6f434267e4243f87700f3c42765d3e4
aleks.astaschenko@gmail.com <aleks.astaschenko@gmail.com@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2009-09-15 12:24:25 +0000

    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@83 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- Source/FE/Main.fs
+++ Source/FE/Main.fs
@@ -6,16 +6,31 @@
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation.
 
-
 #light
+open Microsoft.FSharp.Text.Lexing
 
-let run path = 
-    use reader = IO.binary_reader path
-    let p = reader.PeekChar()
-    Parser.file Lexer.main (Microsoft.FSharp.Text.Lexing.LexBuffer.FromBinaryReader reader)
+let run_common path = 
+    Lexer.currentFileContent := System.IO.File.ReadAllText(path);
+    let reader = IO.binary_reader path in
+    LexBuffer<_>.FromBinaryReader reader
+
+let run path =
+    let buf = run_common path in
+    let example = seq {
+                       while not buf.IsPastEndOfStream do
+                             yield Lexer.main buf  
+                      } in
+    Seq.iter (printfn "%A ") example
+
+
+let run2 path = 
+    let buf = run_common path
+    Parser.file Lexer.main buf
+      
     
-let main = 
- run "test001.yrd";//Sys.argv.[1];
- print_any "end"
+let main =
+ let d = run2 "test001.yrd";
+ d;
+ ()
 
 

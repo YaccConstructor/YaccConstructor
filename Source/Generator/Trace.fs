88d4f96b1954fd7ade9580c4f284e6c746ee914c
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-02-07 19:11:00 +0000

    Trace.fs contains types for labels in FA
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@139 b8160d1e-d411-11dd-a2b5-e5f827957e07

---
+++ Source/Generator/Trace.fs
@@ -0,0 +1,16 @@
+﻿// Trace.fs
+//
+// Copyright 2010 Semen Grigorev
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation.
+
+namespace Yard.Core
+
+type Position = First|Second
+
+type Trace = 
+| Seq
+| Alt of Position
+| Closure

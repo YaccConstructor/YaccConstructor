61c1855b063549b0b2935cf4d0cbf00ce488df4c
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-02-08 17:47:06 +0000

    Graphs for report are added.
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@140 b8160d1e-d411-11dd-a2b5-e5f827957e07

---
+++ Docs/Отчёт/Diploma/sources_new/DOT/make.bat
@@ -0,0 +1,11 @@
+dot sample_for_label_diriv_tree.dot -Tpdf -o sample_for_label_diriv_tree.pdf
+
+dot sample_for_label_syntax_tree.dot -Tpdf -o sample_for_label_syntax_tree.pdf
+
+dot sample_for_label_diriv_tree_with_labels.dot -Tpdf -o sample_for_label_diriv_tree_with_labels.pdf
+
+dot sample_for_label_diriv_tree_extracted.dot -Tpdf -o sample_for_label_diriv_tree_extracted.pdf
+
+dot sample_for_label_general.dot -Tpdf -o sample_for_label_general.pdf
+
+dot sample_for_label_general_ru.dot -Tpdf -o sample_for_label_general_ru.pdf

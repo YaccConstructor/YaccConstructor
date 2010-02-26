31f638e327055a754d80d38d18d8563c6d9e3be3
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-02-26 19:39:01 +0000

    Restructured Docs folder
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@172 b8160d1e-d411-11dd-a2b5-e5f827957e07

---
+++ Docs/Reports/SemenGrigorev/TFA/areticle_ru/ladot/make.sh
@@ -0,0 +1,10 @@
+dot2tex --template Template.tex createTNFACls.dot > createTNFACls.tex
+dot2tex --template Template.tex createTNFAAlt.dot > createTNFAAlt.tex
+dot2tex --template Template.tex createTNFASeq.dot > createTNFASeq.tex
+dot2tex --template Template.tex createTNFALeaf.dot > createTNFALeaf.tex
+dot2tex --template Template.tex TNFAExample.dot  > TNFAExample.tex
+dot2tex --template Template.tex TNFA2TDFA_s1.dot > TNFA2TDFAs1.tex
+dot2tex --template Template.tex TNFA2TDFA_s2.dot > TNFA2TDFAs2.tex
+pdflatex TNFA.tex
+acroread TNFA.pdf &
+

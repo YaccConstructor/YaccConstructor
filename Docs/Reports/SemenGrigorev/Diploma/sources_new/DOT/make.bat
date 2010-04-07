c1166f4502183df379ed9985205f6c75319268c5
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-04-07 14:44:15 +0000

    General toll structure diagram is added
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@218 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- Docs/Reports/SemenGrigorev/Diploma/sources_new/DOT/make.bat
+++ Docs/Reports/SemenGrigorev/Diploma/sources_new/DOT/make.bat
@@ -1,12 +1,14 @@
 dot sample_for_label_diriv_tree.dot -Tpdf -o sample_for_label_diriv_tree.pdf
 
-dot sample_for_label_syntax_tree.dot -Tpdf -o sample_for_label_syntax_tree.pdf
+#dot sample_for_label_syntax_tree.dot -Tpdf -o sample_for_label_syntax_tree.pdf
 
-dot sample_for_label_diriv_tree_with_labels.dot -Tpdf -o sample_for_label_diriv_tree_with_labels.pdf
+#dot sample_for_label_diriv_tree_with_labels.dot -Tpdf -o sample_for_label_diriv_tree_with_labels.pdf
 
-dot sample_for_label_diriv_tree_extracted.dot -Tpdf -o sample_for_label_diriv_tree_extracted.pdf
+#dot sample_for_label_diriv_tree_extracted.dot -Tpdf -o sample_for_label_diriv_tree_extracted.pdf
 
-dot sample_for_label_general.dot -Tpdf -o sample_for_label_general.pdf
+#dot sample_for_label_general.dot -Tpdf -o sample_for_label_general.pdf
 
-dot sample_for_label_general_ru.dot -Tpdf -o ../sample_for_label_general_ru.pdf
-dot sample_for_label_general_ru.dot -Tpdf -o ../../temp_20_febr/sample_for_label_general_ru.pdf
+#dot sample_for_label_general_ru.dot -Tpdf -o ../sample_for_label_general_ru.pdf
+#dot sample_for_label_general_ru.dot -Tpdf -o ../../temp_20_febr/sample_for_label_general_ru.pdf
+
+dot general_tool_structure.dot -Tpdf -o general_tool_structure.pdf

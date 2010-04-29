ce751e63b57ee276b50c179d21cfb56a5209c3e5
ulitin.k <ulitin.k@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-04-29 19:39:19 +0000

    Added empty project
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@235 b8160d1e-d411-11dd-a2b5-e5f827957e07

---
+++ YaccConstructor/RecursiveAscent/RecursiveAscent.fs
@@ -0,0 +1,10 @@
+﻿namespace Yard.Generators
+
+open Yard.Core
+
+type RecursiveAscent() = 
+    interface IGenerator with
+        member this.Name = "RecursiveAscent"
+        member this.Generate t = t.ToString()
+        member this.AcceptableProductionTypes = List.ofArray(Reflection.FSharpType.GetUnionCases typeof<IL.Production.t<string,string>>) |> List.map (fun unionCase -> unionCase.Name)
+    end

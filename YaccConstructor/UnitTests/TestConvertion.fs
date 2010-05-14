b403b45c4b1324f9f21bd20307eab2fc673b98f8
anastasia.nishnevich <anastasia.nishnevich@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-05-14 14:43:29 +0000

    added Convertion tests
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@239 b8160d1e-d411-11dd-a2b5-e5f827957e07

---
+++ YaccConstructor/UnitTests/TestConvertion.fs
@@ -0,0 +1,67 @@
+﻿module TestConvertion
+
+open FsCheck
+open System.Threading
+open Microsoft.FSharp.Text.Lexing
+open Yard.Core.Main
+open Yard.Core.IL
+open Yard.Core.IL.Production
+open NUnit.Framework
+open Yard.Core.Namer
+open Yard.Core.Convertions.ExpandEBNF
+open Yard.Core.Convertions.ExpandAlter
+open Yard.Core.Convertions.ExpandMeta
+
+open Yard.Generators.RecursiveAscent.GrammarPreparer
+
+module module2 =
+    [<TestFixture>]
+    type ``Convertion tests`` () =
+          let  correctForAlExp (t: t<Source.t,Source.t>) =
+              match t with
+            //  |PAlt     (a,b) -> correctForAlExp a  && correctForAlExp b 
+             // |PRef   _ 
+              //|PLiteral _
+            
+              |PToken   _    -> true
+             // |PAlt _-> true
+              | _ -> false
+
+        
+              
+          let ruleIsAfterEBNF (t: Rule.t<Source.t,Source.t>)   = 
+              match t.body with
+              | POpt _ | PSome _ | PMany _   -> false
+              | _ -> true 
+          
+          let ruleIsAfterMeta (t: Rule.t<Source.t,Source.t>)   = 
+              match t.body with
+              | PMetaRef  _   -> false
+              | _ -> true
+
+          let ruleIsAfterAlter (t: Rule.t<Source.t,Source.t>)   = 
+              match t.body with
+              | PAlt _   -> false
+              | _ -> true
+
+          let isOk t = convertToMeta t |> List.forall (fun x -> ruleIsAfterEBNF x) 
+
+// когда начальное условие не выполняется для большого числа вариантов все стоит 
+          let ExpandAlterTest(t : Rule.t<Source.t,Source.t> ) = (correctForAlExp t.body) ==> lazy ( isOk t)
+                                                               
+          let ExpandEBNFTest(t : Rule.t<Source.t,Source.t> ) =  convertToMeta t |> List.forall (fun x -> ruleIsAfterEBNF x)
+
+
+          let ExpandMetaRulesTest(l : Rule.t<Source.t,Source.t> list) =  (expandMetaRules l |> List.forall (fun x -> ruleIsAfterMeta x))
+
+
+          [<Test>] member test.``ExpandEBNF Test`` () =
+                     quickCheck ExpandEBNFTest  
+
+          [<Test>] member test.``ExpandAlter Test`` () =
+                     quickCheck ExpandAlterTest                         
+                     
+          [<Test>] member test.``ExpandMeta Test`` () =
+                     quickCheck ExpandMetaRulesTest 
+                      
+

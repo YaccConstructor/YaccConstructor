1245982e0854609d68307a55ef2ede0004880231
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-08-02 17:05:59 +0000

    RACC generator is migrated from latest version from "Semen" branch. Without Core
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@260 b8160d1e-d411-11dd-a2b5-e5f827957e07

---
+++ YaccConstructor/General/Grammar.fs
@@ -0,0 +1,43 @@
+﻿// Grammar.fs
+//
+// Copyright 2009-2010 Semen Grigorev
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation.
+
+module Yard.Generators.RecursiveAscent.Grammar
+
+open Yard.Core.CompareHelper
+
+module Item = begin 
+  
+   [<CustomEquality; CustomComparison>] 
+   type t<'a when 'a : equality> = 
+       {prod_num:int;
+        prod_name:string;
+        item_num:int;
+        symb: Option<'a>;
+        next_num: Option<int>;
+        seq_number:int;
+        s:int;
+        f: Set<int>;
+        fromStateTrace: List<List<Option<'a>>*List<List<Yard.Generators.RecursiveAscent.Trace>>>
+        toStateTrace: List<List<Option<'a>>*List<List<Yard.Generators.RecursiveAscent.Trace>>>}
+         member self.GetValue x = x.prod_num,x.item_num,x.symb,x.next_num
+         override self.Equals y = equalsOn self.GetValue self y
+         override self.GetHashCode() = hashOn self.GetValue self 
+         interface System.Collections.IStructuralComparable with      
+           member self.CompareTo (y,c) = c.Compare(self.GetValue self , self.GetValue(y :?> t<'a>))
+         override self.ToString() = 
+              "    production number: " + (self.prod_num.ToString()) + "\n"
+            + "    production name  : " + (self.prod_name) + "\n"
+            + "    item number      : " + (self.item_num.ToString()) + "\n"
+            + "    symbol           : " + (self.symb.ToString()) + "\n"
+            + "    next number      : " + (if self.next_num.IsSome then  self.next_num.ToString() else "null") + "\n"
+            + "    start state      : " + (self.s.ToString()) + "\n"
+            + "    finale states    : " + (self.s.ToString()) + "\n"
+            + "    trace for start state: " + (self.fromStateTrace.ToString()) + "\n"
+            + "    trace for start state: " + (self.toStateTrace.ToString()) + "\n"
+  end  
+
---
+++ YaccConstructor/General/Trace.fs
@@ -0,0 +1,41 @@
+﻿// Trace.fs
+//
+// Copyright 2010 Semen Grigorev
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation.
+
+namespace Yard.Generators.RecursiveAscent
+
+type Position = 
+|First
+|Second
+ override self.ToString() = 
+   match self with
+   |First  -> "First"
+   |Second -> "Second"
+
+type Trace = 
+| TSmbS of int
+| TSmbE of int
+| TSeqS of int
+| TSeqE of int
+| TAlt1S of int
+| TAlt1E of int
+| TAlt2S of int
+| TAlt2E of int
+| TClosureS of int
+| TClosureE of int
+ override self.ToString() = 
+   match self with
+   | TSmbS(n)      -> "TSmbS(" + n.ToString() + ")"
+   | TSmbE(n)      -> "TSmbE(" + n.ToString() + ")"
+   | TSeqS(n)      -> "TSeqS(" + n.ToString() + ")"
+   | TSeqE(n)      -> "TSeqE(" + n.ToString() + ")"
+   | TAlt1S(n)     -> "TAlt1S(" + n.ToString() + ")"
+   | TAlt1E(n)     -> "TAlt1E(" + n.ToString() + ")"
+   | TAlt2S(n)     -> "TAlt2S(" + n.ToString() + ")"
+   | TAlt2E(n)     -> "TAlt2E(" + n.ToString() + ")"
+   | TClosureS(n)  -> "TClosureS(" + n.ToString() + ")"
+   | TClosureE(n)  -> "TClosureE(" + n.ToString() + ")"

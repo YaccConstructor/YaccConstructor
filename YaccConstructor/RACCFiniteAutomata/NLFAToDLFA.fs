ee9b9efe80c64acd6e5633f9d22c8fd39786641e
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-09-07 19:16:05 +0000

    LFA draft
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@297 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- YaccConstructor/RACCFiniteAutomata/NLFAToDLFA.fs
+++ YaccConstructor/RACCFiniteAutomata/NLFAToDLFA.fs
@@ -10,9 +10,39 @@
 
 module Yard.Generators.RACC.NLFAToDLFA
 
-let eClosure nlfa = nlfa
 
-let NLFAToDLFA nlfa = eClosure nlfa
+//see Dragon book p129
+let NLFAToDLFA (nlfa:NLFA<_,_,_>) =
+    let symbols = List.map (fun rule -> rule.Symbol) nlfa.Rules
+    let move stateSet symbol = 
+        Set.map 
+            (fun state -> List.map 
+                               (fun rule -> rule.ToStateID)
+                               (List.filter 
+                                     (fun rule -> rule.FromStateID = state && rule.Symbol = symbol) 
+                                     nlfa.Rules))
+            stateSet
 
+    let eClosure statesSet = 
+        let stack = ref (List.ofSeq statesSet)
+        let eCls = ref statesSet
+        while not (List.isEmpty !stack) do            
+            let t = (!stack).Head
+            stack := (!stack).Tail   
+            List.iter 
+                 (fun state -> 
+                      if not (Set.exists ((=)state) !eCls)
+                      then 
+                        eCls := Set.add state !eCls
+                        stack := state::!stack)
+                 (List.map 
+                       (fun rule -> rule.ToStateID)
+                       (List.filter (fun rule -> rule.Symbol = Epsilon && rule.FromStateID = t) nlfa.Rules))
+        done
+        !eCls
+
+    let newStates = ref [(false,eClosure nlfa.StartStates)]
+    let newRules = 1
+    1
 
 

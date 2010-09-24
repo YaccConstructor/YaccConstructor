361370cdd0f0c5ea059cd2e5b43a6749eefc9a76
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-09-24 14:46:46 +0000

    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@314 b8160d1e-d411-11dd-a2b5-e5f827957e07

---
+++ YaccConstructor/_RACCCore/ParserResult.fs
@@ -0,0 +1,26 @@
+﻿// ParserResult.fs
+//
+// Copyright 2009-2010 Semen Grigorev
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation.
+
+namespace Yard.Generators._RACCGenerator
+
+open Yard.Core.CompareHelper
+
+[<CustomEquality; CustomComparison>]
+type ParserResult<'item, 'value, 'lb when 'item : comparison> =
+    {
+        rItem      : 'item        
+        rInpStream : Lexing.LexBuffer<'lb>
+        rLexer     : ILexer<'value,'lb>       
+    }
+      
+     member self.GetValue (x:ParserResult<_,_,_>) =
+        x.rItem, x.rInpStream
+     override self.Equals y = equalsOn self.GetValue self y
+     override self.GetHashCode() = hashOn self.GetValue self 
+     interface System.Collections.IStructuralComparable with      
+            member self.CompareTo (y,c) = c.Compare(self.GetValue self ,self.GetValue (y :?> ParserResult<'item, 'value, 'lb>))
--- YaccConstructor/_RACCCore/TableInterpretator.fs
+++ YaccConstructor/_RACCCore/TableInterpretator.fs
@@ -8,12 +8,41 @@
 
 namespace Yard.Generators._RACCGenerator
 
+type Item<'state, 'symbol> =
+    {
+        state  : 'state
+        symbol : 'symbol
+    }
+
 type TableInterpreter(tables) =
     class
         let goto states symbol = 
             Set.map 
                 (fun state -> (dict tables.gotoSet).[hash(state,symbol)])
                 states            
+
+        let getDFA state = tables.automataDict.[fst state]
+
+        let buildItem state= 
+            Set.map
+                (fun rule -> 
+                    {
+                        state = state
+                        symbol = 
+                            match rule.Symbol with
+                            | DSymbol (s) -> s
+                            | _           -> failwith "Error 01"
+                    })
+
+        let getItems smb state =
+            let dfa = getDFA state
+            Set.filter (fun rule -> rule.FromStateID = snd state && rule.Symbol = DSymbol smb) dfa.DRules
+            |> buildItem state
+
+        let getPrevItems smb state =
+            let dfa = getDFA state
+            Set.filter (fun rule -> rule.ToStateID = snd state && rule.Symbol = DSymbol smb) dfa.DRules
+            |> buildItem state
 
         let memoize f = 
             let t = new System.Collections.Generic.Dictionary<_,_>()
@@ -28,28 +57,48 @@ type TableInterpreter(tables) =
                     t.Add(key,res)
                     res                     
 
-        let climb = 
+        let rec climb = 
             memoize
                 (fun parserState ->
-                    let isFinaleState state= 
-                        let dfa = tables.automataDict.[fst state]
-                        Set.exists ((=) (snd state)) dfa.DFinaleStates
-                    let resPart1 = Set.filter isFinaleState parserState.statesSet
+                    let gotoSet = goto parserState.statesSet parserState.inpSymbol
+                    let parserResult = parse {parserState with statesSet = gotoSet}                    
+                    let resPart1 =                        
+                        let possibleStates = Set.map (getItems parserState.inpSymbol.name) parserState.statesSet  |> Set.unionMany
+                        let resItems =
+                            Set.filter
+                                (fun state ->
+                                    Set.exists
+                                        (fun res -> 
+                                            let prevItems = getPrevItems parserState.inpSymbol.name res.rItem
+                                            Set.exists ((=)state) prevItems
+                                            )
+                                        parserResult)
+                                possibleStates
+
+                        Set.map (fun item -> {parserResult with rItem = item.state}) resItems
                     let resPart2 = Set.empty
                         //let climbRes = climb 
                     resPart1 + resPart2)
 
         
-        let parse = 
+        and parse = 
             memoize
                 (fun parserState ->
                     let isFinaleState state= 
                         let dfa = tables.automataDict.[fst state]
                         Set.exists ((=) (snd state)) dfa.DFinaleStates
-                    let resPart1 = {parserState Set.filter isFinaleState parserState.statesSet
+                    let resPart1 = 
+                        Set.map 
+                            (fun item -> 
+                                {
+                                    rItem      = item
+                                    rInpStream = parserState.inpStream
+                                    rLexer     = parserState.lexer
+                                })
+                            (Set.filter isFinaleState parserState.statesSet)
                     let resPart2 = 
                         let climbRes = climb {parserState with inpSymbol = parserState.lexer.Next(parserState.inpStream)}
-                        Set.filter (not isFinaleState) 
+                        Set.filter (fun res -> not (isFinaleState res.rItem)) climbRes 
                     resPart1 + resPart2)
 
         

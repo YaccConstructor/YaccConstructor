ef6e0688fc5041fe75abe6a7ee824c3c66061ee7
ulitin.k <ulitin.k@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-09-26 10:51:28 +0000

    Added arguments parsing functionality
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@315 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- YaccConstructor/Main/Program.fs
+++ YaccConstructor/Main/Program.fs
@@ -12,32 +12,41 @@ let ApplyConvertion (ilTree:Definition.t<Source.t,Source.t>) (conv:IConvertion) 
         and  foot = ilTree.foot
     }
 
-let () =
-//    let commandLineArgs = System.Environment.GetCommandLineArgs()
+let () =    
+    let feName = ref "YardFrontend"
+    let generatorName = ref "_RACCGenerator"
+
+    let commandLineSpecs =
+        ["-f", ArgType.String (fun s -> feName := s), "Frontend name"
+//         "-c", ArgType.String (fun s -> 
+         "-g", ArgType.String (fun s -> generatorName := s), "Generator name"
+         ] |> List.map (fun (shortcut, argtype, description) -> ArgInfo(shortcut, argtype, description))
+    let commandLineArgs = System.Environment.GetCommandLineArgs()
+    ArgParser.Parse commandLineSpecs
+
     let grammarFilePath = @"..\..\..\..\Tests\test002.yrd"
-    let feName = "YardFrontend"
-    let generatorName = "_RACCGenerator"
+
 
     // Load frontends assemblies dlls - get them from file, current folder or command line
-    let assembly = System.Reflection.Assembly.Load(feName)
-    let inst = assembly.CreateInstance("Yard.Frontends."+feName+"."+feName)
+    let assembly = System.Reflection.Assembly.Load(!feName)
+    let inst = assembly.CreateInstance("Yard.Frontends." + !feName+"." + !feName)
     FrontendsManager.Register(inst :?> IFrontend);
 
     // Load generator assemblies dlls - get them from file, current folder or command line
-    let assembly = System.Reflection.Assembly.Load(generatorName)
-    let inst = assembly.CreateInstance("Yard.Generators."+generatorName+"."+generatorName)
+    let assembly = System.Reflection.Assembly.Load(!generatorName)
+    let inst = assembly.CreateInstance("Yard.Generators." + !generatorName+"." + !generatorName)
     GeneratorsManager.Register(inst :?> IGenerator);
 
     
     // Parse grammar
-    let ilTree = (FrontendsManager.Frontend feName).ParseGrammar grammarFilePath
+    let ilTree = (FrontendsManager.Frontend !feName).ParseGrammar grammarFilePath
    // let ilTree = (FrontendsManager.Frontend feName).ParseGrammar grammarFilePath
 
     // Apply convertions
     let ilTreeExpandedMeta = ApplyConvertion ilTree (new Yard.Core.Convertions.ExpandMeta.ExpandMeta())
 
     // Generate something
-    let gen = GeneratorsManager.Generator(generatorName)
+    let gen = GeneratorsManager.Generator(!generatorName)
     let s = gen.Generate (ilTree) // дерево передается без конвертации для FParsecGenerator
 
     //Run tests

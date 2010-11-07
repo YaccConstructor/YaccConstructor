63715786e775fef9ee2bb9c8770f64ea33898b17
ulitin.k <ulitin.k@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-11-07 21:09:43 +0000

    YardPrinter in progress
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@328 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- YaccConstructor/YardPrinter/Generator.fs
+++ YaccConstructor/YardPrinter/Generator.fs
@@ -11,29 +11,32 @@ let printRule (rule:Rule.t<Source.t,Source.t>) =
     let printAttr = function
         | Some(attr) -> "{"+(Source.toString attr)+"}"
         | None -> ""
-
+    let printListBrackets  l_br r_br metaArgs = if (List.isEmpty metaArgs) then "" else l_br+(String.concat " " (List.map Source.toString metaArgs))+r_br
+    let printMetaArgs = printListBrackets "<<" ">>"
+    let printArgs = printListBrackets "[" "]"
     let rec printProduction (production:Production.t<Source.t,Source.t>) = 
         let printElem (elem:elem<Source.t,Source.t>) = 
             let binding = function
+                | Some(x) when String.exists ((=) ':') (Source.toString x) -> "<"+(Source.toString x)+"> = "
                 | Some(x) -> (Source.toString x) + " = "
                 | None -> ""
-            (binding elem.binding) + (printProduction elem.rule)
+            (if elem.omit then "-" else "") + (binding elem.binding) + (printProduction elem.rule)
         match production with
-        | PAlt(alt1, alt2) -> "(" + (printProduction alt1) + " | " + (printProduction alt2) + ")"//Альтернатива
-        | PSeq(elem_list,attr_option) -> (String.concat " " (List.map printElem elem_list)) + printAttr(attr_option)//Последовательность * атрибут.(атрибут всегда применяется к последовательности) 
+        | PAlt(alt1, alt2) -> (printProduction alt1) + " | " + (printProduction alt2) //Альтернатива
+        | PSeq(elem_list,attr_option) -> (String.concat " " (List.map printElem elem_list)) + printAttr(attr_option) //Последовательность * атрибут.(атрибут всегда применяется к последовательности) 
         | PToken(source) -> Source.toString source //собственно токен
         | PRef(source, attr_option) -> (Source.toString source) + printAttr(attr_option) //Vanilla rule reference with an optional args list.
-//        |PMany    of (t<'patt,'expr>) //expr*
-//        |PMetaRef of Source.t * 'expr option * 'expr list // Metarule reference like in "a: mr<x> y z"
+        | PMany(many) -> "("+(printProduction many)+")*" //expr*
+        | PMetaRef(rule_name, _, args) -> (Source.toString rule_name)+(printMetaArgs args) // Metarule reference like in "a: mr<x> y z"
 //        |PLiteral of Source.t //Литерал. Хочется в правилах явно писать ,например, .."if" expr "then" expr...
 //        |PRepet   of (t<'patt,'expr>) * int option * int option  //extended regexp repetition, "man egrep" for details
 //        |PPerm    of (t<'patt,'expr>) list //permutation (A || B || C)   
 ///// The following are obsolete and reduction to PRepet should be discussed.
-//        |PSome    of (t<'patt,'expr>) //expr+
-//        |POpt     of (t<'patt,'expr>) //expr?
+        | PSome(some) -> "("+(printProduction some)+")+" //expr+
+        | POpt(opt) -> "("+(printProduction opt)+")?" //expr?
         | x -> "ERROR"
 
-    startSign + rule.name + ": " + (printProduction rule.body) + ";"
+    startSign + rule.name + (printMetaArgs rule.metaArgs) + (printArgs rule.args) + ": " + (printProduction rule.body) + ";"
 
 let generate (input_grammar:Definition.t<Source.t,Source.t>) =
     printSourceOpt(input_grammar.head)+(List.fold (fun concated rule -> concated+(printRule rule)+"\n" ) "" input_grammar.grammar)+printSourceOpt(input_grammar.foot)

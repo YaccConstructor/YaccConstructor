b4475659b4d27a4bd2ab3f5fd6d8b0ee5e4883f1
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-11-28 19:50:14 +0000

    GPLv3 license. Not finished.
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@386 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- YaccConstructor/RACCUserApplication/Driver.fs
+++ YaccConstructor/RACCUserApplication/Driver.fs
@@ -1,10 +1,21 @@
 ﻿// Driver.fs
 //
-// Copyright 2009-2010 Semen Grigorev
+//  Copyright 2009,2010 Semen Grigorev <rsdpisuy@gmail.com>
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation.
+//  This file is part of YaccConctructor.
+//
+//  YaccConstructor is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module m1
 
@@ -36,7 +47,8 @@ let run path =
     
     let trees = TableInterpreter.run l tables
     let r = 
-        Seq.collect (fun e -> e.rItem.forest |> List.map (ASTInterpretator.interp RACC.Actions.ruleToAction)) trees
+        1
+    //    Seq.collect (fun e -> e.rItem.forest |> List.map (ASTInterpretator.interp RACC.Actions.ruleToAction)) trees
         
     printf "\nResult %A\n" r
     trees        

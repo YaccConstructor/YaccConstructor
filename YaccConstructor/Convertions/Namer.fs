cdd3c44bf95fbad937f932e1f042b48928dc44e9
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-12-06 18:46:38 +0000

    copyright headers are added in Convertions files.
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@398 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- YaccConstructor/Convertions/Namer.fs
+++ YaccConstructor/Convertions/Namer.fs
@@ -1,9 +1,23 @@
-﻿(** module Namer contains: 
- *  constants and functions for naming of
- *  different items, created by Yard (during gammar transformations)
- *
- *  Author: Ilia
- *)
+﻿// module Namer contains: 
+//  constants and functions for naming of
+//  different items, created by Yard (during gammar transformations)
+//
+//  Copyright 2009,2010 Ilia Chemodanov
+//
+//  This file is part of YaccConctructor.
+//
+//  YaccConstructor is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module Yard.Core.Namer
 
--- YaccConstructor/Convertions/TransformAux.fs
+++ YaccConstructor/Convertions/TransformAux.fs
@@ -1,8 +1,22 @@
-﻿(** Module TransformAux contains:
- *  - common functions for grammar transformations
- *
- *  Author: Ilia
- *)
+﻿// Module TransformAux contains:
+//  - common functions for grammar transformations
+//
+//  Copyright 2009,2010 Ilia Chemodanov
+//
+//  This file is part of YaccConctructor.
+//
+//  YaccConstructor is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module Yard.Core.Convertions.TransformAux
 

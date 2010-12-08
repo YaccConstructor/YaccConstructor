c1ca74f4b5e43c9918b6b5dd22c528d19587cc28
rsdpisuy <rsdpisuy@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-12-08 09:45:30 +0000

    Copyrights are added.
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@403 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- YaccConstructor/RACCCore/RegExpAST.fs
+++ YaccConstructor/RACCCore/RegExpAST.fs
@@ -1,10 +1,22 @@
-﻿// RegExpAST.fs
+﻿// RegExpAST.fs contains types and functions for trace manipulations and 
+// trace to tree transformation
 //
-// Copyright 2009-2010 Semen Grigorev
+//  Copyright 2009,2010 Semen Grigorev <rsdpisuy@gmail.com>
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation.
+//  This file is part of YaccConctructor.
+//
+//  YaccConstructor is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace Yard.Generators.RACCGenerator
 
@@ -144,7 +156,7 @@ type RegExpAST() =
             | hd::[] -> hd
             | []     -> Set.empty                    
 
-        member self.BuilCorrectTrace trace = List.rev trace |> buildCorrectTrace  //|> Set.filter (verifyTrace >> fst)
+        member self.BuilCorrectTrace trace = List.rev trace |> buildCorrectTrace
 
         member self.BuildREAST trace values = buildREAST trace values
     end

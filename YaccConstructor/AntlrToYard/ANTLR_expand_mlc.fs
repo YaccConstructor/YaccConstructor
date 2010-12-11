dd3692038830aa39c6c9cdaac6c3b51aa2b5dcf5
ulitin.k <ulitin.k@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2010-12-11 16:09:33 +0000

    deleted unnessesary files
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@433 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- YaccConstructor/AntlrToYard/ANTLR_expand_mlc.fs
+++
@@ -1,8 +0,0 @@
-module ANTLR_expand_mlc
-
-open FParsec.Primitives
-open FParsec.CharParsers
-
-let rec public start   = many ( attempt((attempt (multiline_comment  |>> fun (_ as _1)   -> (_1 ))) )) |>> fun (_ as _1)   -> (_1 )
-
- and public multiline_comment = pstring "/*" >>= fun (_ ) -> (attempt(many ( attempt(Lexer.pCHAR))) >>= fun (_ ) -> (attempt(pstring "*/") |>> fun (_) -> (printf "comment") ))  

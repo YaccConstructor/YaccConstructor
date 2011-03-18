4fd42421ac88bc7fb025559af2b74412bfc325b6
ulitin.k <ulitin.k@b8160d1e-d411-11dd-a2b5-e5f827957e07>
2011-03-18 20:24:19 +0000

    Added tests for YardFrontend
    git-svn-id: https://recursive-ascent.googlecode.com/svn/trunk@592 b8160d1e-d411-11dd-a2b5-e5f827957e07

--- YaccConstructor/YardFrontend.Test/YardFrontendTests.fs
+++ YaccConstructor/YardFrontend.Test/YardFrontendTests.fs
@@ -23,44 +23,148 @@ module YardFrontendTester
 open Microsoft.FSharp.Text.Lexing
 open Yard.Frontends.YardFrontend
 open Yard.Frontends.YardFrontend.GrammarParser
+open Yard.Core.IL
+open Yard.Core.IL.Production
 open Yard.Core.IL.Definition
 open NUnit.Framework
 
 module Lexer = Yard.Frontends.YardFrontend.GrammarLexer
 
 
-[<TestFixture>]
-type ``YardFrontend lexer tests`` ()=    
-    [<Test>]
-    member test.``Lexer seq test 1`` () =
-        let buf = LexBuffer<_>.FromString "+" //: NUMBER PLUS NUMBER;\n
-        let lexemsSeq = seq {
-            while not buf.IsPastEndOfStream do
-                yield Lexer.main buf  
-        } 
-        let lexemsSeqCorrect = [ PLUS; EOF ] |> Seq.ofList
-//        #if DEBUG
-//        printfn "lexems seq:"
-//        lexemsSeq |> Seq.iter (fun x -> printf "%A\n" x)
-//        #endif
-//        printfn "%A" (Seq.toList lexemsSeq)
-        Assert.AreEqual(lexemsSeq, lexemsSeqCorrect)
+let lexerTest str lexemsListCorrect =
+    let buf = LexBuffer<_>.FromString str
+    Lexer.currentFileContent := str
+    let lexemsSeq = seq {
+        while not buf.IsPastEndOfStream do
+            yield Lexer.main buf  
+    } 
+    let lexemsList = Seq.toList lexemsSeq
+
+//    #if DEBUG
+    printfn "%A" lexemsList
+//    #endif
+    Assert.AreEqual(lexemsList, lexemsListCorrect)
+
+let parserTest str ilDefCorrect =
+    let buf = LexBuffer<_>.FromString str
+    Lexer.currentFileContent := str
+    let ilDef = GrammarParser.file Lexer.main buf
+//    #if DEBUG
+    printfn "ilDef = %A" ilDef
+    printfn "ilDefCorrect = %A" ilDefCorrect
+//    #endif
+    Assert.AreEqual(ilDef, ilDefCorrect)
+
+let completeTest str lexemsListCorrect ilDefCorrect = 
+    lexerTest str lexemsListCorrect
+    parserTest str ilDefCorrect
 
 [<TestFixture>]
-type ``YardFrontend Parser tests`` ()=    
+type ``YardFrontend lexer tests`` () =    
     [<Test>]
-    member test.``Seq test 1`` () =
-        let buf = LexBuffer<_>.FromString "+s: NUMBER PLUS NUMBER;\n" //
-        let ilDef = GrammarParser.file Lexer.main buf
-        let ilDefCorrect = {
-            info = { fileName = "" };
-            head = None;
-            grammar = [
+    member test.``Lexer seq test`` () =
+        lexerTest 
+            "+s: NUMBER PLUS NUMBER;"
+            [PLUS; LIDENT ("s", (1, 2)); COLON; UIDENT ("NUMBER", (4, 10)); 
+                UIDENT ("PLUS", (11, 15)); UIDENT ("NUMBER", (16, 22)); SEMICOLON; EOF]
+
+    [<Test>]
+    member test.``Lexer cls test`` () =
+        lexerTest 
+            "+s: (MINUS|PLUS)*;"
+            [PLUS; LIDENT ("s", (1, 2)); COLON; LPAREN; UIDENT ("MINUS", (5, 10)); BAR;
+                UIDENT ("PLUS", (11, 15)); RPAREN; STAR; SEMICOLON; EOF]
+
+[<TestFixture>]
+type ``YardFrontend Parser tests`` () =    
+    [<Test>]
+    member test.``Seq test`` () =
+        parserTest
+            "+s: NUMBER PLUS NUMBER;" 
+            { new Definition.t<Source.t, Source.t> with
+                info = {fileName = "";} 
+                and head = None  
+                and grammar = [{ 
+                    name = "s";
+                    args = [];
+                    body =
+                        PSeq(
+                            [{ 
+                                omit = false;
+                                rule = PToken ("NUMBER", (4, 10));
+                                binding = None;
+                                checker = None;
+                            }; { 
+                                omit = false;
+                                rule = PToken ("PLUS", (11, 15));
+                                binding = None;
+                                checker = None;
+                            }; {
+                                omit = false;
+                                rule = PToken ("NUMBER", (16, 22));
+                                binding = None;
+                                checker = None;
+                            }],
+                            None
+                        );
+                    _public = true;
+                    metaArgs = [];
+                }] 
+                and foot = None;
+            } 
                 
-            ];
-            foot = None;
-        }
-        #if DEBUG
-        printfn "tree: %A" res
-        #endif
-        Assert.AreEqual(ilDef,ilDefCorrect)
+[<TestFixture>]
+type ``YardFrontend Complete tests`` () =    
+    [<Test>]
+    member test.``L_attr test`` () =
+        completeTest @"
+{
+let value x = (x:>Lexeme<string>).value
+}
++s: <res:int> = e[1] {res};
+e[i]: n=NUMBER {(value n |> int) + i};"
+            [ACTION (@"
+let value x = (x:>Lexeme<string>).value
+", 
+                (3, 46)); PLUS;
+                LIDENT ("s", (50, 51)); COLON; PATTERN ("res:int", (54, 61)); EQUAL;
+                LIDENT ("e", (65, 66)); PARAM ("1", (67, 68)); ACTION ("res", (71, 74));
+                SEMICOLON; LIDENT ("e", (78, 79)); PARAM ("i", (80, 81)); COLON;
+                LIDENT ("n", (84, 85)); EQUAL; UIDENT ("NUMBER", (86, 92));
+                ACTION ("(value n |> int) + i", (94, 114)); SEMICOLON; EOF]
+            { new Definition.t<Source.t, Source.t> with 
+                info = { fileName = ""; }
+                and head = Some ("\r\nlet value x = (x:>Lexeme<string>).value\r\n", (3, 46))
+                and grammar = [{ 
+                    name = "s";
+                    args = [];
+                    body = 
+                        PSeq (
+                            [{
+                                omit = false;
+                                rule = PRef (("e", (65, 66)),Some ("1", (67, 68)));
+                                binding = Some ("res:int", (54, 61));
+                                checker = None;
+                            }],
+                            Some ("res", (71, 74))
+                        );
+                    _public = true;
+                    metaArgs = [];
+                  }; { 
+                    name = "e";
+                    args = [("i", (80, 81))];
+                    body = 
+                        PSeq (
+                            [{
+                                omit = false;
+                                rule = PToken ("NUMBER", (86, 92));
+                                binding = Some ("n", (84, 85));
+                                checker = None;
+                            }],
+                            Some ("(value n |> int) + i", (94, 114))
+                        );
+                    _public = false;
+                    metaArgs = [];
+                }];
+                and foot = None
+            }

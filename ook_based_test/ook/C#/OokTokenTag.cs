afc035affbd07cce51f6949a9c22d05aaae1fede
rsdpisuy <rsdpisuy@gmail.com>
2011-10-20 14:26:16 +0400

    Ook based solution for tests and research VS 2010 integration.

---
+++ ook_based_test/ook/C#/OokTokenTag.cs
@@ -0,0 +1,310 @@
+﻿// Copyright (c) Microsoft Corporation
+// All rights reserved
+
+namespace OokLanguage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Classification;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Text.Tagging;
+    using Microsoft.VisualStudio.Utilities;
+    using System.Linq;
+
+    [Export(typeof(IViewTaggerProvider))]
+    [ContentType("text")]
+    [TagType(typeof(TextMarkerTag))]
+    internal class BraceMatchingTaggerProvider : IViewTaggerProvider
+    {
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            return new BraceMatchingTagger(textView, buffer) as ITagger<T>;
+        }
+    }
+
+    internal class BraceMatchingTagger : ITagger<TextMarkerTag>
+    {
+
+        ITextView View { get; set; }
+        ITextBuffer SourceBuffer { get; set; }
+        SnapshotPoint? CurrentChar { get; set; }
+        private Dictionary<char, char> m_braceList;
+
+        internal BraceMatchingTagger(ITextView view, ITextBuffer sourceBuffer)
+        {
+           
+            //here the keys are the open braces, and the values are the close braces
+            m_braceList = new Dictionary<char, char>();
+            m_braceList.Add('{', '}');
+            m_braceList.Add('[', ']');
+            m_braceList.Add('(', ')');
+            this.View = view;
+            this.SourceBuffer = sourceBuffer;
+            this.CurrentChar = null;
+
+            this.View.Caret.PositionChanged += CaretPositionChanged;
+            this.View.LayoutChanged += ViewLayoutChanged;
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+
+        void ViewLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
+        {
+            if (e.NewSnapshot != e.OldSnapshot) //make sure that there has really been a change
+            {
+                UpdateAtCaretPosition(View.Caret.Position);
+            }
+        }
+
+        void CaretPositionChanged(object sender, CaretPositionChangedEventArgs e)
+        {
+            UpdateAtCaretPosition(e.NewPosition);
+        }
+
+        void UpdateAtCaretPosition(CaretPosition caretPosition)
+        {
+            CurrentChar = caretPosition.Point.GetPoint(SourceBuffer, caretPosition.Affinity);
+
+            if (!CurrentChar.HasValue)
+                return;
+
+            var tempEvent = TagsChanged;
+            if (tempEvent != null)
+                tempEvent(this, new SnapshotSpanEventArgs(new SnapshotSpan(SourceBuffer.CurrentSnapshot, 0,
+                    SourceBuffer.CurrentSnapshot.Length)));
+        }
+
+        public IEnumerable<ITagSpan<TextMarkerTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            if (spans.Count == 0)   //there is no content in the buffer
+                yield break;
+
+            //don't do anything if the current SnapshotPoint is not initialized or at the end of the buffer
+            if (!CurrentChar.HasValue || CurrentChar.Value.Position >= CurrentChar.Value.Snapshot.Length)
+                yield break;
+
+            //hold on to a snapshot of the current character
+            SnapshotPoint currentChar = CurrentChar.Value;
+
+            //if the requested snapshot isn't the same as the one the brace is on, translate our spans to the expected snapshot
+            if (spans[0].Snapshot != currentChar.Snapshot)
+            {
+                currentChar = currentChar.TranslateTo(spans[0].Snapshot, PointTrackingMode.Positive);
+            }
+
+            //get the current char and the previous char
+            char currentText = currentChar.GetChar();
+            SnapshotPoint lastChar = currentChar == 0 ? currentChar : currentChar - 1; //if currentChar is 0 (beginning of buffer), don't move it back
+            char lastText = lastChar.GetChar();
+            SnapshotSpan pairSpan = new SnapshotSpan();
+
+            if (m_braceList.ContainsKey(currentText))   //the key is the open brace
+            {
+                char closeChar;
+                m_braceList.TryGetValue(currentText, out closeChar);
+                if (BraceMatchingTagger.FindMatchingCloseChar(currentChar, currentText, closeChar, View.TextViewLines.Count, out pairSpan) == true)
+                {
+                    yield return new TagSpan<TextMarkerTag>(new SnapshotSpan(currentChar, 1), new TextMarkerTag("blue"));
+                    yield return new TagSpan<TextMarkerTag>(pairSpan, new TextMarkerTag("blue"));
+                }
+            }
+            else if (m_braceList.ContainsValue(lastText))    //the value is the close brace, which is the *previous* character 
+            {
+                var open = from n in m_braceList
+                           where n.Value.Equals(lastText)
+                           select n.Key;
+                if (BraceMatchingTagger.FindMatchingOpenChar(lastChar, (char)open.ElementAt<char>(0), lastText, View.TextViewLines.Count, out pairSpan) == true)
+                {
+                    yield return new TagSpan<TextMarkerTag>(new SnapshotSpan(lastChar, 1), new TextMarkerTag("blue"));
+                    yield return new TagSpan<TextMarkerTag>(pairSpan, new TextMarkerTag("blue"));
+                }
+            }
+        }
+
+        private static bool FindMatchingCloseChar(SnapshotPoint startPoint, char open, char close, int maxLines, out SnapshotSpan pairSpan)
+        {
+            pairSpan = new SnapshotSpan(startPoint.Snapshot, 1, 1);
+            ITextSnapshotLine line = startPoint.GetContainingLine();
+            string lineText = line.GetText();
+            int lineNumber = line.LineNumber;
+            int offset = startPoint.Position - line.Start.Position + 1;
+
+            int stopLineNumber = startPoint.Snapshot.LineCount - 1;
+            if (maxLines > 0)
+                stopLineNumber = Math.Min(stopLineNumber, lineNumber + maxLines);
+
+            int openCount = 0;
+            while (true)
+            {
+                //walk the entire line
+                while (offset < line.Length)
+                {
+                    char currentChar = lineText[offset];
+                    if (currentChar == close) //found the close character
+                    {
+                        if (openCount > 0)
+                        {
+                            openCount--;
+                        }
+                        else    //found the matching close
+                        {
+                            pairSpan = new SnapshotSpan(startPoint.Snapshot, line.Start + offset, 1);
+                            return true;
+                        }
+                    }
+                    else if (currentChar == open) // this is another open
+                    {
+                        openCount++;
+                    }
+                    offset++;
+                }
+
+                //move on to the next line
+                if (++lineNumber > stopLineNumber)
+                    break;
+
+                line = line.Snapshot.GetLineFromLineNumber(lineNumber);
+                lineText = line.GetText();
+                offset = 0;
+            }
+
+            return false;
+        }
+
+        private static bool FindMatchingOpenChar(SnapshotPoint startPoint, char open, char close, int maxLines, out SnapshotSpan pairSpan)
+        {
+            pairSpan = new SnapshotSpan(startPoint, startPoint);
+
+            ITextSnapshotLine line = startPoint.GetContainingLine();
+
+            int lineNumber = line.LineNumber;
+            int offset = startPoint - line.Start - 1; //move the offset to the character before this one
+
+            //if the offset is negative, move to the previous line
+            if (offset < 0)
+            {
+                line = line.Snapshot.GetLineFromLineNumber(--lineNumber);
+                offset = line.Length - 1;
+            }
+
+            string lineText = line.GetText();
+
+            int stopLineNumber = 0;
+            if (maxLines > 0)
+                stopLineNumber = Math.Max(stopLineNumber, lineNumber - maxLines);
+
+            int closeCount = 0;
+
+            while (true)
+            {
+                // Walk the entire line
+                while (offset >= 0)
+                {
+                    char currentChar = lineText[offset];
+
+                    if (currentChar == open)
+                    {
+                        if (closeCount > 0)
+                        {
+                            closeCount--;
+                        }
+                        else // We've found the open character
+                        {
+                            pairSpan = new SnapshotSpan(line.Start + offset, 1); //we just want the character itself
+                            return true;
+                        }
+                    }
+                    else if (currentChar == close)
+                    {
+                        closeCount++;
+                    }
+                    offset--;
+                }
+
+                // Move to the previous line
+                if (--lineNumber < stopLineNumber)
+                    break;
+
+                line = line.Snapshot.GetLineFromLineNumber(lineNumber);
+                lineText = line.GetText();
+                offset = line.Length - 1;
+            }
+            return false;
+        }
+    }
+
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("ook!")]
+    [TagType(typeof(OokTokenTag))]
+    internal sealed class OokTokenTagProvider : ITaggerProvider
+    {
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            return new OokTokenTagger(buffer) as ITagger<T>;
+        }
+    }
+
+    public class OokTokenTag : ITag 
+    {
+        public OokTokenTypes type { get; private set; }
+
+        public OokTokenTag(OokTokenTypes type)
+        {
+            this.type = type;
+        }
+    }
+
+    internal sealed class OokTokenTagger : ITagger<OokTokenTag>
+    {
+
+        ITextBuffer _buffer;
+        IDictionary<string, OokTokenTypes> _ookTypes;
+
+        internal OokTokenTagger(ITextBuffer buffer)
+        {
+            _buffer = buffer;
+            _ookTypes = new Dictionary<string, OokTokenTypes>();
+            _ookTypes["ook!"] = OokTokenTypes.OokExclaimation;
+            _ookTypes["ook."] = OokTokenTypes.OokPeriod;
+            _ookTypes["ook?"] = OokTokenTypes.OokQuestion;
+            //_ookTypes["("] = OokTokenTypes.OBrace;
+            //_ookTypes[")"] = OokTokenTypes.CBrace;
+
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public IEnumerable<ITagSpan<OokTokenTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+
+            foreach (SnapshotSpan curSpan in spans)
+            {
+                ITextSnapshotLine containingLine = curSpan.Start.GetContainingLine();
+                int curLoc = containingLine.Start.Position;
+                string[] tokens = containingLine.GetText().ToLower().Split(' ');
+
+                foreach (string ookToken in tokens)
+                {
+                    if (_ookTypes.ContainsKey(ookToken))
+                    {
+                        var tokenSpan = new SnapshotSpan(curSpan.Snapshot, new Span(curLoc, ookToken.Length));
+                        if( tokenSpan.IntersectsWith(curSpan) ) 
+                            yield return new TagSpan<OokTokenTag>(tokenSpan, 
+                                                                  new OokTokenTag(_ookTypes[ookToken]));
+                    }
+
+                    //add an extra char location because of the space
+                    curLoc += ookToken.Length + 1;
+                }
+            }
+            
+        }
+    }
+}

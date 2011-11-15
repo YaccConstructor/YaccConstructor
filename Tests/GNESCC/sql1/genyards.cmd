b278ee28837c13090e2b850dc5297908a075497b
kakadu.hafanana <kakadu.hafanana@gmail.com>
2011-11-15 12:18:07 +0300

    created test for gnes[ 'c' ]*

---
+++ Tests/GNESCC/sql1/genyards.cmd
@@ -0,0 +1,13 @@
+cls
+set failo=yards\a1.yrd
+
+del /F %failo%.actions.fs
+del /F %failo%.regexp.fs
+del /F %failo%.tables.fs
+
+..\..\YaccConstructor\Main\bin\Release\YaccConstructor.exe -c ExpandMeta -f YardFrontend -g GNESCCGenerator -i %failo%
+
+IF exist %failo%.actions.fs copy %failo%.actions.fs %failo%\..\..\Actions.fs
+IF exist %failo%.tables.fs copy %failo%.tables.fs %failo%\..\..\Tables.fs
+IF exist %failo%.regexp.fs copy %failo%.regexp.fs %failo%\..\..\Regexp.fs
+

1c61a1755d3e6bac3b6e8a717d5b6d1354abc344
rsdpisuy <rsdpisuy@gmail.com>
2011-11-29 19:29:24 +0400

    Small cleanup.

--- YaccConstructor/Common/GeneratorsManager.fs
+++ YaccConstructor/Common/GeneratorsManager.fs
@@ -16,6 +16,5 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module Yard.Core.GeneratorsManager
-type GeneratorsManager () =
-    inherit Yard.Core.Manager.Manager<Generator>()    
+type GeneratorsManager () = inherit Yard.Core.Manager.Manager<Generator>()    
     

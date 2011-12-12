1033665f04db57a070a00cc08057bcaecc13406a
Shcherbakov Oleg <shcherbakov.o@gmail.com>
2011-12-12 13:45:52 +0400

    Some clean-up and some commented nonworking lines

--- VSYard/VSYard/Classifiers/YardClassifier.cs
+++ VSYard/VSYard/Classifiers/YardClassifier.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ComponentModel.Composition;
-using Microsoft.VisualStudio.Text;
-using Microsoft.VisualStudio.Text.Classification;
-using Microsoft.VisualStudio.Text.Editor;
-using Microsoft.VisualStudio.Text.Tagging;
-using Microsoft.VisualStudio.Utilities;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.ComponentModel.Composition;
+//using Microsoft.VisualStudio.Text;
+//using Microsoft.VisualStudio.Text.Classification;
+//using Microsoft.VisualStudio.Text.Editor;
+//using Microsoft.VisualStudio.Text.Tagging;
+//using Microsoft.VisualStudio.Utilities;
 
 
-namespace MyCompany.VSYard.Classifiers
-{
-    [Export(typeof(ITaggerProvider))]
-    [ContentType("yard")]
-    [TagType(typeof(ClassificationTag))]
-    internal sealed class YardClassifierProvider : ITaggerProvider
-    {
-        [Export]
-        [Name("yard")]
-        [BaseDefinition("code")]
-        internal static ContentTypeDefinition OokContentType = null;
+//namespace MyCompany.VSYard.Classifiers
+//{
+//    [Export(typeof(ITaggerProvider))]
+//    [ContentType("yard")]
+//    [TagType(typeof(ClassificationTag))]
+//    internal sealed class YardClassifierProvider : ITaggerProvider
+//    {
+//        [Export]
+//        [Name("yard")]
+//        [BaseDefinition("code")]
+//        internal static ContentTypeDefinition OokContentType = null;
 
-        [Export]
-        [FileExtension(".yrd")]
-        [ContentType("yard")]
-        internal static FileExtensionToContentTypeDefinition OokFileType = null;
+//        [Export]
+//        [FileExtension(".yrd")]
+//        [ContentType("yard")]
+//        internal static FileExtensionToContentTypeDefinition OokFileType = null;
 
-        [Import]
-        internal IClassificationTypeRegistryService ClassificationTypeRegistry = null;
+//        [Import]
+//        internal IClassificationTypeRegistryService ClassificationTypeRegistry = null;
 
-        [Import]
-        internal IBufferTagAggregatorFactoryService aggregatorFactory = null;
+//        [Import]
+//        internal IBufferTagAggregatorFactoryService aggregatorFactory = null;
 
-        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
-        {
-            ITagAggregator<VSYardNS.TokenTag> ookTagAggregator =
-                                            aggregatorFactory.CreateTagAggregator<VSYardNS.TokenTag>(buffer);
+//        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+//        {
+//            ITagAggregator<VSYardNS.TokenTag> ookTagAggregator =
+//                                            aggregatorFactory.CreateTagAggregator<VSYardNS.TokenTag>(buffer);
 
-            return new VSYardNS.YardClassifier(buffer, ookTagAggregator, ClassificationTypeRegistry) as ITagger<T>;
-        }
-    }
-}
+//            return new VSYardNS.YardClassifier(buffer, ookTagAggregator, ClassificationTypeRegistry) as ITagger<T>;
+//        }
+//    }
+//}
--- VSYard/VSYardLibrary/VSYardLibrary/BraceMatchingTagger.fs
+++ VSYard/VSYardLibrary/VSYardLibrary/BraceMatchingTagger.fs
@@ -10,18 +10,61 @@ open Microsoft.VisualStudio.Text.Tagging
 open Microsoft.VisualStudio.Utilities
 open System.Linq
 
+
 type BraceMatchingTagger (view : ITextView, sourceBuffer : ITextBuffer) =
-    let caretPositionChanged  = 
-       fun x ->
-          let y = x 
-          () 
+    let mutable View : ITextView = null
+    let mutable SourceBuffer : ITextBuffer = null
+    let mutable CurrentChar : Nullable<SnapshotPoint> = new Nullable<SnapshotPoint>() //is this right?
+//    let mutable CurrenToken : Yard.Frontends.YardFrontend.GrammarParser.token = Yard.Frontends.YardFrontend.GrammarParser.LPAREN (new Yard.Frontends.YardFrontend.GrammarParser.Range(1,2))
+//    let mutable m_braceList : Dictionary<Yard.Frontends.YardFrontend.GrammarParser.token,Yard.Frontends.YardFrontend.GrammarParser.token> = null
+    let mutable lexeredText = null
+
+// *    let TagsChanged : Event<SnapshotSpanEventArgs> = new Event<SnapshotSpanEventArgs>() //how should this be done?
+//    let UpdateAtCaretPosition caretPosition : CaretPosition = 
+//        CurrentChar <- caretPosition.Point.GetPoint(SourceBuffer, caretPosition.Affinity)
+//        if not CurrentChar.HasValue then ()
+//            else
+//                let tempEvent = TagsChanged
+//                if not (tempEvent.Equals(null)) then tempEvent.Trigger(new SnapshotSpanEventArgs(new SnapshotSpan(SourceBuffer.CurrentSnapshot, 0,SourceBuffer.CurrentSnapshot.Length)))
+//    let CaretPositionChanged (sender : obj, e : CaretPositionChangedEventArgs) = 
+//        UpdateAtCaretPosition(e.NewPosition)
+//          ()
+//                
+//    let ViewLayoutChanged (sender : obj, e : TextViewLayoutChangedEventArgs) = 
+//        if e.NewSnapshot = e.OldSnapshot then UpdateAtCaretPosition(View.Caret.Position)
+//        ()
     do
-      view.Caret.PositionChanged.Add(caretPositionChanged)
+        //m_braceList.Add(Yard.Frontends.YardFrontend.GrammarParser.LPAREN,Yard.Frontends.YardFrontend.GrammarParser.RPAREN)
+        View <- view
+        SourceBuffer <- sourceBuffer
+// *       View.Caret.PositionChanged.Add(CaretPositionChanged)
+//        View.LayoutChanged.Add(ViewLayoutChanged)
+//        if not (View.ToString() = "") then lexeredText <- Yard.Frontends.YardFrontend.Main.LexString (View.ToString())
+
     let getTags (spans : NormalizedSnapshotSpanCollection) =
-        //Здесь логика по подсветке скобок
-        let spans_Text = spans.ToString
-        let preres = Yard.Frontends.YardFrontend.Main.LexString "ahgfvhasg"
+        if spans.Count = 0 then ()
+
+//        if not ((not CurrentChar.HasValue) || CurrentChar.Value.Position >= CurrentChar.Value.Snapshot.Length)&& (not (spans.Count = 0) ) then 
+//            let mutable currentChar : SnapshotPoint = CurrentChar.Value
+//        
+//            if not (spans.ElementAt(0).Snapshot = currentChar.Snapshot) then                   //is replacing spans[0] to what is here correct?
+//                currentChar <- currentChar.TranslateTo(spans.ElementAt(0).Snapshot, PointTrackingMode.Positive)
+//
+//            let mutable currentText : char = currentChar.GetChar()
+//            let mutable lastChar : SnapshotPoint = 
+//                if currentChar.Position = 0 then currentChar
+//                    else currentChar - 1
+//            let mutable lastText = lastChar.GetChar()
+//            let mutable pairSpan = new SnapshotSpan()
+//
+//    //        if currentText = Yard.Frontends.YardFrontend.GrammarParser.LPAREN then
+//    //            let closeChar = ')'
+//
+//            let x = Yard.Frontends.YardFrontend.Main.LexString (View.ToString())
+//            ()
+
         Seq.empty
+    
         
     interface ITagger<TextMarkerTag> with
         member self.GetTags spans = getTags spans

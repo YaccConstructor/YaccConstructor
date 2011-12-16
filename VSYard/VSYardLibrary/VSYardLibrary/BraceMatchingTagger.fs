259265c7bf4142b5a1c1969360c71f1b32f40c16
rsdpisuy <rsdpisuy@gmail.com>
2011-12-16 10:47:55 +0400

    Event subscription.

--- VSYard/VSYardLibrary/VSYardLibrary/BraceMatchingTagger.fs
+++ VSYard/VSYardLibrary/VSYardLibrary/BraceMatchingTagger.fs
@@ -15,14 +15,13 @@ open Yard.Frontends.YardFrontend.GrammarParser
 type BraceMatchingTagger (view : ITextView, sourceBuffer : ITextBuffer) as self =
     let mutable View : ITextView = null
     let mutable SourceBuffer : ITextBuffer = null
-    let mutable CurrentChar : Nullable<SnapshotPoint> = new Nullable<SnapshotPoint>() //is this right?
+    let mutable CurrentChar : Nullable<SnapshotPoint> = new Nullable<SnapshotPoint>()
     let mutable lexeredText = null
 
     let getTags (spans : NormalizedSnapshotSpanCollection) =
         Seq.empty
 
     let TagsChanged = new Event<_>()
-        
 
     let UpdateAtCaretPosition (position : CaretPosition) =
         CurrentChar <- position.Point.GetPoint(SourceBuffer, position.Affinity)
@@ -46,9 +45,9 @@ type BraceMatchingTagger (view : ITextView, sourceBuffer : ITextBuffer) as self 
         lexeredText <- LexString (SourceBuffer.CurrentSnapshot.GetText())
         let currentPosition = start.Position //shows the number of chars before the cursor position
         let hasTheCurrentPosition t =
-                match t with
-                | LPAREN (x) -> (x.Start.AbsoluteOffset = currentPosition)
-                | _ -> false
+            match t with
+            | LPAREN (x) -> (x.Start.AbsoluteOffset = currentPosition)
+            | _ -> false
         let isParenthesisToken t =
             match t with
             | LPAREN (x) -> (x.Start.AbsoluteOffset > currentPosition)
@@ -82,7 +81,6 @@ type BraceMatchingTagger (view : ITextView, sourceBuffer : ITextBuffer) as self 
 
     interface ITagger<TextMarkerTag> with
         member self.GetTags spans = getTags spans
-        member self.add_TagsChanged z  = //no idea of any way of transforming EventHandler<SnapshotSpanEvebtArgs> to acceptible type for Event.add or TagsChanged.Add
-           // TagsChanged.Publish.AddHandler z
-            () // Event.add x TagsChanged.Publish
-        member self.remove_TagsChanged x =  ()
+        member self.add_TagsChanged x =
+           Event.add (fun arg -> x.Invoke(self,arg)) TagsChanged.Publish
+        member self.remove_TagsChanged x = ()

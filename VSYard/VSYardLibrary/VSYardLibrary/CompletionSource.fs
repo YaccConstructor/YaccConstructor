06d176a26b75f59f0b9c361e5c4d635f7f70e601
Shcherbakov Oleg <shcherbakov.o@gmail.com>
2012-03-08 20:29:32 +0400

    a try of being parallel.

--- VSYard/VSYardLibrary/VSYardLibrary/CompletionSource.fs
+++ VSYard/VSYardLibrary/VSYardLibrary/CompletionSource.fs
@@ -26,36 +26,42 @@ type YardCompletionSource (buffer : ITextBuffer) =
     do
         _buffer <- buffer
     let augmentCompletionSession (session : ICompletionSession) (completionSets : IList<CompletionSet>) = 
-        let recomputeAllCompletions () = 
+        let recomputeAllCompletions = 
+            async{
             let fileText = _buffer.CurrentSnapshot.GetText()              
             //get completions
             let getNonterminals (tree : Yard.Core.IL.Definition.t<Yard.Core.IL.Source.t,Yard.Core.IL.Source.t> ) = 
-                tree.grammar |> List.map (fun node -> new Completion(node.name))
+                tree.grammar |> List.map (fun node -> node.name)
             let completions =
                 try 
                     let parsed = ParseText fileText
                     let getText (completion : Completion) = completion.DisplayText
 //                    let sorted = List.sortBy getText (getNonterminals parsed)
-//                    List.ofSeq <| sorted.Distinct()
-                    let result =List.sortBy getText ( List.ofSeq <| (getNonterminals parsed).Distinct())
+//                    List.ofSeq <| sorted.Distinct() List.map (fun x -> new Completion(x))
+                    //let result = List.map (fun x -> new Completion(x)) ((getNonterminals parsed).Distinct() |> List.ofSeq |> List.sort) 
+                    let r = (getNonterminals parsed).Distinct() |> List.ofSeq |> List.sort |> List.rev
+                    let result = List.map (fun x -> new Completion(x)) r
                     result
                 with
                 | _ -> List.Empty
-            theList <- new ConcurrentBag<Completion>(List.ofSeq completions)
-
-        let snapshot = _buffer.CurrentSnapshot
-        let triggerPoint = session.GetTriggerPoint(snapshot).GetValueOrDefault() //ok?
+            theList <- new ConcurrentBag<Completion>(completions)}
+        let returnCollection = 
+            async{
+            let snapshot = _buffer.CurrentSnapshot
+            let triggerPoint = session.GetTriggerPoint(snapshot).GetValueOrDefault() //ok?
        
-        //check that triggerPoint != null
-        let line = triggerPoint.GetContainingLine()
-        let start = ref triggerPoint
-        while (!start).Position > line.Start.Position  && not ( Char.IsWhiteSpace((!start - 1).GetChar()) ) do //why doesn't work without impicit getting POSITION?
-            start := !start - 1
-        let x = new SnapshotSpan(!start, triggerPoint)
-        let applicableTo = snapshot.CreateTrackingSpan(x.Span, SpanTrackingMode.EdgeInclusive)//wtf? it works in C#;
-        let completions = theList.ToList()
-        completionSets.Add(new CompletionSet("All", "All", applicableTo, completions, Enumerable.Empty<Completion>()))
-        recomputeAllCompletions()
+            //check that triggerPoint != null
+            let line = triggerPoint.GetContainingLine()
+            let start = ref triggerPoint
+            while (!start).Position > line.Start.Position  && not ( Char.IsWhiteSpace((!start - 1).GetChar()) ) do //why doesn't work without impicit getting POSITION?
+                start := !start - 1
+            let x = new SnapshotSpan(!start, triggerPoint)
+            let applicableTo = snapshot.CreateTrackingSpan(x.Span, SpanTrackingMode.EdgeInclusive)//wtf? it works in C#;
+            let completionsList = theList.ToList()
+            completionSets.Add(new CompletionSet("All", "All", applicableTo, completionsList, Enumerable.Empty<Completion>()))}
+        Async.StartImmediate(recomputeAllCompletions)
+        Async.StartImmediate(returnCollection)
+        //[recomputeAllCompletions; returnCollection] |> Async.Parallel |> Async.RunSynchronously |> ignore
     let dispose () = 
         _disposed <- true
     interface ICompletionSource with

ab2dc3a60462fd57aac7699764420435a28a88b8
rsdpisuy <rsdpisuy@gmail.com>
2012-03-10 23:08:19 +0400

    Revert "From list of tuple to list with counter. Tests are fixed."
    This reverts commit 70f450483f0450e9048f02b9bc27197fd0565abd.

--- YaccConstructor/YardFrontend.Test/YardFrontendTests.fs
+++ YaccConstructor/YardFrontend.Test/YardFrontendTests.fs
@@ -293,7 +293,10 @@ type ``Yard frontend preprocessor tests`` () =
                             body = PSeq ([{omit = false
                                            rule = PToken ("A", (15, 16))
                                            binding = None
-                                           checker = None}],None)
+                                           checker = None}; {omit = false
+                                                             rule = PToken ("B", (31, 32))
+                                                             binding = None
+                                                             checker = None}],None)
                             _public = true
                             metaArgs = []}]
                 foot = None}

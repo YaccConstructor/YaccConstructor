be99d759ad65b0a9cc5faf6ccf9b0000a5afc51e
Alisa Bakaeva <alisa.bakaeva@gmail.com>
2012-07-04 12:42:04 +0400

    Simple lables are added.

--- YaccConstructor/CYK/CYK/Program.fs
+++ YaccConstructor/CYK/CYK/Program.fs
@@ -3,20 +3,20 @@
 
 //Правила контекстно-свободной грамматики в нормальной форме Хомского
 type Rule = 
-   |ToBranch of string*string*string//А->BC, A,B,C - нетерминалы
-   |ToLeaf of string*char//A->a, а - терминал 
+   |ToBranch of string*string*string*Option<string> //А->BC, A,B,C - нетерминалы
+   |ToLeaf of string*char*Option<string> //A->a, а - терминал 
 
 //Вывод правил
 let printRules =
    (function
-   | ToBranch(a,b,c) -> System.Console.WriteLine(String.concat "" [a;"->";b;c])                 
-   | ToLeaf(a,b)     -> System.Console.WriteLine(String.concat "" [a;"->";b.ToString()]))
+   | ToBranch(a,b,c,l) -> System.Console.WriteLine(String.concat "" [a; "->"; b; c; "  "; (match l with | Some t -> t | None -> "")])
+   | ToLeaf(a,b,l)     -> System.Console.WriteLine(String.concat "" [a;"->";b.ToString();"  "; (match l with | Some t -> t | None -> "") ]))
    |> List.iter
 
 //Последовательное применение правил rs, начиная с первого левого нетерминала start                             
 let printOutput rs start =
    //последовательно строит сентенциальные формы вывода
-   let rec sentForm current rules =  
+   let rec sentForm current rules =
       match rules with
       |x::xs -> //первый левый нетерминал
                 let leftNonTerminal = List.find (fun (y:string) -> (y = (y.ToUpper()))&&((y.ToUpper()) <> (y.ToLower()))) current
@@ -25,8 +25,8 @@ let printOutput rs start =
                    match oldForm with
                    |l::ls -> if l = leftNonTerminal then //ищем первый нетерминал
                                 match x with //замена l в соответствии с правилом x
-                                |ToBranch(a,b,c) -> b::c::ls
-                                |ToLeaf(a,b)     -> b.ToString()::ls
+                                |ToBranch(a,b,c,l) -> b::c::ls
+                                |ToLeaf(a,b,l)     -> b.ToString()::ls                                
                              else l::(newForm ls)
                    |_     -> []
                 System.Console.Write(String.concat "" ("->"::(newForm current)))                   
@@ -38,16 +38,17 @@ let printOutput rs start =
                                                                        
 let recognitionTable (rules,_) (s:string) = 
            
-   let recTable = Microsoft.FSharp.Collections.Array2D.create s.Length s.Length ([],(0,0),(0,0),[])
+   let recTable = Microsoft.FSharp.Collections.Array2D.create s.Length s.Length ([],(0,0),(0,0),None,[])
    //recTable(i,l), для l > 0
 
    let inline processRule rule i k l =
         match rule with
-        |ToBranch(a,b,c) -> let nonTerminals1,_,_,_ = recTable.[i,k]
-                            let nonTerminals2,_,_,_ = recTable.[k+i+1,l-k-1]
-                            let nonTerminals,_,_,rules = recTable.[i,l]
-                            if (List.exists ((=)b) nonTerminals1) && (List.exists ((=)c) nonTerminals2)
-                            then recTable.[i,l] <- (a::nonTerminals),(i,k),(k+i+1,l-k-1),(rule::rules)
+        |ToBranch(a,b,c,rule_lbl) ->
+            let nonTerminals1,_,_,lbl,_ = recTable.[i,k]
+            let nonTerminals2,_,_,lbl,_ = recTable.[k+i+1,l-k-1]
+            let nonTerminals,_,_,lbl,rules = recTable.[i,l]
+            if (List.exists ((=)b) nonTerminals1) && (List.exists ((=)c) nonTerminals2)
+            then recTable.[i,l] <- (a::nonTerminals),(i,k),(k+i+1,l-k-1),rule_lbl,(rule::rules)
         |_               -> ()   
 
    let elem i l = rules |>  Array.iter (fun rule -> for k in 0..(l-1) do processRule rule i k l)
@@ -67,9 +68,9 @@ let recognitionTable (rules,_) (s:string) =
    for rule in rules do
       for k in 0..(s.Length-1) do
          match rule with
-         |ToLeaf(a,b) -> if b = s.[k] then
-                            let nonTerminals,_,_,rules = recTable.[k,0]
-                            recTable.[k,0] <- ((a::nonTerminals),(-1,-1),(-1,-1),(rule::rules))
+         |ToLeaf(a,b,l) -> if b = s.[k] then
+                            let nonTerminals,_,_,lbl,rules = recTable.[k,0]
+                            recTable.[k,0] <- ((a::nonTerminals),(-1,-1),(-1,-1),l,(rule::rules))
          |_           -> ()
    //последующие столбцы
    fillTable 0 1
@@ -80,17 +81,17 @@ let recognize ((_, start) as g) s =
    let recTable = recognitionTable g s
    //восстановление вывода по таблице начиная с ячейки recTable(i,l) нетерминала top
    let rec subRecognize i l top = 
-      let nonTerminals,(leftI,leftL),(rightI,rightL),rs = recTable.[i,l]
+      let nonTerminals,(leftI,leftL),(rightI,rightL),lbl,rs = recTable.[i,l]
       //правило вида А->_, где А = top
-      let currentRule = List.find (fun (ToBranch(st,_,_)|ToLeaf(st,_)) -> st = top) rs
+      let currentRule = List.find (fun (ToBranch(st,_,_,l)|ToLeaf(st,_,l)) -> st = top) rs
       let leftNT,rightNT = 
          match currentRule with
-         |ToBranch(_,left,right) -> left,right
-         |ToLeaf(_,terminal)     -> terminal.ToString(),""  
+         |ToBranch(_,left,right,l) -> left,right
+         |ToLeaf(_,terminal,l)     -> terminal.ToString(),""  
       match i,l with
-      | _,0 -> [ToLeaf(top,leftNT.[0])]
+      | _,0 -> [ToLeaf(top,leftNT.[0],None)]
       | _     -> currentRule::(subRecognize leftI leftL leftNT)@(subRecognize rightI rightL rightNT)
-   let resultRules = if List.exists ((=)start) ((fun (a,_,_,_) -> a) recTable.[0, s.Length-1])
+   let resultRules = if List.exists ((=)start) ((fun (a,_,_,_,l) -> a) recTable.[0, s.Length-1])
                      then subRecognize 0 (s.Length-1) start//если цепочка принадлежит языку L(g)
                      else []
 
@@ -101,17 +102,21 @@ let recognize ((_, start) as g) s =
 
 
 //грамматика, допускающая операции +, * и позволяющая группировать их с помощью скобок, преобразованная в НФХ
-let testGrammar1 = (Array.ofList <| (List.map ToBranch [("E","T","N1");("N1","N2","E");("E","F","N3");("N3","N4","T");("E","N5","N6");("N6","E","N7");("E1","N2","E");("T","F","N3");("T","N5","N6");("T1","N4","T");("F","N5","N6")]) @ (List.map ToLeaf [("N2",'+');("N4",'*');("E",'a');("N5",'(');("N7",')');("T",'a');("F",'a')]),"E")
-let testGrammar2 = ((List.map ToBranch [("S","A","B");("B","N1","N2");("N1","D1","D2")])@(List.map ToLeaf [("A",'a');("D2",'c');("B",'a');("D1",'b');("N2",'d')]),"S")
+//let testGrammar1 = (Array.ofList <| (List.map ToBranch [("E","T","N1");("N1","N2","E");("E","F","N3");("N3","N4","T");("E","N5","N6");("N6","E","N7");("E1","N2","E");("T","F","N3");("T","N5","N6");("T1","N4","T");("F","N5","N6")]) @ (List.map ToLeaf [("N2",'+');("N4",'*');("E",'a');("N5",'(');("N7",')');("T",'a');("F",'a')]),"E")
+//let testGrammar2 = ((List.map ToBranch [("S","A","B");("B","N1","N2");("N1","D1","D2")])@(List.map ToLeaf [("A",'a');("D2",'c');("B",'a');("D1",'b');("N2",'d')]),"S")
 //System.Console.WriteLine("Test1:")
 //recognize testGrammar1 "a*a+a"
+
+let testGrammar_lbl1 = (Array.ofList <| (List.map ToBranch [("E","T","X",Some("l1"));("E","T","Y",Some("l2"));("E","E","E",None)]) @ (List.map ToLeaf [("T",'a',None);("X",'*',None);("Y",'+',None)]),"E")
 
 
 let str = List.init 20 (fun i -> "(a+a)*a") |> String.concat "+"
 System.Console.WriteLine("Test2:")
 let start = System.DateTime.Now
-recognize testGrammar1 str
+//recognize testGrammar1 str
 printfn "Time = %A" (System.DateTime.Now - start)
+
+recognize testGrammar_lbl1 "a*a+"
 
 //System.Console.WriteLine("Test3:")
 //recognize testGrammar2 "abcd"

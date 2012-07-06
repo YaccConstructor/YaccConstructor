a08d2677486a7f0ecdfa8ae69d969384aad41a70
rsdpisuy <rsdpisuy@gmail.com>
2012-07-06 15:57:57 +0400

    Small formatting and refactoring.

--- VSYard/VSYardLibrary/VSYardLibrary/CompletionSource.fs
+++ VSYard/VSYardLibrary/VSYardLibrary/CompletionSource.fs
@@ -26,41 +26,52 @@ type YardCompletionSource (buffer : ITextBuffer) =
 
     let augmentCompletionSession (session : ICompletionSession) (completionSets : IList<CompletionSet>) = 
         let tokens = buffer.CurrentSnapshot.GetText() |> LexString |> List.ofSeq //все токены
+
         let f = function 
             | SEMICOLON _ -> true
             |_ -> false
+
         let rec getNumberOfSemicolumns = function
             |[] -> 0
             |h::t when f h -> 1 + getNumberOfSemicolumns t
             | _::t -> getNumberOfSemicolumns t
-        let roolsNumber =
-            getNumberOfSemicolumns tokens
+
+        let roolsNumber = getNumberOfSemicolumns tokens
+
         //assume there was a change <=> number != roolsNumber
         let position = session.TextView.Caret.Position.BufferPosition.Position
 
         let less p = function
             | SEMICOLON r when r.Start.AbsoluteOffset <= p -> true
             | _ -> false
+
         let lessSemi = List.filter (less position) tokens
+
         let lessSemiPositions = List.map 
                                     <| function
                                        |(SEMICOLON r) -> r.End.AbsoluteOffset
                                        | _ -> 0
                                     <| lessSemi
+
         let Start = lessSemiPositions.Last()
+
         let greater p = function
             | SEMICOLON r when r.Start.AbsoluteOffset >= p -> true
             | _ -> false
+
         let greaterSemi = List.filter (greater position) tokens
+
         let greaterSemiPositions = List.map 
                                     <| function
                                        |(SEMICOLON r) -> r.End.AbsoluteOffset
                                        | _ -> 0
                                     <| greaterSemi
+
         let End = greaterSemiPositions.[0]
 
         let updatedText = session.TextView.TextBuffer.CurrentSnapshot.GetText()
         let u = updatedText.[Start..End]
+
         let recomputeAllCompletions = 
             async{
                     let fileText = session.TextView.TextBuffer.CurrentSnapshot.GetText()//u//buffer.CurrentSnapshot.GetText()
@@ -75,6 +86,7 @@ type YardCompletionSource (buffer : ITextBuffer) =
                     with
                     | _ -> ()
                     if theList.Count > 0 then l <- List.ofArray <| theList.ToArray() }
+
         let returnCollection () =            
             let snapshot = buffer.CurrentSnapshot
             let triggerPoint = session.GetTriggerPoint(snapshot).GetValueOrDefault()
@@ -89,17 +101,18 @@ type YardCompletionSource (buffer : ITextBuffer) =
             let mutable completionsList = theList.ToList()
             if theList.Count = 0 then completionsList <- l.ToList()
             completionSets.Add(new CompletionSet("All", "All", applicableTo, completionsList, Enumerable.Empty<Completion>()))
+
         [recomputeAllCompletions]|> Async.Parallel  |> Async.RunSynchronously |> ignore
         returnCollection ()
         
-    let dispose () = 
-        _disposed <- true
+    let dispose () =  _disposed <- true
+
     interface ICompletionSource with
         member self.AugmentCompletionSession (x, y) = 
             try 
-            augmentCompletionSession x y
+                augmentCompletionSession x y
             with
-            |_->()
+            |_-> ()
         member self.Dispose () = dispose ()
 
 

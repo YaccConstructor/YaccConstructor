1f39cae90db4719e486997d48df2032b2128df9c
rsdpisuy <rsdpisuy@gmail.com>
2012-07-10 20:38:02 +0400

    Rules list generation

--- YaccConstructor/CYKGenerator/CYKGenerator.fs
+++ YaccConstructor/CYKGenerator/CYKGenerator.fs
@@ -29,12 +29,63 @@ type CYKGeneartorImpl () =
         let ntermDict = new Dictionary<_,_>()
         let termDict = new Dictionary<_,_>()
         let lblDict = new Dictionary<_,_>()
+        let ntermNum = ref 1
+        let termNum = ref 1
+        let lblNum = ref 1
+        let processLbl lbl =
+            match lbl with
+            | Some (l:Production.DLabel) ->
+                let lblId =
+                    if lblDict.ContainsKey l.label
+                    then lblDict.[l.label] 
+                    else
+                        let id = !lblNum 
+                        lblDict.Add(l.label,id)
+                        incr lblNum
+                        id
+                match l.weight with
+                | Some i -> lblId, int i
+                | None -> lblId,0
+            | None -> 0,0
+
+        let ntermId name = 
+            if ntermDict.ContainsKey name 
+            then ntermDict.[name] 
+            else
+                let id = !ntermNum 
+                ntermDict.Add(name,id)
+                incr ntermNum
+                id
+
+        let processNtermElem (elem:Production.elem<_,_>) = 
+            match elem.rule with
+            | Production.PRef ((n,_),_) -> ntermId n                
+            | _ -> failwith "CYK. Incorrect rule structure. Expected PRef."
+
         let processRule (r:Rule.t<_,_>) =
             let name = r.name
-            let body = r.body
+            let body = r.body            
             match body with
-            | Production.PSeq ([t],_,_) -> ""
-            | Production.PSeq ([nt1;nt2],_,_) -> ""
+            | Production.PSeq ([elem],_,lbl) ->
+                match elem.rule with
+                | Production.PToken(n,_) -> 
+                    let tId =
+                        if termDict.ContainsKey n
+                        then termDict.[n]
+                        else
+                            let id = !termNum
+                            termDict.Add(n,id)
+                            incr termNum
+                            id
+                    let lN,lW = processLbl lbl
+
+                    buildRule (ntermId name) tId 0 lN lW
+
+                | _ -> failwith "CYK. Incorrect rule structure. Expected PToken."
+            | Production.PSeq ([elem1; elem2],_,lbl) -> 
+                let lN,lW = processLbl lbl
+                buildRule (ntermId name) (processNtermElem elem1) (processNtermElem elem2) lN lW
+
             | _ -> failwith "CYK. Incorrect rule structure. Must be in CNF"
             
         il.grammar |> List.map processRule
@@ -44,6 +95,7 @@ type CYKGeneartorImpl () =
         [rName]
 
     static member Generate grammar = ()
+    member x.GenRulesList grammar = grammarFromIL grammar
 
 type CYKGenerator() =    
     inherit Generator()

f865e799a093ecc010fc7473f9ccdc86c1a75ea7
rsdpisuy <rsdpisuy@gmail.com>
2012-07-20 20:35:27 +0400

    More complex demo. With timing.

--- Examples/CYK/DialectsCYKDemo/DialectsCYKDemo/Demo1.yrd.CYK.fs
+++ Examples/CYK/DialectsCYKDemo/DialectsCYKDemo/Demo1.yrd.CYK.fs
@@ -6,13 +6,17 @@ type cykToken =
   | EOF
   | NUM
   | PLUS
+  | MULT
 let getTag token = 
   match token with 
   | EOF -> 0us
   | NUM -> 1us
   | PLUS -> 2us
+  | MULT -> 3us
 let rules = 
-  [ 281483566841856UL; 562954248388864UL; 844433520067072UL; 844437815231232UL ]
+  [
+  281483566841856UL; 562954248388608UL; 1125908496777216UL; 1407387768455424UL;
+  844442110132224UL; 844446405099520UL; 844442110066688UL ]
   |> Array.ofList
 let StartNTerm = 1
 let CodeTokenStream (stream:seq<CYKToken<cykToken,_>>) = 

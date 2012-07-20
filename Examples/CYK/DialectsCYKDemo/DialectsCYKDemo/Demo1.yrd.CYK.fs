a78fa54454cc8563eb9d9d21563f2f5ae0285dfc
rsdpisuy <rsdpisuy@gmail.com>
2012-07-20 17:54:58 +0400

    CYK demo updated with last CYK generator.

--- Examples/CYK/DialectsCYKDemo/DialectsCYKDemo/Demo1.yrd.CYK.fs
+++ Examples/CYK/DialectsCYKDemo/DialectsCYKDemo/Demo1.yrd.CYK.fs
@@ -2,19 +2,22 @@ module Yard.Generators.CYK
 
 open Yard.Core
 open Yard.Generators.CYKGenerator
-
 type cykToken = 
+  | EOF
   | NUM
   | PLUS
-  | EOF
 let getTag token = 
   match token with 
-  | NUM -> 1
-  | PLUS -> 2
-  | EOF -> -1
+  | EOF -> 0us
+  | NUM -> 1us
+  | PLUS -> 2us
 let rules = 
   [ 281483566841856UL; 562954248388864UL; 844433520067072UL; 844437815231232UL ]
   |> Array.ofList
-let StartNTerm =  1
+let StartNTerm = 1
 let CodeTokenStream (stream:seq<CYKToken<cykToken,_>>) = 
-  stream |> Seq.map (fun t -> getTag t.Tag)
+  stream
+  |> Seq.choose (fun t ->
+    let tag = getTag t.Tag
+    if tag <> 0us then Some tag else None)
+  |> Array.ofSeq
--- Examples/CYK/DialectsCYKDemo/DialectsCYKDemo/Program.fs
+++ Examples/CYK/DialectsCYKDemo/DialectsCYKDemo/Program.fs
@@ -4,13 +4,11 @@
         seq { 
             while not buf.IsPastEndOfStream do
             let t = Lexer.token buf
-            yield  t
+            yield t
         }
         |> Yard.Generators.CYK.CodeTokenStream
-        |> Seq.filter ((<>) -1)
-        |> Seq.map (fun x -> uint16 x)
-        |> Array.ofSeq
+        
     let cyk = new Yard.Generators.CYKGenerator.CYKCore()
     cyk.Recognize (new ResizeArray<_>(Yard.Generators.CYK.rules),Yard.Generators.CYK.StartNTerm) tokens (fun x y z -> 0uy)
-
-do printfn "%s" <| run "9+"
+    
+do printfn "%s" <| run "9++"

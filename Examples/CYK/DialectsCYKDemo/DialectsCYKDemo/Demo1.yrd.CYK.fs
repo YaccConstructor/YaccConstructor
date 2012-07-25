7586bea7cc905e0f96fd62862c1db1a63b97cba6
Alisa Bakaeva <alisa.bakaeva@gmail.com>
2012-07-25 18:38:05 +0400

    Подготовлено демострационное приложение.

--- Examples/CYK/DialectsCYKDemo/DialectsCYKDemo/Demo1.yrd.CYK.fs
+++ Examples/CYK/DialectsCYKDemo/DialectsCYKDemo/Demo1.yrd.CYK.fs
@@ -5,18 +5,23 @@ open Yard.Generators.CYKGenerator
 type cykToken = 
   | EOF
   | NUM
+  | BAR
+  | STAR
   | PLUS
   | MULT
 let getTag token = 
   match token with 
   | EOF -> 0us
   | NUM -> 1us
-  | PLUS -> 2us
-  | MULT -> 3us
+  | BAR -> 2us
+  | STAR -> 3us
+  | PLUS -> 4us
+  | MULT -> 5us
 let rules = 
   [
-  281483566841856UL; 562954248388608UL; 1125908496777216UL; 1407387768455424UL;
-  844442110132224UL; 844446405099520UL; 844442110066688UL ]
+  281483566841856UL; 281479271677952UL; 1125908496777472UL; 1125912791745024UL;
+  1407392063422464UL; 1407396358389760UL; 1407383473488128UL; 1407387768455680UL;
+  844446405033984UL; 562954248388608UL ]
   |> Array.ofList
 let StartNTerm = 1
 let CodeTokenStream (stream:seq<CYKToken<cykToken,_>>) = 

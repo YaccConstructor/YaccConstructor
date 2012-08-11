9af139807ea2351a029077ccd8e12b0d8f1cd772
Alisa Bakaeva <alisa.bakaeva@gmail.com>
2012-08-11 19:01:20 +0400

    Чтение текста из файла, имя файла - аргумент командной строки.

--- Examples/CYK/DialectsCYKDemo/SQLSelectDemo/Program.fs
+++ Examples/CYK/DialectsCYKDemo/SQLSelectDemo/Program.fs
@@ -1,5 +1,11 @@
-﻿let run inStr =
-    let  buf = Lexing.LexBuffer<_>.FromString inStr 
+﻿let mutable path = None
+let commandLineSpecs =
+    ["-i", ArgType.String (fun s ->
+                                path <- Some s ), "Input text"         
+    ] |> List.map (fun (shortcut, argtype, description) -> ArgInfo(shortcut, argtype, description))
+
+let run input =
+    let  buf = Lexing.LexBuffer<_>.FromTextReader input 
     let tokens =
         seq { 
             while not buf.IsPastEndOfStream do
@@ -13,9 +19,16 @@
 
 let time () =    
     let start = System.DateTime.Now
-    let res = 
-        run("SELECT a AS b FROM table")
-    printfn "%s" res
-    printfn "%A" (System.DateTime.Now - start)
+    ArgParser.Parse commandLineSpecs
+    match path with
+    | Some v -> 
+        let res = 
+            run(new System.IO.StreamReader(v))
+        printfn "%s" res
+        printfn "%A" (System.DateTime.Now - start)
+    | _ -> 
+        printfn "%s" "Не задано имя входного файла."
+    
 do 
     time ()
+    System.Console.ReadKey()

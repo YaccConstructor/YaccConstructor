05089352ae69efa7c303c70b4987b9ea8054e44f
rsdpisuy <rsdpisuy@gmail.com>
2012-08-27 19:11:03 +0400

    Small code cleanup

--- YaccConstructor/Statistics/Statistics.fs
+++ YaccConstructor/Statistics/Statistics.fs
@@ -51,8 +51,8 @@ let RuleLength (def:Yard.Core.IL.Definition.t<_,_>) =
         |> List.averageBy(float)
     
     let max = List.max lengths
-    
-    let distribution = 
+
+    let distribution =
         lengths
         |> Seq.groupBy id
         |> Seq.map (fun (gn,gv) -> seq [string gn;Seq.length gv |> string])
@@ -60,7 +60,7 @@ let RuleLength (def:Yard.Core.IL.Definition.t<_,_>) =
     let report = new Report("grammarReport.txt")
 
     let reportBody =
-        ["Average: " + string avg |> RStr 
+        ["Average: " + string avg |> RStr
         ; "Max: " + string avg |> RStr
         ; Table distribution |> RTable]
     report.Body <- reportBody

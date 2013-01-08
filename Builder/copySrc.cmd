0ccaadd98830a54333559c92762cc6dd5eb6610e
rsdpisuy <rsdpisuy@gmail.com>
2013-01-08 17:21:08 +0400

    Copy 3rdParty folder.

--- Builder/copySrc.cmd
+++ Builder/copySrc.cmd
@@ -5,6 +5,14 @@ mkdir .\workdir\src
 
 xcopy /s /i /Y ..\YaccConstructor .\workdir\src
 
+if  exist .\workdir\3rdParty (
+    rd /s /q .\workdir\3rdParty )
+
+mkdir .\workdir\3rdParty
+
+xcopy /s /i /Y ..\3rdParty .\workdir\3rdParty
+
+
 cd scripts
 
 fsi prepare.fsx

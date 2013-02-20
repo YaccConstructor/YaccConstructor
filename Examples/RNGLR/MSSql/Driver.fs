192b3d1a9db34989d166d7b1582fd6ab7251cf5e
ragozina.anastasiya <ragozina.anastasiya@gmail.com>
2013-02-20 18:02:37 +0400

    Parse all directory

--- Examples/RNGLR/MSSql/Driver.fs
+++ Examples/RNGLR/MSSql/Driver.fs
@@ -51,10 +51,9 @@ let Parse (srcFilePath:string) =
         //printfn "%A" translated
         //translated.Head  
         
-  //let ParseAllDirectory (directoryName:string) =
-  //let files : string array = System.IO.Dyrectory.GetFiles(directoryName)  
-  //for i in 0 .. files.Length - 1 do
-  //Parse Array.get files i 
+  let ParseAllDirectory (directoryName:string) =
+  System.IO.Dyrectory.GetFiles directoryName
+  |> Array.iter Parse
   
   //do ParseAllDirecrory @"..\..\..\..\..\Tests\Materials\ms-sql\sqlsrvanalysissrvcs
 

409345fc7d24d4b333814c35f71594b1b85346ba
rsdpisuy <rsdpisuy@gmail.com>
2013-03-02 13:30:07 +0400

    Headers.

--- YaccConstructor/RNGLRAbstractParser/RNGLRAbstractParser.fs
+++ YaccConstructor/RNGLRAbstractParser/RNGLRAbstractParser.fs
@@ -1,2 +1,21 @@
-﻿module Yard.Generators.RNGLR.Abstract.Parser
+﻿//  RNGLRAbstractParser.fs contains implementation of interpreter for abstract parser based on RNGLR.
+//
+//  Copyright 2013 Semyon Grigorev <rsdpisuy@gmail.com>
+//
+//  This file is part of YaccConctructor.
+//
+//  YaccConstructor is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module Yard.Generators.RNGLR.Abstract.Parser
 

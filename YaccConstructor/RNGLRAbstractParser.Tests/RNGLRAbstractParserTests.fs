a19f40cec12265919f3398b996e9b58d2d5929e0
rsdpisuy <rsdpisuy@gmail.com>
2013-03-02 13:04:48 +0400

    Basic tests for graph loading.

--- YaccConstructor/RNGLRAbstractParser.Tests/RNGLRAbstractParserTests.fs
+++ YaccConstructor/RNGLRAbstractParser.Tests/RNGLRAbstractParserTests.fs
@@ -1,25 +1,36 @@
-﻿// Learn more about F# at http://fsharp.net
-
-module RNGLRAbstractParserTests
+﻿module RNGLRAbstractParserTests
 
 open Graphviz4Net.Dot.AntlrParser
 open System.IO
 open Graphviz4Net.Dot
 open QuickGraph
+open NUnit.Framework
 
-
-let graph filePath = 
+let loadGraphFromDOT filePath = 
     let parser = AntlrParserAdapter<string>.GetParser()
     parser.Parse(new StreamReader(File.OpenRead filePath))
 
-let g = graph @"D:\projects\string-embedded-languages-analysis-platform\Tests\Materials\DOTGraphs\str\_NumSeq.dot"
-let l = g.Edges |> Seq.map(fun e -> (e :?> DotEdge<string>).Label)
+let baseInputGraphsPath = "../../../../Tests/AbstractRNGLR/DOT"
 
-let qGraph = new AdjacencyGraph<int, TaggedEdge<_,string>>()
+[<TestFixture>]
+type ``RNGLR abstract parser tests`` () =
+    let path name = System.IO.Path.Combine(baseInputGraphsPath,name)
+    [<Test>]
+    member this.``Load graph test from DOT`` () =
+        let g = loadGraphFromDOT(path "IFExists_lex.dot")
+        Assert.AreEqual(g.Edges |> Seq.length, 29)
+        Assert.AreEqual(g.Vertices |> Seq.length, 25)
 
-g.Edges |> Seq.iter(fun e -> let edg = e :?> DotEdge<string> in qGraph.AddEdge(new TaggedEdge<_,_>(int edg.Source.Id,int edg.Destination.Id,edg.Label)) |> ignore)
-
-printfn "%A" l
-
-let f = 56
-printfn "12431423"
+    [<Test>]
+    member this.``Load graph test from DOT to QuickGraph`` () =
+        let g = loadGraphFromDOT(path "IFExists_lex.dot")
+        let qGraph = new AdjacencyGraph<int, TaggedEdge<_,string>>()
+        g.Edges 
+        |> Seq.iter(
+            fun e -> 
+                let edg = e :?> DotEdge<string>
+                qGraph.AddVertex(int edg.Source.Id) |> ignore
+                qGraph.AddVertex(int edg.Destination.Id) |> ignore
+                qGraph.AddEdge(new TaggedEdge<_,_>(int edg.Source.Id,int edg.Destination.Id,edg.Label)) |> ignore)
+        Assert.AreEqual(qGraph.Edges |> Seq.length, 29)
+        Assert.AreEqual(qGraph.Vertices |> Seq.length, 25)

ea4ca8d24baa05e950632a283f7e05f31f36f1c6
rsdpisuy <rsdpisuy@gmail.com>
2013-03-02 10:46:28 +0400

    RNGLRAbstractParser basic structure. It is not my dream to rewrite ready infrastructure from F# to Java, Scala or other JVM-based language only for experiments. YC is good platform for "grammarware research". If you are ready to port YC to JVM -- welcome!

---
+++ YaccConstructor/RNGLRAbstractParser.Tests/RNGLRAbstractParserTests.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module RNGLRAbstractParserTests
+
+open Graphviz4Net.Dot.AntlrParser
+open System.IO
+open Graphviz4Net.Dot
+open QuickGraph
+
+
+let graph filePath = 
+    let parser = AntlrParserAdapter<string>.GetParser()
+    parser.Parse(new StreamReader(File.OpenRead filePath))
+
+let g = graph @"D:\projects\string-embedded-languages-analysis-platform\Tests\Materials\DOTGraphs\str\_NumSeq.dot"
+let l = g.Edges |> Seq.map(fun e -> (e :?> DotEdge<string>).Label)
+
+let qGraph = new AdjacencyGraph<int, TaggedEdge<_,string>>()
+
+g.Edges |> Seq.iter(fun e -> let edg = e :?> DotEdge<string> in qGraph.AddEdge(new TaggedEdge<_,_>(int edg.Source.Id,int edg.Destination.Id,edg.Label)) |> ignore)
+
+printfn "%A" l
+
+let f = 56
+printfn "12431423"

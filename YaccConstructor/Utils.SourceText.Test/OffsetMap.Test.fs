d500e89b8f79e19514201046e62733a7bc85eaaf
2x2.4raymon <2x2.4raymon@gmail.com>
2013-03-09 00:52:00 +0400

    Ready Offset Map and tests

---
+++ YaccConstructor/Utils.SourceText.Test/OffsetMap.Test.fs
@@ -0,0 +1,110 @@
+﻿module Yard.Utils.OffsetMap.Test
+
+open System
+open System.IO
+open NUnit.Framework
+
+[<TestFixture>]
+type UtilsTest () =
+    [<Test>]
+    member test.``Test for random values of line and column`` () =
+        let map : int[] = [| 0; 30; 44; 88; 116; 274; 333; 448; 511; 690; 820; 1042 |]
+        let line = 5
+        let column = 20
+        let offset = Yard.Utils.OffsetMap.getAbsoluteOffset map line column
+        let actual = Yard.Utils.OffsetMap.getCoordinates map offset
+        Assert.AreEqual(line, actual.Line)
+        Assert.AreEqual(column, actual.Column)
+
+    [<Test>]
+    member test.``Test for the extreme lower values of line`` () =
+        let map : int[] = [| 0; 30; 44; 88; 116; 274; 333; 448; 511; 690; 820; 1042 |]
+        let line = 1
+        let column = 25
+        let offset = Yard.Utils.OffsetMap.getAbsoluteOffset map line column
+        let actual = Yard.Utils.OffsetMap.getCoordinates map offset
+        Assert.AreEqual(line, actual.Line)
+        Assert.AreEqual(column, actual.Column)
+
+    [<Test>]
+    member test.``Test for the extreme upper values of line`` () =
+        let map : int[] = [| 0; 30; 44; 88; 116; 274; 333; 448; 511; 690; 820; 1042 |]
+        let line = 11
+        let column = 23
+        let offset = Yard.Utils.OffsetMap.getAbsoluteOffset map line column
+        let actual = Yard.Utils.OffsetMap.getCoordinates map offset
+        Assert.AreEqual(line, actual.Line)
+        Assert.AreEqual(column, actual.Column)
+
+    [<Test>]
+    member test.``Test for the extreme lower values of column`` () =
+        let map : int[] = [| 0; 30; 44; 88; 116; 274; 333; 448; 511; 690; 820; 1042 |]
+        let line = 4
+        let column = 1
+        let offset = Yard.Utils.OffsetMap.getAbsoluteOffset map line column
+        let actual = Yard.Utils.OffsetMap.getCoordinates map offset
+        Assert.AreEqual(line, actual.Line)
+        Assert.AreEqual(column, actual.Column)
+
+    [<Test>]
+    member test.``Test for the extreme upper values of column`` () =
+        let map : int[] = [| 0; 30; 44; 88; 116; 274; 333; 448; 511; 690; 820; 1042 |]
+        let line = 4
+        let column = 27 
+        let offset = Yard.Utils.OffsetMap.getAbsoluteOffset map line column
+        let actual = Yard.Utils.OffsetMap.getCoordinates map offset
+        Assert.AreEqual(line, actual.Line)
+        Assert.AreEqual(column, actual.Column)
+
+    [<Test>]
+    member test.``Test for the extreme lower values of line and column`` () =
+        let map : int[] = [| 0; 30; 44; 88; 116; 274; 333; 448; 511; 690; 820; 1042 |]
+        let line = 1
+        let column = 1
+        let offset = Yard.Utils.OffsetMap.getAbsoluteOffset map line column
+        let actual = Yard.Utils.OffsetMap.getCoordinates map offset
+        Assert.AreEqual(line, actual.Line)
+        Assert.AreEqual(column, actual.Column)
+
+    [<Test>]
+    member test.``Test for the extreme upper values of line and column`` () =
+        let map : int[] = [| 0; 30; 44; 88; 116; 274; 333; 448; 511; 690; 820; 1042 |]
+        let line = 11
+        let column = 130
+        let offset = Yard.Utils.OffsetMap.getAbsoluteOffset map line column
+        let actual = Yard.Utils.OffsetMap.getCoordinates map offset
+        Assert.AreEqual(line, actual.Line)
+        Assert.AreEqual(column, actual.Column)
+
+    [<Test>]
+    member test.``Test for line and column in received map`` () =
+        let basePass = @"../../../../Tests/SourceText/Map.txt"
+        let map : int[] = Yard.Utils.OffsetMap.getMap basePass
+        let line = 6
+        let column = 5
+        let offset = Yard.Utils.OffsetMap.getAbsoluteOffset map line column
+        let actual = Yard.Utils.OffsetMap.getCoordinates map offset
+        Assert.AreEqual(line, actual.Line)
+        Assert.AreEqual(column, actual.Column) 
+
+    [<Test>]
+    member test.``Test for get map with empty line`` () =
+        try
+            let basePass = @"../../../../Tests/SourceText/Map.txt"
+            let map : int[] = Yard.Utils.OffsetMap.getMap basePass
+            let line = 4
+            let column = 5
+            let offset = Yard.Utils.OffsetMap.getAbsoluteOffset map line column
+            let actual = Yard.Utils.OffsetMap.getCoordinates map offset
+            Assert.AreEqual(line, actual.Line)
+            Assert.AreEqual(column, actual.Column)
+        with
+        | :? _ as ex -> ()
+
+    [<Test>]
+    member test.``Test for get right map`` () =
+        let basePass = @"../../../../Tests/SourceText/LittleMap.txt"
+        let map : int[] = Yard.Utils.OffsetMap.getMap basePass
+        let actual = [| 0; 10; 11; 28; 29; 43; 48 |]
+        Assert.AreEqual(map, actual)
+
---
+++ YaccConstructor/Utils.SourceText/OffsetMap.fs
@@ -0,0 +1,68 @@
+﻿module Yard.Utils.OffsetMap
+
+open System
+open System.IO
+
+type Coord = {Line : int; Column : int}
+
+let getMap path = 
+    try
+        let lines = System.IO.File.ReadLines path
+        let sum = ref 0
+        let res = seq { yield 0
+                        yield! (lines |> Seq.map(fun str -> 
+                                                        let r = sum.Value + str.Length + 1
+                                                        sum := r
+                                                        r))                                                   
+                      }
+        res |> Seq.toArray
+    with
+    | :? DriveNotFoundException
+    | :? DirectoryNotFoundException
+        -> printfn "Unhandled Drive or Directory not found exception"
+           [||]
+    | :? FileNotFoundException as ex
+        -> printfn "Unhandled FileNotFoundException: %s" ex.Message
+           [||]
+    | _ as ex
+        -> printfn "Unhandled Exception: %s" ex.Message
+           [||]
+
+
+let getAbsoluteOffset (map : array<_>) line column =
+    if line > map.Length - 1  || line < 1 then
+        failwith ("Value of Line is out of Map's range")
+    if column > map.[line] - map.[line - 1] - 1 || column < 1 then
+        failwith ("Column is out of Map's range")
+    let res = Some(map.[line - 1] + column - 1)
+    if res |> Option.isSome
+    then
+        res |> Option.get
+    else
+        failwith ("There is no such line or column")       
+        
+               
+let rec getLine (map : array<_>) offset left right = 
+    if offset > map.[right] - 1 then
+        failwith ("Offset is too big")
+    let center = (left + right) / 2
+    if offset >= map.[center] && offset < map.[center + 1] 
+    then
+        center
+    else
+        if offset < map.[center]
+        then
+            getLine map offset left center
+        else
+            getLine map offset center right
+
+
+let getCoordinates (map : array<_>) offset = 
+    let inLine = getLine map offset 0 (map.Length - 1) + 1
+    let inColumn = offset - map.[inLine - 1] + 1
+    let res = Some({Line = inLine; Column = inColumn})
+    if res |> Option.isSome 
+    then
+        res |> Option.get
+    else
+        failwith ("Offset is out of Map's range") 

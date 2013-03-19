b2fbd98485a4e10138682acb07685141fcd249d8
Ragozina Anastasiya <ragozina.anastasiya@gmail.com>
2013-03-19 19:17:15 +0400

    small changes

--- Examples/RNGLR/MSSql.Test/ParserTests.fs
+++ Examples/RNGLR/MSSql.Test/ParserTests.fs
@@ -117,7 +117,7 @@ type ``MS-SQL parser tests`` () =
         complexSpFile "sp_password.sql" |> runParserTest
 
     
-    //[<Test>]
+    [<Test>]
     member test.``sp_revokedbaccess complex test.`` () =
         complexSpFile "sp_revokedbaccess.sql" |> runParserTest
 

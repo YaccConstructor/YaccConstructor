afc680c5a901edd15744a02d658819182f135947
rsdpisuy <rsdpisuy@gmail.com>
2013-03-30 12:38:37 +0400

    First attempt to create FsYard ms-build task.

---
+++ YaccConstructor/FsYARD/FsYARD.fs
@@ -0,0 +1,21 @@
+﻿open Microsoft.FSharp.Text
+
+let generate inFile =
+    let fe = new Yard.Frontends.YardFrontend.YardFrontend()
+    let be = new Yard.Generators.RNGLR.RNGLR()
+    fe.ParseGrammar inFile |> be.Generate |> ignore
+
+let () =
+    let userDefs = ref []
+    let userDefsStr = ref ""
+    let inFile = ref None
+    let commandLineSpecs =
+        [
+         "-D", ArgType.String (fun s -> userDefs := !userDefs @ [s]), "User defined constants for YardFrontend lexer."
+         "-U", ArgType.String (fun s -> userDefs := List.filter ((<>) s) !userDefs), 
+                "Remove previously defined constants for YardFrontend lexer."
+         "-i", ArgType.String (fun s -> inFile := s|> Some), "Input grammar"
+         ] |> List.map (fun (shortcut, argtype, description) -> ArgInfo(shortcut, argtype, description))
+    ArgParser.Parse commandLineSpecs
+    generate inFile
+
---
+++ YaccConstructor/FsYARD/FsYARDBuildTask.fs
@@ -0,0 +1,30 @@
+﻿module Yard.Build
+
+open Microsoft.Build.Framework
+
+[<Class>] 
+type SetEnv() =
+    let mutable engine = Unchecked.defaultof<IBuildEngine>
+    let mutable host = Unchecked.defaultof<ITaskHost>
+    let mutable items =  Array.empty<ITaskItem>
+
+    [<Required>]
+    member this.InputFiles
+        with get () = items
+        and set v = items <- v
+
+    interface ITask with
+        override this.Execute() =          
+            let message = sprintf "Items: %A" items
+            let args = new BuildMessageEventArgs(message, "", "SetEnvironmentVariable", MessageImportance.Normal)
+            engine.LogMessageEvent(args)
+            System.IO.File.WriteAllText("D:/projects/YC/recursive-ascent/YaccConstructor/FsYARD/tttt", message)
+            true
+            
+        override this.HostObject
+            with get () = host
+            and set v = host <- v
+
+        override this.BuildEngine
+            with get () = engine
+            and set v =  engine <- v

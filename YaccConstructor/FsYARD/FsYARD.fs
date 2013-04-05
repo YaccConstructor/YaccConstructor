7005efd8f94875cdcb24d61700f7016372c7c213
rsdpisuy <rsdpisuy@gmail.com>
2013-04-06 01:46:46 +0400

    FsYard task. In progress.

--- YaccConstructor/FsYARD/FsYARD.fs
+++ YaccConstructor/FsYARD/FsYARD.fs
@@ -1,11 +1,33 @@
 ﻿module Yard.FsYard
 
 open Microsoft.FSharp.Text
+open Yard.Core.Conversions
+open Yard.Core.IL
 
-let generate inFile ycArgs rnglrArgs =
+let generate inFile replLit rnglrArgs =
     let fe = new Yard.Frontends.YardFrontend.YardFrontend()
     let be = new Yard.Generators.RNGLR.RNGLR()
-    fe.ParseGrammar inFile |> be.Generate |> ignore
+    let applyConversion (conv:Yard.Core.Conversion) parameters (ilTree:Definition.t<Source.t,Source.t>)  = 
+      {new Definition.t<Source.t,Source.t>
+        with info = ilTree.info
+        and  head = ilTree.head
+        and  grammar = conv.ConvertGrammar (ilTree.grammar, parameters)
+        and  foot = ilTree.foot
+        and options = ilTree.options
+      }
+    let conversions =
+        [
+            new ExpandMeta.ExpandMeta() :> Yard.Core.Conversion ,[||]
+            new ExpandEbnfStrict.ExpandEbnf() :> Yard.Core.Conversion ,[||] 
+            new ReplaceLiterals.ReplaceLiterals() :> Yard.Core.Conversion ,[|replLit|]
+            new ExpandInnerAlt.ExpandInnerAlt() :> Yard.Core.Conversion ,[||]
+            new ExpandBrackets.ExpandBrackets() :> Yard.Core.Conversion ,[||]
+            new LeaveLast.LeaveLast() :> Yard.Core.Conversion ,[||]
+        ]
+        |> List.map (fun (x,y) -> fun (il:Definition.t<_,_>) -> System.IO.File.WriteAllText("D:/projects/yc/recursive-ascent/YaccConstructor/FsYARD/сссс"+x.Name,il.grammar.ToString()+y.ToString());applyConversion x y il)
+    fe.ParseGrammar inFile 
+    |> (fun il -> List.fold (fun il conv -> conv il) il conversions)
+    |>  fun il -> System.IO.File.WriteAllText("D:/projects/yc/recursive-ascent/YaccConstructor/FsYARD/сссс",il.grammar.ToString() + "\n" + rnglrArgs); be.Generate(il,rnglrArgs) |> ignore
 
 let () =
     let userDefs = ref []
@@ -19,5 +41,5 @@ let () =
          "-i", ArgType.String (fun s -> inFile := s|> Some), "Input grammar"
          ] |> List.map (fun (shortcut, argtype, description) -> ArgInfo(shortcut, argtype, description))
     ArgParser.Parse commandLineSpecs
-    generate inFile 1 1
+    generate inFile "" ""
 
--- YaccConstructor/FsYARD/FsYARDBuildTask.fs
+++ YaccConstructor/FsYARD/FsYARDBuildTask.fs
@@ -6,15 +6,18 @@ open Microsoft.Build.Framework
 type FsYard() =
     let mutable engine = Unchecked.defaultof<IBuildEngine>
     let mutable host = Unchecked.defaultof<ITaskHost>
+
     let mutable items = Array.empty<ITaskItem>
+
     let mutable moduleName = ""
     let mutable tokenType = ""
     let mutable fullPath = false
     let mutable positionType = "Microsoft.FSharp.Text.Lexing.Position"
     let mutable needTranslate = true
-    let mutable light = true
+    let mutable light = "on"
     let mutable printInfiniteEpsilonPath = ""
     let mutable output = ""
+    let mutable replLiterals = ""
 
     [<Required>]
     member this.InputFiles
@@ -25,12 +28,48 @@ type FsYard() =
         with get() = output
         and set v = output <- v
 
+    member this.Light
+        with get() = light
+        and set v = light <- v
+
+    member this.ModuleName
+        with get() = moduleName
+        and set v = moduleName <- v
+
+    member this.NeedTranslate
+        with get() = needTranslate
+        and set v = needTranslate <- v
+
+    member this.TokenType
+        with get() = tokenType
+        and set v = tokenType <- v
+
+    member this.EpsilonPath
+        with get() = printInfiniteEpsilonPath
+        and set v = printInfiniteEpsilonPath <- v
+
+    member this.ReplLiterals
+        with get() = replLiterals
+        and set v = replLiterals <- v
+
+    member this.FullPath
+        with get() = fullPath
+        and set v = fullPath <- v
+
     interface ITask with
         override this.Execute() =          
-            let message = sprintf "Items: %A" items
-            let args = new BuildMessageEventArgs(message, "", "SetEnvironmentVariable", MessageImportance.Normal)
-            engine.LogMessageEvent(args)
-            System.IO.File.WriteAllText("D:/projects/YC/recursive-ascent/YaccConstructor/FsYARD/tttt", message)
+            let rnglrArgs = 
+                if needTranslate then sprintf "-translate  %A " needTranslate else ""
+                + if tokenType.Trim() <> "" then  sprintf "-token %s " tokenType else ""
+                + if moduleName.Trim() <> "" then sprintf "-module %s " moduleName else ""
+                + if printInfiniteEpsilonPath.Trim() <> "" then sprintf "-infEpsPath %s " printInfiniteEpsilonPath else ""
+                + if light.Trim() <> "" then sprintf "-light %s " light else ""
+                + if output.Trim() <> "" then sprintf "-o %s " output else ""
+                + if fullPath then sprintf "-fullpath %A"  fullPath else ""
+            Yard.FsYard.generate (items.[0].ToString()) replLiterals rnglrArgs
+            //let args = new BuildMessageEventArgs(message, "", "SetEnvironmentVariable", MessageImportance.Normal)
+            //engine.LogMessageEvent(args)
+            //System.IO.File.WriteAllText("D:/projects/YC/recursive-ascent/YaccConstructor/FsYARD/tttt", message)
             true
             
         override this.HostObject

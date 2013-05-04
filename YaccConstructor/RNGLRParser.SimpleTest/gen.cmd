f53e53f1687c76c1fdae3f1735271b5c00129480
dimonbv@gmail.com <dimonbv@gmail.com>
2013-05-05 03:35:48 +0400

    Change test generator for RNGLR.

--- YaccConstructor/RNGLRParser.SimpleTest/gen.cmd
+++ YaccConstructor/RNGLRParser.SimpleTest/gen.cmd
@@ -3,17 +3,17 @@
 del log.txt
 
 for %%i in (Epsilon, Longest, InfEpsilon) do (
-    ..\Main\bin\Release\YaccConstructor.exe -i %%i.yrd -c ExpandEbnf -c ExpandMeta ^
+    ..\YaccConstructor\bin\Release\YaccConstructor.exe -i %%i.yrd -c ExpandEbnf -c ExpandMeta ^
         -g "RNGLRGenerator -pos int -token int -module RNGLR.Parse%%i -translate true -table LR -light on -o %%i.yrd.fs" >> log.txt
 )
 
 for %%i in (Order,Cond,Attrs,Calc,Counter,Cycle,Resolvers) do (
-    ..\Main\bin\Release\YaccConstructor.exe -i %%i.yrd ^
+    ..\YaccConstructor\bin\Release\YaccConstructor.exe -i %%i.yrd ^
         -g "RNGLRGenerator -pos int -token int -module RNGLR.Parse%%i -translate true -table LR -light on -o %%i.yrd.fs" >> log.txt
 )
 
 for %%i in (ComplexRightNull,Expr,First,List,SimpleRightNull) do (
-    ..\Main\bin\Release\YaccConstructor.exe -i %%i.yrd ^
+    ..\YaccConstructor\bin\Release\YaccConstructor.exe -i %%i.yrd ^
         -g "RNGLRGenerator -pos int -token int -module RNGLR.Parse%%i -translate false -table LR -light on -o %%i.yrd.fs" >> log.txt
 )
 

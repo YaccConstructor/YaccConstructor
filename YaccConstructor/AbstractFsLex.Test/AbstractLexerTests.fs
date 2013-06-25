eeaae2e60d2210b7062edead175aeb98d10420a8
rsdpisuy <rsdpisuy@gmail.com>
2013-06-25 13:16:09 +0400

    First simple tests for graph loader.

--- YaccConstructor/AbstractFsLex.Test/AbstractLexerTests.fs
+++ YaccConstructor/AbstractFsLex.Test/AbstractLexerTests.fs
@@ -1,4 +1,36 @@
-﻿namespace AbstractFsLex.Test
+﻿module AbstractFsLex.Test
 
-type Class1() = 
-    member this.X = "F#"
+open Graphviz4Net.Dot.AntlrParser
+open System.IO
+open Graphviz4Net.Dot
+open QuickGraph
+open NUnit.Framework
+
+let loadGraphFromDOT filePath = 
+    let parser = AntlrParserAdapter<string>.GetParser()
+    parser.Parse(new StreamReader(File.OpenRead filePath))
+
+let baseInputGraphsPath = "../../../../Tests/AbstractLexing/DOT"
+
+[<TestFixture>]
+type ``RNGLR abstract parser tests`` () =
+    let path name = System.IO.Path.Combine(baseInputGraphsPath,name)
+    [<Test>]
+    member this.``Load graph test from DOT`` () =
+        let g = loadGraphFromDOT(path "test_1.dot")
+        Assert.AreEqual(g.Edges |> Seq.length, 5)
+        Assert.AreEqual(g.Vertices |> Seq.length, 5)
+
+    [<Test>]
+    member this.``Load graph test from DOT to QuickGraph`` () =
+        let g = loadGraphFromDOT(path "test_1.dot")
+        let qGraph = new AdjacencyGraph<int, TaggedEdge<_,string>>()
+        g.Edges 
+        |> Seq.iter(
+            fun e -> 
+                let edg = e :?> DotEdge<string>
+                qGraph.AddVertex(int edg.Source.Id) |> ignore
+                qGraph.AddVertex(int edg.Destination.Id) |> ignore
+                qGraph.AddEdge(new TaggedEdge<_,_>(int edg.Source.Id,int edg.Destination.Id,edg.Label)) |> ignore)
+        Assert.AreEqual(qGraph.Edges |> Seq.length, 5)
+        Assert.AreEqual(qGraph.Vertices |> Seq.length, 5)

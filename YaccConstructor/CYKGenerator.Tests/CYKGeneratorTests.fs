e8ffd90b27e8944d50399608ae321f7a942da49f
rsdpisuy <rsdpisuy@gmail.com>
2013-07-02 21:42:32 +0400

    CYK geterator tests are fixed.

--- YaccConstructor/CYKGenerator.Tests/CYKGeneratorTests.fs
+++ YaccConstructor/CYKGenerator.Tests/CYKGeneratorTests.fs
@@ -82,7 +82,7 @@ type ``CYK generator tests`` () =
             ; "    let tag = getTag t.Tag"
             ; "    if tag <> 0us then Some tag else None)"
             ; "  |> Array.ofSeq"
-            ] |> String.concat "\r\n"
+            ] |> String.concat "\n"
 
         let code = generator.Generate il
         printfn "%s" expectedCode

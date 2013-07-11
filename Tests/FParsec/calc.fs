737d881e5c6e8818a5b0161e7f956b053f43effb
2x2.4raymon <2x2.4raymon@gmail.com>
2013-07-11 17:59:09 +0400

    Changes in generated calc.fs file

--- Tests/FParsec/calc.fs
+++ Tests/FParsec/calc.fs
@@ -1,21 +1,21 @@
 module calc
 
 open FParsec.Primitives
-let rec public expr   = yard_rule_binExpr_4  |>> fun (res) -> ( res )   
+let rec public expr   = yard_rule_binExpr_1  |>> fun (res) -> ( res )   
 
- and private yard_rule_binExpr_4   = term  >>= fun (l ) -> (many ( attempt(termOp  >>= fun (op ) -> (term  |>> fun (r) -> ( op,r ) )  )) |>> fun (r) -> (List.fold (fun l (op,r) -> op l r) l r ) )  
+ and private yard_rule_binExpr_1   = term  >>= fun (l ) -> (many ( attempt(termOp  >>= fun (op ) -> (term  |>> fun (r) -> ( op,r ) )  )) |>> fun (r) -> (List.fold (fun l (op,r) -> op l r) l r ) )  
 
  and private termOp   = (attempt (Lexer.pPLUS |>> fun (_) -> ( (+) )   )) <|> (Lexer.pMINUS |>> fun (_) -> ( (-) )   )
 
- and private term   = yard_rule_binExpr_5  |>> fun (res) -> ( res )   
+ and private term   = yard_rule_binExpr_2  |>> fun (res) -> ( res )   
 
- and private yard_rule_binExpr_5   = factor  >>= fun (l ) -> (many ( attempt(factorOp  >>= fun (op ) -> (factor  |>> fun (r) -> ( op,r ) )  )) |>> fun (r) -> (List.fold (fun l (op,r) -> op l r) l r ) )  
+ and private yard_rule_binExpr_2   = factor  >>= fun (l ) -> (many ( attempt(factorOp  >>= fun (op ) -> (factor  |>> fun (r) -> ( op,r ) )  )) |>> fun (r) -> (List.fold (fun l (op,r) -> op l r) l r ) )  
 
  and private factorOp   = (attempt (Lexer.pMULT |>> fun (_) -> ( ( * ) )   )) <|> (Lexer.pDIV |>> fun (_) -> ( (/) )   )
 
- and private factor   = yard_rule_binExpr_6  |>> fun (res) -> ( res )   
+ and private factor   = yard_rule_binExpr_3  |>> fun (res) -> ( res )   
 
- and private yard_rule_binExpr_6   = powExpr  >>= fun (l ) -> (many ( attempt(powOp  >>= fun (op ) -> (powExpr  |>> fun (r) -> ( op,r ) )  )) |>> fun (r) -> (List.fold (fun l (op,r) -> op l r) l r ) )  
+ and private yard_rule_binExpr_3   = powExpr  >>= fun (l ) -> (many ( attempt(powOp  >>= fun (op ) -> (powExpr  |>> fun (r) -> ( op,r ) )  )) |>> fun (r) -> (List.fold (fun l (op,r) -> op l r) l r ) )  
 
  and private powOp   = Lexer.pPOW |>> fun (_) -> ( ( ** ) )   
 

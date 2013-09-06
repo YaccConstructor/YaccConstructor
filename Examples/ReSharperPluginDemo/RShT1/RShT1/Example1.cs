9dd4ed219d34867dc7c366bb4f7698cddc0afa32
rsdpisuy <rsdpisuy@gmail.com>
2013-09-06 22:23:14 +0400

    Project for plugin demonstration.

---
+++ Examples/ReSharperPluginDemo/RShT1/RShT1/Example1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace RShT1
+{
+    class TSQLDEmo
+    {
+        private void Go(int cond)
+        {
+            var stmt = "drop procedure";
+            if (1 == cond)
+            {
+                Program.ExecuteImmediate(stmt + "prc2");
+            }
+            else
+            {
+                Program.ExecuteImmediate(stmt + "prc1");
+            }
+            Program.ExecuteImmediate("select x from y where" + "v > b" );
+            Program.Eval("123 +" + " 23 ** 3 +  14");
+            Program.Eval("123 +" + " 14");
+            Program.Eval("123 +" + " -14");
+            var add = "+ 2 ";
+            if (cond < 1)
+            {
+                add = "+";
+            }
+            Program.Eval("123 +(" + " 14)" + add);
+        }
+    }
+}

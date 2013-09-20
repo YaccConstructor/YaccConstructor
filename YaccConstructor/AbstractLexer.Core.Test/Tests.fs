55af85a56656f5c56cc6c1c2210fa24dcee0e904
rsdpisuy <rsdpisuy@gmail.com>
2013-09-20 20:40:55 +0400

    First implementation of epsilon closure for parser input graph.

---
+++ YaccConstructor/AbstractLexer.Core.Test/Tests.fs
@@ -0,0 +1,21 @@
+﻿module AbstractLexer.Core.Test
+
+open AbstractParsing.Common
+
+
+type Class1() = 
+    let graph = new ParserInputGraph<_>()
+    let init () =
+        [
+          new ParserEdge<_>(0,1,None)
+          new ParserEdge<_>(0,1,Some 3)
+          new ParserEdge<_>(0,1,Some 2)
+        ]
+        |> graph.AddVerticesAndEdgeRange
+    member this.X () = 
+        init()
+        EpsClosure.NfaToDfa graph
+        
+let c = new Class1()
+let r = c.X()
+printfn "%A" r.Edges

e68a5e3d8f0614d3b745b1a82fa8ef5b5dbee7e5
gsv <rsdpisuy@gmail.com>
2013-10-14 18:18:21 +0400

    Small build fixes.

--- YaccConstructor/YardFrontend/gen.cmd
+++ YaccConstructor/YardFrontend/gen.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-..\YaccConstructor_min_base\bin\Release\YaccConstructor_min_base.exe  -f FsYaccFrontend -i Parser.fsy ^
+..\..\bin\Release\v45\YaccConstructor_min_base.exe  -f FsYaccFrontend -i Parser.fsy ^
     -g "RNGLRGenerator -o Parser.fs -module Yard.Frontends.YardFrontend.GrammarParser -pos Source.Position -token Source.t" > log.txt

e6c02d1ca0a2df90f078d9a6fd609ad6a581e59b
2x2.4raymon <2x2.4raymon@gmail.com>
2013-11-26 18:22:29 +0400

    Init new project

--- YaccConstructor/YardFrontend/Main.fs
+++ YaccConstructor/YardFrontend/Main.fs
@@ -164,7 +164,8 @@ let private parse buf userDefs =
                         (rangeToString (src.startPos, src.endPos)) msg src.text
     | Parser.Error (_, token, msg, debugs, _) -> 
         debugs.drawGSSDot "res.dot"
-        failwithf "Parse error on position %s on token %A: %s"  (token |> tokenToRange |> rangeToString) token msg
+        //todo
+        failwithf "Parse error on position %s on token %A: %s"  (token.[0] |> tokenToRange |> rangeToString) token msg
     
     
 let posTo2D (source:string) pos =    

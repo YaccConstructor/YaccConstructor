3f59c1788cb24d3c59fcac51e4f807ee3d07ef97
emavchun <emavchun@gmail.com>
2013-11-26 16:58:33 +0400

    JSON parser tests

--- Examples/AbstractAnalysis/AbstractAnalysisJSON/gen_lex.cmd
+++
@@ -1,4 +0,0 @@
-..\..\..\YaccConstructor\AbstractLexer.Generator\bin\Release\AbstractLexer.Generator.exe Lexer.fsl --unicode -o Lexer.fs
-
-..\..\..\YaccConstructor\YaccConstructor\bin\Release\YaccConstructor.exe -i JSON.yrd -c ExpandEbnf -c ExpandMeta ^
-        -g "RNGLRGenerator -o JSON.yrd.fs" >> log.txt

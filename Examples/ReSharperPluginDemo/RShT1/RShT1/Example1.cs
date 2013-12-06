8171f30c0b452f122bdd2f2e215daf2f6ec0764f
gsv <rsdpisuy@gmail.com>
2013-12-06 16:23:27 +0400

    TSQL parser is fixed.

--- Examples/ReSharperPluginDemo/RShT1/RShT1/Example1.cs
+++ Examples/ReSharperPluginDemo/RShT1/RShT1/Example1.cs
@@ -13,14 +13,16 @@ namespace RShT1
             var stmt = "drop procedure";
             if (1 == cond)
             {
-                Program.ExecuteImmediate(stmt + "prc2");
+                //Program.ExecuteImmediate(stmt + "prc2");
             }
             else
             {
-                Program.ExecuteImmediate(stmt + "prc1");
+                //Program.ExecuteImmediate(stmt + "prc1");
             }
+            var s = "insert into y (x,v) values (1,2)";
+            Program.ExecuteImmediate(s);
             Program.ExecuteImmediate("select x from y where" + "v > b" );
-            Program.Eval("123 +" + " 23 ** 3 +  14");
+            Program.Eval("123 ++" + " 23 ** 3 +  14");
             Program.Eval("123 +" + " 14");
             Program.Eval("123 +" + " -14");
             var add = "+ 2 ";

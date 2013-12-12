5d5e1a519af51a4cf0802edc3c3edf4cfbef4366
gsv <rsdpisuy@gmail.com>
2013-12-12 10:57:53 +0400

    ReSharper SDK from NuGet.

--- src/Calc/Calc.yrd.fs
+++ src/Calc/Calc.yrd.fs
@@ -25,7 +25,7 @@ type Token =
 
 let genLiteral (str : string) posStart posEnd =
     match str.ToLower() with
-    | x -> failwithf "Literal %%s undefined" x
+    | x -> failwithf "Literal %s undefined" x
 let tokenData = function
     | DIV x -> box x
     | ERROR x -> box x
@@ -47,15 +47,15 @@ let numToString = function
     | 5 -> "powOp"
     | 6 -> "term"
     | 7 -> "termOp"
-    | 8 -> "yard_exp_brackets_4"
-    | 9 -> "yard_exp_brackets_5"
-    | 10 -> "yard_exp_brackets_6"
-    | 11 -> "yard_many_1"
-    | 12 -> "yard_many_2"
-    | 13 -> "yard_many_3"
-    | 14 -> "yard_rule_binExpr_1"
-    | 15 -> "yard_rule_binExpr_2"
-    | 16 -> "yard_rule_binExpr_3"
+    | 8 -> "yard_exp_brackets_804"
+    | 9 -> "yard_exp_brackets_805"
+    | 10 -> "yard_exp_brackets_806"
+    | 11 -> "yard_many_89"
+    | 12 -> "yard_many_90"
+    | 13 -> "yard_many_91"
+    | 14 -> "yard_rule_binExpr_801"
+    | 15 -> "yard_rule_binExpr_802"
+    | 16 -> "yard_rule_binExpr_803"
     | 17 -> "yard_start_rule"
     | 18 -> "DIV"
     | 19 -> "ERROR"

4dffad61a1663b3942d0044c9c25e923f187f311
ivanovandrew <ivanovandrew2004@gmail.com>
2014-03-16 02:34:46 +0400

    added translate function in TSQL

---
+++ src/TSQL/gen_highlighting.cmd
@@ -0,0 +1,5 @@
+del log.txt
+
+rem echo mssql.yrd >> log.txt
+    ..\..\bin\Release\v%1\YaccConstructor.exe -i mssql.yrd -c ExpandEbnf -c Linearize -c "ReplaceLiterals KW_%%s"^
+        -g "RNGLRGenerator -pos array<Position<JetBrains.ReSharper.Psi.CSharp.Tree.ICSharpLiteralExpression>> -token SourceText*array<Position<JetBrains.ReSharper.Psi.CSharp.Tree.ICSharpLiteralExpression>> -module Yard.Examples.MSParser -translate true -highlighting true -namespace TSQLHighlighting -table LALR -o MSParser.fs" >> log.txt

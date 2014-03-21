3f7d45e8ba95ebbd92b95e23d41521c6b46c17b3
Марина <polubelovam@gmail.com>
2014-03-21 04:05:33 +0400

    Work version with List.

--- YaccConstructor/AbstractLexer.Core/Lexing.fs
+++ YaccConstructor/AbstractLexer.Core/Lexing.fs
@@ -44,8 +44,8 @@ type Position<'br> =
 [<Struct>]
 type StateInfo<'a, 'br> =
     val StartV: int
-    val AccumulatedString: ResizeArray<'a>
-    val Positions: ResizeArray<Position<'br>>
+    val AccumulatedString: list<'a> 
+    val Positions: list<Position<'br>>
     new (startV, str, positions) = {StartV = startV; AccumulatedString = str; Positions = positions}
 
 [<Struct>]
@@ -79,7 +79,7 @@ type LexBuffer<'char,'br>(inGraph:LexerInputGraph<'br>) =
         let g = new LexerInnerGraph<'br>(inGraph)
         let sorted = g.TopologicalSort() |> Array.ofSeq
         let states = Array.init ((Array.max sorted)+1) (fun _ -> new ResizeArray<_>())
-        let startState = new State<_,'br>(0,-1, ResizeArray.singleton (new StateInfo<_,'br>(0, new ResizeArray<_>(), new ResizeArray<_>())))
+        let startState = new State<_,'br>(0,-1, ResizeArray.singleton (new StateInfo<_,'br>(0, [], [])))
         do states.[g.StartVertex] <- ResizeArray.singleton startState
         let edgesSeq = seq{ for v in sorted do
                                 yield g.OutEdges v |> Array.ofSeq    
@@ -220,16 +220,18 @@ type UnicodeTables(trans: uint16[] array, accept: uint16[]) =
     let tokenize actions (lexbuf:LexBuffer<_,_>) printG =
         //let edgesSeq = edgesSeq |> Array.ofSeq
         let add (edg:LexerEdge<_,_>) (newStt:State<_,_>) =
-            match lexbuf.States.[edg.Target]
-                  |> ResizeArray.tryFind(fun (x:State<_,_>) -> x.AcceptAction = newStt.AcceptAction && x.StateID = newStt.StateID)
-                with
+            let x = 
+                lexbuf.States.[edg.Target]
+                |> ResizeArray.tryFind 
+                    (fun (x:State<_,_>) -> x.AcceptAction = newStt.AcceptAction && x.StateID = newStt.StateID)
+            match x with
             | Some x ->
                 newStt.Info
                 |> ResizeArray.iter(
                     fun i -> 
                         if x.Info.Exists(fun j -> j.StartV = i.StartV
-                                                  && i.AccumulatedString.Count = j.AccumulatedString.Count
-                                                  && ResizeArray.forall2 (=) i.AccumulatedString j.AccumulatedString) 
+                                                  && i.AccumulatedString.Length = j.AccumulatedString.Length
+                                                  && List.forall2 (=) i.AccumulatedString j.AccumulatedString) 
                            |> not
                         then x.Info.Add i)
             | None -> lexbuf.States.[edg.Target].Add newStt
@@ -250,7 +252,7 @@ type UnicodeTables(trans: uint16[] array, accept: uint16[]) =
                 | _ ->
                     {
                         pos_fname = ""
-                        pos_lnum = 0
+                        pos_lnum = 0 
                         pos_bol = 0
                         pos_cnum = 0
                         back_ref = edg.BackRef.Value
@@ -264,21 +266,21 @@ type UnicodeTables(trans: uint16[] array, accept: uint16[]) =
                     (
                         fun i ->
                             let pos = 
-                                if i.Positions.Count = 0 then None else Some i.Positions.[0] 
+                                if i.Positions.Length = 0 then None else Some i.Positions.[0] 
                                 |> newPos
                             new StateInfo<_,'br>((match stt.PreviousV with | Some x -> x | None -> i.StartV)
-                            , ResizeArray.concat [i.AccumulatedString; ResizeArray.singleton ch]
-                            , ResizeArray.concat [ResizeArray.singleton pos; i.Positions])
+                            , ch :: i.AccumulatedString
+                            , pos :: i.Positions)
                     )
             else 
                 new StateInfo<_,'br>((match stt.PreviousV with | Some x -> x | None -> edg.Source)
-                , ResizeArray.singleton ch
-                , ResizeArray.singleton (newPos (Some lexbuf.CurrentPos)))
+                , [ch]
+                , [newPos (Some lexbuf.CurrentPos)])
                 |> ResizeArray.singleton
 
         let processToken onAccept (p: StateInfo<_,_>) =
-            let s = (new string(p.AccumulatedString |> Array.ofSeq))
-            actions onAccept (new string(p.AccumulatedString |> Array.ofSeq)) (p.Positions |> Array.ofSeq |> Array.rev)                          
+            let s = (new string(p.AccumulatedString |> Array.ofList |> Array.rev))
+            actions onAccept s (p.Positions |> Array.ofSeq |> Array.rev)                          
 
         let processEdg (edg:LexerEdge<_,_>) stt reduced =
             let acc = new ResizeArray<_>(10) 
@@ -328,8 +330,6 @@ type UnicodeTables(trans: uint16[] array, accept: uint16[]) =
         //let r = newEdgs
         res.AddVerticesAndEdgeRange newEdgs
         |> ignore
-        //EpsClosure.NfaToDfa res
-        //|> ignore
         let eps_res = EpsClosure.NfaToDfa res
         eps_res
                           
@@ -354,8 +354,3 @@ type UnicodeTables(trans: uint16[] array, accept: uint16[]) =
         res
 
     static member Create(trans, accept) = new UnicodeTables(trans, accept)
-
-//    let query = "select \" f , " + (if x < 2 then "x" else "y") + "from z"
-//    let DB = new MS_DB("")
-//    let res = DB.exec query
-//    res |> Seq.iter (printfn "%A")

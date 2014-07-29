e74524e758b608f04674d3a7e0455c0601ae74f1
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-29 12:04:01 +0400

    New build version of YC.Core

--- src/YC.YaccConstructor/AssemblyInfo.fs
+++ src/YC.YaccConstructor/AssemblyInfo.fs
@@ -57,8 +57,8 @@ open System.Runtime.InteropServices
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.8.11")]
-[<assembly: AssemblyVersion("0.0.8.11")>]
-[<assembly: AssemblyFileVersion("0.0.8.11")>]
+// [assembly: AssemblyVersion("0.0.8.12")]
+[<assembly: AssemblyVersion("0.0.8.12")>]
+[<assembly: AssemblyFileVersion("0.0.8.12")>]
 
 do ()

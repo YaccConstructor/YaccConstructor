2d6a411699713ba13edbd0290f3a3056e842e9fc
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-29 19:53:26 +0400

    New build version of YC.Tools

--- src/YC.FsYacc/AssemblyInfo.fs
+++ src/YC.FsYacc/AssemblyInfo.fs
@@ -57,8 +57,8 @@ open System.Runtime.InteropServices
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.6.13")]
-[<assembly: AssemblyVersion("0.0.6.13")>]
-[<assembly: AssemblyFileVersion("0.0.6.13")>]
+// [assembly: AssemblyVersion("0.0.6.24")]
+[<assembly: AssemblyVersion("0.0.6.24")>]
+[<assembly: AssemblyFileVersion("0.0.6.24")>]
 
 do ()

402363b8c3a177d2f10ff8149527a58e62bc7488
yc.TeamCity <yc.teamcity@gmail.com>
2014-07-30 19:05:35 +0000

    Change version of package in AssemblyInfo and Nuspec files

--- src/FsYacc/AssemblyInfo.fs
+++ src/FsYacc/AssemblyInfo.fs
@@ -57,8 +57,8 @@ open System.Runtime.InteropServices
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.6.63")]
-[<assembly: AssemblyVersion("0.0.6.63")>]
-[<assembly: AssemblyFileVersion("0.0.6.63")>]
+// [assembly: AssemblyVersion("0.0.6.64")]
+[<assembly: AssemblyVersion("0.0.6.64")>]
+[<assembly: AssemblyFileVersion("0.0.6.64")>]
 
 do ()

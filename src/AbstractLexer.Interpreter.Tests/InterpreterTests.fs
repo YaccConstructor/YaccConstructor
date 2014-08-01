a67f432bc57be5b1fbd74f3446d7304e69686c78
Marina <polubelovam@gmail.com>
2014-08-01 14:48:13 +0400

    Happy end!!!!!!!!!!!!!

--- src/AbstractLexer.Interpreter.Tests/InterpreterTests.fs
+++ src/AbstractLexer.Interpreter.Tests/InterpreterTests.fs
@@ -23,10 +23,24 @@ type ``Lexer FST Tests`` () =
         let fstInputLexer = appr.ToFST()
         let resFST = FST<_,_>.Compos(fstInputLexer, fstLexer())
         resFST.PrintToDOT @"C:\recursive-ascent\src\AbstractLexer.Interpreter.Tests\Tests\test1.dot"
-                      
+
+    [<Test>]
+    member this.``Lexer FST Tests. Test 1.`` () = 
+        let startState = ResizeArray.singleton 0
+        let finishState = ResizeArray.singleton 3
+        let transitions = new ResizeArray<_>()
+        transitions.Add(0, Smb("+", "+"), 1)
+        transitions.Add(1, Smb("+", "+"), 2)
+        transitions.Add(2, Smb("*", "*"), 1)
+        transitions.Add(1, Smb("*", "*"), 3)
+        let appr = new Appr<_>(startState, finishState, transitions)
+        let fstInputLexer = appr.ToFST()
+        let resFST = FST<_,_>.Compos(fstInputLexer, fstLexer())
+        resFST.PrintToDOT @"C:\recursive-ascent\src\AbstractLexer.Interpreter.Tests\Tests\test2.dot"                      
+        Interpret(resFST)
 
 [<EntryPoint>]
 let f x =
       let t = new ``Lexer FST Tests`` () 
-      t.``Lexer FST Tests. Simple test.``()
+      t.``Lexer FST Tests. Test 1.``()
       1
--- src/AbstractLexer.Interpreter/Interpreter.fs
+++ src/AbstractLexer.Interpreter/Interpreter.fs
@@ -15,47 +15,68 @@ type StateInfo<'inSymb, 'br> =
     val StartV: int
     val AccumulatedString: ResizeArray<'inSymb> 
     val Positions: ResizeArray<Position<'br>>
-    val EndV: Option<int>
+    val mutable EndV: Option<int>
     new (startV, str, positions) = {StartV = startV; AccumulatedString = str; Positions = positions; EndV = None}
     new (startV, str, positions, endV) = {StartV = startV; AccumulatedString = str; Positions = positions; EndV = endV}
 
-let Interpret (inputFstLexer: FST<_,_>) =
+let Interpret (inputFstLexer: FST<_,_>) =   
     let maxV = inputFstLexer.Vertices |> Seq.max |> ref
     let visited = ResizeArray.init (!maxV + 1) (fun _ -> false)
 
-    let stateInfo = ResizeArray.init (!maxV + 1) (fun _ -> new ResizeArray<StateInfo<_, _>>())
-
-    let dfs vertex =
-        let stackV = new Stack<_>()
-        visited.[vertex] <- true
-        stackV.Push(vertex)
+    let bfs vertex (stt: StateInfo<_,_>) =
+        let queueV = new Queue<_>()
+        queueV.Enqueue(vertex,stt)
         
         let NotEmpty x =
             if x > 0 then true else false
-    
-        let ExistNotVisitedVertex topV = 
-            inputFstLexer.OutEdges(topV) |> Seq.exists(fun x -> not <| visited.[x.Target])
-
-        let NotVisitedVertex topV = 
-            inputFstLexer.OutEdges(topV) |> Seq.find(fun x -> not <| visited.[x.Target]) 
-
-        let HandlingVertex topV v =
-            //stateInfo.[v] <- ResizeArray.singleton (new StateInfo<_,_>(v, ))
-            ()
-                       
-        while NotEmpty stackV.Count do 
-            let topV = stackV.Peek()
-            if ExistNotVisitedVertex topV 
+        
+                           
+        while NotEmpty queueV.Count do 
+            let flag = ref false
+            let topV,curStt = queueV.Dequeue()            
+            if not <| visited.[topV]
             then
-                let notVisitedEdge = NotVisitedVertex topV
-                //handling (topV; notVisitedVertex)
-                visited.[notVisitedEdge.Target] <- true
-                stackV.Push(notVisitedEdge.Target)
-            else 
-                stackV.Pop() |> ignore
+                visited.[topV] <- true
+                for v in inputFstLexer.OutEdges(topV) do                    
+                    if v.Tag.OutSymb = Eps 
+                    then   
+                        //добавляем информацию
+                        let newStt = new StateInfo<_,_>(curStt.StartV, ResizeArray.append curStt.AccumulatedString (ResizeArray.singleton v.Tag.InSymb), new ResizeArray<_>()) 
+                        queueV.Enqueue(v.Target, newStt)
+                    else
+                         flag := true
+                         //new StateInfo<_,_>(topV, ResizeArray.singleton v.Tag.InSymb, new ResizeArray<_>()) |> ignore
+
+                if !flag 
+                then
+                    printfn 
+                        "startV %i string %s" 
+                        curStt.StartV 
+                        (curStt.AccumulatedString 
+                            |> ResizeArray.map (function | Smbl x -> x.ToString() | Eps -> "Eps" | Exclosure y -> y.ToString())
+                            |> String.concat "")
+              
+                for v in inputFstLexer.OutEdges(topV) do                 
+                    if not <| (v.Tag.OutSymb = Eps)
+                    then 
+                        let newStt = new StateInfo<_,_>(topV, ResizeArray.singleton v.Tag.InSymb, new ResizeArray<_>()) 
+                        queueV.Enqueue(v.Target, newStt)
+
+
+            else
+                for v in inputFstLexer.OutEdges(topV) do                    
+                    if not <| (v.Tag.OutSymb = Eps)  
+                    then
+                        printfn 
+                            "startV %i string %s" 
+                            curStt.StartV 
+                            (curStt.AccumulatedString 
+                                |> ResizeArray.map (function | Smbl x -> x.ToString() | Eps -> "Eps" | Exclosure y -> y.ToString())
+                                |> String.concat "")
                  
     for v in inputFstLexer.InitState do
-        dfs v            
+        let stt = new StateInfo<_,_>(v, new ResizeArray<_>(), new ResizeArray<_>())
+        bfs v stt
          
     let res = new ParserInputGraph<_>()
     res

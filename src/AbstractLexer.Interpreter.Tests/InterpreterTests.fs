b71b8eb1cb2117fab06ca6c34354ce7d5db0fe3a
Marina <polubelovam@gmail.com>
2014-09-08 21:11:34 +0400

    Fix interpreter tests

--- src/AbstractLexer.Interpreter.Tests/InterpreterTests.fs
+++ src/AbstractLexer.Interpreter.Tests/InterpreterTests.fs
@@ -1,58 +1,13 @@
 ﻿module YC.FST.AbstractLexing.Tests.Interpreter
 
 open NUnit.Framework
+open Microsoft.FSharp.Collections
+open QuickGraph
+open AbstractAnalysis.Common
 open YC.FST.GraphBasedFst
-open Microsoft.FSharp.Collections 
-open QuickGraph 
-open YC.FST.AbstractLexing.FstLexer
 open YC.FST.FstApproximation
 open YC.FST.AbstractLexing.Interpreter
-open AbstractAnalysis.Common
-
-let eof = RNGLR_EOF("", [||])
-
-let printTok =
-     fun x -> string x  |> (fun s -> s.Split '+' |> Array.rev |> fun a -> a.[0])
-
-let printBref =
-    let printBrs brs =
-        "["
-        + (brs |> Array.map (fun (pos:Position<_>) -> pos.back_ref ) |> String.concat "; ")
-        + "]"        
-        + (brs |> Array.map (fun (pos:Position<_>) -> "(" + pos.start_offset.ToString() + ", " + pos.end_offset.ToString() + ")") |> String.concat "; ")         
-
-    fun x ->
-        match x with
-            | PLUS(v,br) -> "+: " + printBrs br
-            | MULT(v,br) -> "*: " + printBrs br
-            | POW(v,br)  -> "**: " + printBrs br
-            | x -> string x  |> (fun s -> s.Split '+' |> Array.rev |> fun a -> a.[0]) 
-   
-
-let checkGraph (graph:AdjacencyGraph<_,_>) countE countV  =
-    Assert.AreEqual(graph.EdgeCount, countE, "Count of edges not equal expected number. ")
-    Assert.AreEqual(graph.VertexCount, countV, "Count of vertices not equal expected number. ")
-
-let checkArr expectedArr actualArr =
-    if Array.length expectedArr = Array.length actualArr
-    then 
-        Array.iteri2 (
-            fun i x1 x2 -> 
-            if x1 <> x2 then Assert.Fail ("Arrays differ at position: " + string i)) expectedArr actualArr
-        Assert.Pass()
-    else Assert.Fail ("Arrays have different length")
- 
-let positions (parserInputGraph : ParserInputGraph<_>) getVal =
-    parserInputGraph.Edges 
-        |> Seq.collect
-            (fun e -> 
-                match e.Tag with
-                    | PLUS(v,br)
-                    | MULT(v,br)
-                    | POW(v,br) ->
-                        br |> Array.map getVal
-                    | RNGLR_EOF _ -> [||])
-        |> Array.ofSeq
+open YC.FST.AbstractLexing.Tests.CommonTestChecker
          
 [<TestFixture>]
 type ``Lexer FST Tests`` () =            
@@ -67,7 +22,7 @@ type ``Lexer FST Tests`` () =
         transitions.Add(2, Smb("+", "+"), 3)
         let appr = new Appr<_>(startState, finishState, transitions)
         let fstInputLexer = appr.ToFST()
-        let resFST = FST<_,_>.Compos(fstInputLexer, fstLexer())
+        let resFST = FST<_,_>.Compos(fstInputLexer, YC.FST.AbstractLexing.FstLexer.fstLexer())
         resFST.PrintToDOT @"C:\recursive-ascent\src\AbstractLexer.Interpreter.Tests\Tests\test1.dot"
         checkGraph resFST 6 6
 
@@ -85,7 +40,7 @@ type ``Lexer FST Tests`` () =
         //let resFST = FST<_,_>.Compos(fstInputLexer, fstLexer())
         //resFST.PrintToDOT @"C:\recursive-ascent\src\AbstractLexer.Interpreter.Tests\Tests\test2.dot"                      
         //Interpret resFST (actions()) 
-        let parserInputGraph = tokenize eof appr
+        let parserInputGraph = YC.FST.AbstractLexing.FstLexer.tokenize eof appr
         ToDot parserInputGraph  @"C:\recursive-ascent\src\AbstractLexer.Interpreter.Tests\Tests\test2.dot" printBref
         checkGraph parserInputGraph 8 7
 
@@ -96,7 +51,7 @@ type ``Lexer FST Tests`` () =
         let transitions = new ResizeArray<_>()
         transitions.Add(0, Smb("++", "+1"), 1)
         let appr = new Appr<_>(startState, finishState, transitions)
-        let parserInputGraph = tokenize eof appr
+        let parserInputGraph = YC.FST.AbstractLexing.FstLexer.tokenize eof appr
         ToDot parserInputGraph  @"C:\recursive-ascent\src\AbstractLexer.Interpreter.Tests\Tests\test3.dot" printBref
         checkGraph parserInputGraph 3 4
         let startPos = positions parserInputGraph (fun p -> p.start_offset)
@@ -114,7 +69,7 @@ type ``Lexer FST Tests`` () =
         transitions.Add(0, Smb("+*", "+1"), 1)
         transitions.Add(1, Smb("**", "2"), 2)
         let appr = new Appr<_>(startState, finishState, transitions)
-        let parserInputGraph = tokenize eof appr
+        let parserInputGraph = YC.FST.AbstractLexing.FstLexer.tokenize eof appr
         ToDot parserInputGraph  @"C:\recursive-ascent\src\AbstractLexer.Interpreter.Tests\Tests\test4.dot" printBref
         checkGraph parserInputGraph 4 5
         let startPos = positions parserInputGraph (fun p -> p.start_offset)
@@ -133,7 +88,7 @@ type ``Lexer FST Tests`` () =
         transitions.Add(0, new EdgeLbl<_,_>(Eps, Eps), 1)
         transitions.Add(1, new EdgeLbl<_,_>(Eps, Smbl '*'), 1)
         let fst = new FST<_,_>(startState, finishState, transitions)
-        let resFST = FST<_,_>.Compos(fst, fstLexer())
+        let resFST = FST<_,_>.Compos(fst, YC.FST.AbstractLexing.FstLexer.fstLexer())
         resFST.PrintToDOT @"C:\recursive-ascent\src\AbstractLexer.Interpreter.Tests\Tests\testCompose.dot"   
         //let parserInputGraph = Interpret resFST (actions()) eof
         //ToDot parserInputGraph  @"C:\recursive-ascent\src\AbstractLexer.Interpreter.Tests\Tests\testComposeGraph.dot" printBref   
@@ -141,6 +96,6 @@ type ``Lexer FST Tests`` () =
 //[<EntryPoint>]
 //let f x =
 //      let t = new ``Lexer FST Tests`` () 
-//      let a = t.``Lexer FST Tests. Test with back_ref 2.``()
+//      let a = t.``Lexer FST Tests. Calc Lexer.``()
 //      printfn "%A" a      
 //      1

2d02d7b6a7afb78f3e423a338f5da0b8a4442ace
ivanovandrew <ivanovandrew2004@gmail.com>
2014-09-16 16:36:01 +0400

    Update version of YaccConstructor package in cmd files

--- src/RNGLRParser.SimpleTest/gen.cmd
+++ src/RNGLRParser.SimpleTest/gen.cmd
@@ -4,20 +4,20 @@ del log.txt
 for %%i in (Epsilon, Longest, InfEpsilon) do (
 		echo . >> log.txt
 		echo %%i >> log.txt
-    ..\packages\YaccConstructor.0.0.8.11\tools\YaccConstructor\YaccConstructor.exe -i %%i.yrd -c ExpandEbnf -c ExpandMeta ^
+    ..\packages\YaccConstructor.0.0.8.12\tools\YaccConstructor\YC.YaccConstructor.exe -i %%i.yrd -c ExpandEbnf -c ExpandMeta ^
         -g "RNGLRGenerator -pos int -token int -module RNGLR.Parse%%i -translate true -table LR -o %%i.yrd.fs" >> log.txt
 )
 
 for %%i in (Order,Cond,Attrs,Calc,Counter,Cycle,LongCycle,Resolvers, LolCalc, Omit) do (
 		echo . >> log.txt
 		echo %%i >> log.txt
-    ..\packages\YaccConstructor.0.0.8.11\tools\YaccConstructor\YaccConstructor.exe -i %%i.yrd ^
+    ..\packages\YaccConstructor.0.0.8.12\tools\YaccConstructor\YC.YaccConstructor.exe -i %%i.yrd ^
         -g "RNGLRGenerator -pos int -token int -module RNGLR.Parse%%i -translate true -table LR -o %%i.yrd.fs" >> log.txt
 )
 
 for %%i in (ComplexRightNull,Expr,First,List,SimpleRightNull) do (
 		echo . >> log.txt
 		echo %%i >> log.txt
-    ..\packages\YaccConstructor.0.0.8.11\tools\YaccConstructor\YaccConstructor.exe -i %%i.yrd ^
+    ..\packages\YaccConstructor.0.0.8.12\tools\YaccConstructor\YC.YaccConstructor.exe -i %%i.yrd ^
         -g "RNGLRGenerator -pos int -token int -module RNGLR.Parse%%i -translate false -table LR -o %%i.yrd.fs" >> log.txt
 )

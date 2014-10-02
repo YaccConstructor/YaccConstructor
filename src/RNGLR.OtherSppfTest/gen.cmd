9d7afa0df59e2a895a80c22c1bf93f3a8c62ed5c
rsdpisuy <rsdpisuy@gmail.com>
2014-10-02 15:01:21 +0400

    Projects references instead of packages. not finished. Compilable.

--- src/RNGLR.OtherSppfTest/gen.cmd
+++ src/RNGLR.OtherSppfTest/gen.cmd
@@ -4,7 +4,7 @@ del log1.txt
 for %%i in (Elementary, Ambiguous, Cycles, Summator) do (
 	echo.  >> log1.txt
 	echo %%i >> log1.txt
-	..\packages\YaccConstructor.0.0.8.12\tools\YaccConstructor\YC.YaccConstructor.exe  -i %%i.yrd ^
+	..\..\Bin\Release\v40\YC.YaccConstructor.exe  -i %%i.yrd ^
         -g "RNGLRGenerator -pos int -token int -module RNGLR.Parse%%i -translate false -highlighting true -namespace %%i -light true -o %%i.yrd.fs" >> log1.txt
 )
          

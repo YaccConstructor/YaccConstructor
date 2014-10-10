f31a5556d9b3c36e2ee46344ab773b510a208588
rsdpisuy <rsdpisuy@gmail.com>
2014-10-10 20:00:04 +0400

    Solution folders

--- src/YC.ReSharper.AbstractAnalysis.Plugin.Helper/Common.fs
+++ src/YC.ReSharper.AbstractAnalysis.Plugin.Helper/Common.fs
@@ -1,7 +1,4 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-
-module YC.EL.ReSharper.Common
+﻿module YC.EL.ReSharper.Common
 
 open JetBrains.ReSharper.Psi.Tree
 open JetBrains.ReSharper.Psi.CSharp.Tree

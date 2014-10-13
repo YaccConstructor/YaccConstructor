c938e4ffbd7c10f37cb0a363a6385128fba22368
Marina <polubelovam@gmail.com>
2014-10-13 19:33:32 +0400

    change lexer for Calc -- graph token value

--- src/Calc/gen_highlighting.cmd
+++ src/Calc/gen_highlighting.cmd
@@ -2,4 +2,4 @@ del log.txt
 
 echo calc.yrd >> log.txt
     ..\..\Bin\Release\v40\YC.YaccConstructor.exe -f YardFrontend -i calc.yrd -c ExpandEbnf -c ExpandMeta ^
-        -g "RNGLRGenerator -pos uint64 -token string*array<Position<JetBrains.ReSharper.Psi.CSharp.Tree.ICSharpLiteralExpression>> -module Calc.AbstractParser -translate true -highlighting true -namespace CalcHighlighting -table LALR -o Calc.yrd.fs" >> log.txt
+        -g "RNGLRGenerator -pos uint64 -module Calc.AbstractParser -translate true -highlighting true -namespace CalcHighlighting -table LALR -o Calc.yrd.fs" >> log.txt

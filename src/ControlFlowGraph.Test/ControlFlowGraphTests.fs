c2856411c3b0fde34aa12c3c204fb36dda8c8607
ivanovandrew <ivanovandrew2004@gmail.com>
2014-11-20 16:27:59 +0300

    Merge https://code.google.com/p/recursive-ascent into cfg
    Added simple control flow graph

---
+++ src/ControlFlowGraph.Test/ControlFlowGraphTests.fs
@@ -0,0 +1,318 @@
+﻿module ControlFlowGraphTests
+
+open AbstractAnalysis.Common
+open QuickGraph
+open Yard.Generators.RNGLR
+open Yard.Generators.RNGLR.Parser
+open Yard.Generators.RNGLR.AbstractParser
+open ControlFlowGraph
+open NUnit.Framework
+
+let createEdge from _to label = new ParserEdge<_>(from, _to, label)
+
+let inline printErr (num, token : 'a, msg) =
+    printfn "Error in position %d on Token %A: %s" num token msg
+    Assert.Fail(sprintf "Error in position %d on Token %A: %s" num token msg)
+
+let tokenToPos (tokenData : _ -> obj) token = 
+    let t = tokenData token
+    match t with
+    | :? int as i -> [i] |> Seq.ofList
+    | _ -> failwith ""
+
+[<TestFixture>]
+type ``Control Flow Graph Building`` () =
+
+    [<Test>]
+    member test.``Elementary test``() =
+        let buildAbstractAst = RNGLR.ParseExtendedCalc.buildAstAbstract
+        let tokenToNumber = RNGLR.ParseExtendedCalc.tokenToNumber
+        let leftSides = RNGLR.ParseExtendedCalc.leftSide
+        let indToString = RNGLR.ParseExtendedCalc.numToString
+        let tokenData = RNGLR.ParseExtendedCalc.tokenData
+
+        let semicolon = RNGLR.ParseExtendedCalc.SEMICOLON 0
+        let semicolonNumber = tokenToNumber semicolon
+        let nodeToType = dict["assign", Assignment;]
+        
+        let typeToDelimiters = dict [
+                                        Assignment, [semicolonNumber]; 
+                                    ]
+        
+        let stmntNumber = 0
+        
+        let parserSource = new ParserSource<RNGLR.ParseExtendedCalc.Token>(tokenToNumber, indToString, leftSides, tokenData)
+        let langSource = new LanguageSource(nodeToType, typeToDelimiters)
+
+        let qGraph = new ParserInputGraph<_>()
+        qGraph.AddVertexRange[0; 1; 2; 3; 4; (*5; 6; 7; 8; 9; 10; 11; 12;*)] |> ignore
+        qGraph.AddVerticesAndEdgeRange
+            [
+                createEdge 0 1 (RNGLR.ParseExtendedCalc.X 0)
+                createEdge 1 2 (RNGLR.ParseExtendedCalc.EQ 1)
+                createEdge 2 3 (RNGLR.ParseExtendedCalc.ONE 2)
+                createEdge 3 4 (RNGLR.ParseExtendedCalc.SEMICOLON 3)
+                createEdge 4 5 (RNGLR.ParseExtendedCalc.Y 4)
+                createEdge 5 6 (RNGLR.ParseExtendedCalc.EQ 5)
+                createEdge 6 7 (RNGLR.ParseExtendedCalc.TWO 6)
+                createEdge 7 8 (RNGLR.ParseExtendedCalc.SEMICOLON 7)
+                createEdge 8 9 (RNGLR.ParseExtendedCalc.Z 8)
+                createEdge 9 10 (RNGLR.ParseExtendedCalc.EQ 9)
+                createEdge 10 11 (RNGLR.ParseExtendedCalc.THREE 10)
+                createEdge 11 12 (RNGLR.ParseExtendedCalc.SEMICOLON 11)
+            ] |> ignore
+
+        let parseResult = (new Parser<_>()).Parse buildAbstractAst qGraph
+        
+        match parseResult with 
+        | Parser.Error (num, tok, err, _, _) -> printErr (num, tok, err)
+        | Parser.Success (mAst, _, _) ->
+            RNGLR.ParseExtendedCalc.defaultAstToDot mAst "elementary.dot"
+            let cfg = ControlFlow (mAst, parserSource, langSource, mAst.Tokens)
+            cfg.PrintToDot "elementary cfg.dot"
+
+    [<Test>]
+    member test.``Simple If test``() =
+        let buildAbstractAst = RNGLR.ParseIf.buildAstAbstract
+        let tokenToNumber = RNGLR.ParseIf.tokenToNumber
+        let leftSides = RNGLR.ParseIf.leftSide
+        let indToString = RNGLR.ParseIf.numToString
+        let tokenData = RNGLR.ParseIf.tokenData
+
+        let semicolon = RNGLR.ParseIf.SEMICOLON 0
+        let semicolonNumber = tokenToNumber semicolon
+        let nodeToType = dict[
+                                "simple_statement", Assignment;
+                                "if_statement", IfStatement;
+                            ]
+        let ifNumber = tokenToNumber <| RNGLR.ParseIf.IF 0
+        let thenNumber = tokenToNumber <| RNGLR.ParseIf.THEN 0
+        let elseNumber = tokenToNumber <| RNGLR.ParseIf.ELSE 0
+        let endIfNumber = tokenToNumber <| RNGLR.ParseIf.ENDIF 0
+
+        let typeToDelimiters = dict [
+                                        Assignment, [semicolonNumber]; 
+                                        IfStatement, [ifNumber; thenNumber; elseNumber; endIfNumber];
+                                    ]
+
+        let parserSource = new ParserSource<RNGLR.ParseIf.Token>(tokenToNumber, indToString, leftSides, tokenData)
+        let langSource = new LanguageSource(nodeToType, typeToDelimiters, elseNumber, endIfNumber)
+
+        let qGraph = new ParserInputGraph<_>()
+        qGraph.AddVertexRange[0; 1; 2; 3; 4; 5; 6; 7; 8; 9; (*10; 11; 12;*)] |> ignore
+        qGraph.AddVerticesAndEdgeRange
+            [
+                createEdge 0 1 (RNGLR.ParseIf.IF 0)
+                createEdge 1 2 (RNGLR.ParseIf.A 1)
+                createEdge 2 3 (RNGLR.ParseIf.THEN 2)
+                createEdge 3 4 (RNGLR.ParseIf.B 3)
+                createEdge 4 5 (RNGLR.ParseIf.SEMICOLON 4)
+                createEdge 5 6 (RNGLR.ParseIf.ELSE 5)
+                createEdge 6 7 (RNGLR.ParseIf.E 6)
+                createEdge 7 8 (RNGLR.ParseIf.SEMICOLON 7)
+                createEdge 8 9 (RNGLR.ParseIf.ENDIF 8)
+                createEdge 9 10 (RNGLR.ParseIf.F 9)
+                createEdge 10 11 (RNGLR.ParseIf.SEMICOLON 10)
+            ] |> ignore
+
+        let parseResult = (new Parser<_>()).Parse buildAbstractAst qGraph
+        
+        match parseResult with 
+        | Parser.Error (num, tok, err, _, _) -> printErr (num, tok, err)
+        | Parser.Success (mAst, _, _) ->
+            RNGLR.ParseIf.defaultAstToDot mAst "simple if-tree.dot"
+            let cfg = ControlFlow (mAst, parserSource, langSource, mAst.Tokens)
+            
+            cfg.PrintToDot "simple if-cfg.dot"
+            printfn "%s" <| cfg.ToString()
+
+    [<Test>]
+    member test.``Big If test``() =
+        let buildAbstractAst = RNGLR.ParseIf.buildAstAbstract
+        let tokenToNumber = RNGLR.ParseIf.tokenToNumber
+        let leftSides = RNGLR.ParseIf.leftSide
+        let indToString = RNGLR.ParseIf.numToString
+        let tokenData = RNGLR.ParseIf.tokenData
+
+        let semicolon = RNGLR.ParseIf.SEMICOLON 0
+        let semicolonNumber = tokenToNumber semicolon
+        let nodeToType = dict[
+                                "simple_statement", Assignment;
+                                "if_statement", IfStatement;
+                            ]
+        let ifNumber = tokenToNumber <| RNGLR.ParseIf.IF 0
+        let thenNumber = tokenToNumber <| RNGLR.ParseIf.THEN 0
+        let elseNumber = tokenToNumber <| RNGLR.ParseIf.ELSE 0
+        let endIfNumber = tokenToNumber <| RNGLR.ParseIf.ENDIF 0
+
+        let typeToDelimiters = dict [
+                                        Assignment, [semicolonNumber]; 
+                                        IfStatement, [ifNumber; thenNumber; elseNumber; endIfNumber];
+                                    ]
+
+        let parserSource = new ParserSource<RNGLR.ParseIf.Token>(tokenToNumber, indToString, leftSides, tokenData)
+        let langSource = new LanguageSource(nodeToType, typeToDelimiters, elseNumber, endIfNumber)
+
+        let qGraph = new ParserInputGraph<_>()
+        qGraph.AddVertexRange[0; 1; 2; 3; 4; 5; 6; 7; 8; 9; (*10; 11; 12;*)] |> ignore
+        qGraph.AddVerticesAndEdgeRange
+            [
+                createEdge 0 1 (RNGLR.ParseIf.IF 0)
+                createEdge 1 2 (RNGLR.ParseIf.A 1)
+                createEdge 2 3 (RNGLR.ParseIf.THEN 2)
+                createEdge 3 4 (RNGLR.ParseIf.B 3)
+                createEdge 4 5 (RNGLR.ParseIf.SEMICOLON 4)
+                createEdge 5 6 (RNGLR.ParseIf.C 5)
+                createEdge 6 7 (RNGLR.ParseIf.SEMICOLON 6)
+                createEdge 7 8 (RNGLR.ParseIf.ELSE 7)
+                createEdge 8 9 (RNGLR.ParseIf.D 8)
+                createEdge 9 10 (RNGLR.ParseIf.SEMICOLON 9)
+                createEdge 10 11 (RNGLR.ParseIf.E 10)
+                createEdge 11 12 (RNGLR.ParseIf.SEMICOLON 11)
+                createEdge 12 13 (RNGLR.ParseIf.ENDIF 12)
+                createEdge 13 14 (RNGLR.ParseIf.F 13)
+                createEdge 14 15 (RNGLR.ParseIf.SEMICOLON 14)
+            ] |> ignore
+
+        let parseResult = (new Parser<_>()).Parse buildAbstractAst qGraph
+        
+        match parseResult with 
+        | Parser.Error (num, tok, err, _, _) -> printErr (num, tok, err)
+        | Parser.Success (mAst, _, _) ->
+            RNGLR.ParseIf.defaultAstToDot mAst "big if-tree.dot"
+            let cfg = ControlFlow (mAst, parserSource, langSource, mAst.Tokens)
+            
+            cfg.PrintToDot "big if-cfg.dot"
+            printfn "%s" <| cfg.ToString()
+
+    [<Test>]
+    member test.``If without else test``() =
+        let buildAbstractAst = RNGLR.ParseIf.buildAstAbstract
+        let tokenToNumber = RNGLR.ParseIf.tokenToNumber
+        let leftSides = RNGLR.ParseIf.leftSide
+        let indToString = RNGLR.ParseIf.numToString
+        let tokenData = RNGLR.ParseIf.tokenData
+
+        let semicolon = RNGLR.ParseIf.SEMICOLON 0
+        let semicolonNumber =  tokenToNumber semicolon
+        let nodeToType = dict[
+                                "simple_statement", Assignment;
+                                "if_statement", IfStatement;
+                            ]
+        let ifNumber = tokenToNumber <| RNGLR.ParseIf.IF 0
+        let thenNumber = tokenToNumber <| RNGLR.ParseIf.THEN 0
+        let elseNumber = tokenToNumber <| RNGLR.ParseIf.ELSE 0
+        let endIfNumber = tokenToNumber <| RNGLR.ParseIf.ENDIF 0
+
+        let typeToDelimiters = dict [
+                                        Assignment, [semicolonNumber]; 
+                                        IfStatement, [ifNumber; thenNumber; elseNumber; endIfNumber];
+                                    ]
+
+        let parserSource = new ParserSource<RNGLR.ParseIf.Token>(tokenToNumber, indToString, leftSides, tokenData)
+        let langSource = new LanguageSource(nodeToType, typeToDelimiters, elseNumber, endIfNumber)
+
+        let qGraph = new ParserInputGraph<_>()
+        qGraph.AddVertexRange[0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10;] |> ignore
+        qGraph.AddVerticesAndEdgeRange
+            [
+                createEdge 0 1 (RNGLR.ParseIf.IF 0)
+                createEdge 1 2 (RNGLR.ParseIf.A 1)
+                createEdge 2 3 (RNGLR.ParseIf.THEN 2)
+                createEdge 3 4 (RNGLR.ParseIf.B 3)
+                createEdge 4 5 (RNGLR.ParseIf.SEMICOLON 4)
+                createEdge 5 6 (RNGLR.ParseIf.C 5)
+                createEdge 6 7 (RNGLR.ParseIf.SEMICOLON 6)
+                createEdge 7 8 (RNGLR.ParseIf.ENDIF 7)
+                createEdge 8 9 (RNGLR.ParseIf.F 8)
+                createEdge 9 10 (RNGLR.ParseIf.SEMICOLON 9)
+            ] |> ignore
+
+        let parseResult = (new Parser<_>()).Parse buildAbstractAst qGraph
+        
+        match parseResult with 
+        | Parser.Error (num, tok, err, _, _) -> printErr (num, tok, err)
+        | Parser.Success (mAst, _, _) ->
+            RNGLR.ParseIf.defaultAstToDot mAst "if-without-else-tree.dot"
+            let cfg = ControlFlow (mAst, parserSource, langSource, mAst.Tokens)
+            
+            cfg.PrintToDot "if_statement.dot"
+            printfn "%s" <| cfg.ToString()
+            
+    [<Test>]
+    member test.``Inner if``() =
+        let buildAbstractAst = RNGLR.ParseIf.buildAstAbstract
+        let tokenToNumber = RNGLR.ParseIf.tokenToNumber
+        let leftSides = RNGLR.ParseIf.leftSide
+        let indToString = RNGLR.ParseIf.numToString
+        let tokenData = RNGLR.ParseIf.tokenData
+
+        let semicolon = RNGLR.ParseIf.SEMICOLON 0
+        let semicolonNumber = tokenToNumber semicolon
+        let nodeToType = dict[
+                                "if_statement", IfStatement;
+                                "simple_statement", Assignment;
+                            ]
+        let ifNumber = tokenToNumber <| RNGLR.ParseIf.IF 0
+        let thenNumber = tokenToNumber <| RNGLR.ParseIf.THEN 0
+        let elseNumber = tokenToNumber <| RNGLR.ParseIf.ELSE 0
+        let endIfNumber = tokenToNumber <| RNGLR.ParseIf.ENDIF 0
+        
+        let typeToDelimiters = dict [
+                                        Assignment, [semicolonNumber]; 
+                                        IfStatement, [ifNumber; thenNumber; elseNumber; endIfNumber];
+                                    ]
+
+        let parserSource = new ParserSource<RNGLR.ParseIf.Token>(tokenToNumber, indToString, leftSides, tokenData)
+        let langSource = new LanguageSource(nodeToType, typeToDelimiters, elseNumber, endIfNumber)
+
+        let qGraph = new ParserInputGraph<_>()
+        let vertices = Array.init 23 (fun i -> i)
+        qGraph.AddVertexRange vertices |> ignore
+        qGraph.AddVerticesAndEdgeRange
+            [
+                createEdge 0 1  (RNGLR.ParseIf.IF 0)
+                createEdge 1 2  (RNGLR.ParseIf.A 1)
+                createEdge 2 3  (RNGLR.ParseIf.THEN 2)
+                createEdge 3 4  (RNGLR.ParseIf.IF 3)
+                createEdge 4 5  (RNGLR.ParseIf.B 4)
+                createEdge 5 6  (RNGLR.ParseIf.THEN 5)
+                createEdge 6 7  (RNGLR.ParseIf.C 6)
+                createEdge 7 8  (RNGLR.ParseIf.SEMICOLON 7)
+                createEdge 8 9  (RNGLR.ParseIf.ELSE 8)
+                createEdge 9 10 (RNGLR.ParseIf.D 9)
+                createEdge 10 11(RNGLR.ParseIf.SEMICOLON 10)
+                createEdge 11 12(RNGLR.ParseIf.ENDIF 11)
+                createEdge 12 13(RNGLR.ParseIf.ELSE 12)
+                createEdge 13 14(RNGLR.ParseIf.IF 13)
+                createEdge 14 15(RNGLR.ParseIf.E 14)
+                createEdge 15 16(RNGLR.ParseIf.THEN 15)
+                createEdge 16 17(RNGLR.ParseIf.F 16)
+                createEdge 17 18(RNGLR.ParseIf.SEMICOLON 17)
+                createEdge 18 19(RNGLR.ParseIf.ELSE 18)
+                createEdge 19 20(RNGLR.ParseIf.G 19)
+                createEdge 20 21(RNGLR.ParseIf.SEMICOLON 20)
+                createEdge 21 22(RNGLR.ParseIf.ENDIF 21)
+                createEdge 22 23(RNGLR.ParseIf.ENDIF 22)
+            ] |> ignore
+
+        let parseResult = (new Parser<_>()).Parse buildAbstractAst qGraph
+        
+        match parseResult with 
+        | Parser.Error (num, tok, err, _, _) -> printErr (num, tok, err)
+        | Parser.Success (mAst, _, _) ->
+            RNGLR.ParseIf.defaultAstToDot mAst "Inner if.dot"
+            let cfg = ControlFlow (mAst, parserSource, langSource, mAst.Tokens)
+            
+            cfg.PrintToDot "Inner if cfg.dot"
+            printfn "%s" <| cfg.ToString()
+
+[<EntryPoint>]
+let f x = 
+    let cfgBuilding = new ``Control Flow Graph Building``()
+//    cfgBuilding.``Elementary test``()
+//    cfgBuilding.``Simple If test``()
+    cfgBuilding.``Big If test``()
+//    cfgBuilding.``If without else test``()
+//    cfgBuilding.``Inner if``()
+    1
---
+++ src/ControlFlowGraph/ControlFlowGraph.fs
@@ -0,0 +1,307 @@
+﻿module ControlFlowGraph
+
+open Yard.Generators.Common.AST
+
+type BlockType = 
+    | Assignment 
+    | Declaration 
+    | Definition
+    | IfStatement 
+    | ThenStatement 
+    | ElseStatement 
+    | ForStatement 
+    | WhileStatement
+    | Start
+    | None 
+
+type ParserSource<'TokenType> = 
+    val tokenToNumber : 'TokenType -> int
+    val leftSides : array<int>
+    val tokenData : 'TokenType -> obj
+    val numToString : int -> string
+
+    new (tokenToNumber, numToString, leftSides, tokenData) =
+        {
+            tokenToNumber = tokenToNumber;
+            numToString = numToString;
+            leftSides = leftSides;
+            tokenData = tokenData;
+        }
+
+type LanguageSource = 
+    val nodeToType : System.Collections.Generic.IDictionary<string, BlockType>
+    val typeToDelimiters: System.Collections.Generic.IDictionary<BlockType, int list>
+    val elseNumber : int
+    val endIfNumber : int
+
+    new (nodeToType, typeToDelimiters) = 
+        {
+            nodeToType = nodeToType; 
+            typeToDelimiters = typeToDelimiters;
+            elseNumber = -1;
+            endIfNumber = -1;
+        }
+
+    new (nodeToType, typeToDelimiters, elseNumber, endIfNumber) = 
+        {
+            nodeToType = nodeToType; 
+            typeToDelimiters = typeToDelimiters;
+            elseNumber = elseNumber;
+            endIfNumber = endIfNumber;
+        }
+
+type Block<'TokenType> = 
+    val blockType : BlockType
+    val values : 'TokenType array
+    val mutable children : InterNode<'TokenType>
+
+//    val mutable parents : UsualOne<InterNode<'TokenType>>
+
+    new (block, vals, children) = {blockType = block; values = vals; children = children(*; parents = Unchecked.defaultof<UsualOne<_>>*)}
+
+    override this.ToString() = 
+        let typeStr = 
+            match this.blockType with
+            | Assignment -> "Assignment"
+            | Declaration -> "Declaration"
+            | Definition -> "Definition"
+            | IfStatement -> "IfStatement"
+            | ThenStatement -> "ThenStatement"
+            | ElseStatement -> "ElseStatement"
+            | ForStatement -> "ForStatement"
+            | WhileStatement -> "WhileStatement"
+            | Start -> "Start"
+            | None -> "None"
+
+        let strValues = 
+            this.values
+            |> Array.map (fun t -> t.ToString())
+            |> String.concat ""
+
+        sprintf "%s\n tokens: %A\n" typeStr this.values //strValues
+
+    static member defaultBlock = Block<'TokenType>(None, [||], new InterNode<'TokenType>([]))
+
+//    static member addParent (parent : InterNode<'TokenType>) = 
+//        if this.parents.first = null 
+//        then 
+//            this.parents <- UsualOne (parent)
+//        else
+//            this.parent 
+
+and InterNode<'TokenType> =
+    val mutable children : Block<'TokenType> list
+    
+    new (children) = {children = children}
+
+    member this.AddChild(block) = 
+        if this.children.Length = 0 
+        then 
+            this.children <- [ block ]
+        else
+            this.children <- List.append <| this.children <| [ block ]
+
+type ControlFlow<'TokenType> (tree : Tree<'TokenType>, parserSource : ParserSource<'TokenType>, langSource : LanguageSource, input : array<'TokenType>) = 
+    let start, finish = 
+        let treeRoot = 
+            match tree.Root with
+            | :? AST as ast -> ast
+            | _ -> null
+
+        let createNewBlock tokens blockType = 
+            let toks = 
+                tokens
+                |> List.map (fun t -> tree.Tokens.[t])
+                |> List.toArray
+
+            let block = new Block<'TokenType>(blockType, toks, new InterNode<'TokenType>([]))
+            block
+
+        let concatBlocks (blocks : list<Block<_> * Block<_>>) = 
+            let startBlock = fst blocks.Head
+            let mutable finishBlock = snd blocks.Head
+                            
+            for first, last in blocks.Tail do
+                finishBlock.children.AddChild first
+                finishBlock <- last
+                            
+            startBlock, finishBlock
+
+        let createIfBlock (blocks : list<Block<_> * Block<_>>) = 
+            let outBlock = Block.defaultBlock
+
+            let ifBlock = fst blocks.Head
+            let startThenBlock, endThenBlock = blocks.Tail.Head
+
+            ifBlock.children.AddChild startThenBlock
+            
+            let elseExists = blocks.Length > 1
+            
+            if elseExists 
+            then
+                let startElseBlock, endElseBlock = List.nth blocks 2
+                ifBlock.children.AddChild startElseBlock
+
+                endElseBlock.children.AddChild outBlock
+
+            endThenBlock.children.AddChild outBlock
+
+            ifBlock, outBlock
+
+        let stack = System.Collections.Generic.Stack<_>()
+        let blockType = ref Start
+        let blocks = ref []
+        let tokens = ref []
+
+        let restoreContext() = 
+            let oldType, oldBlocks, oldTokens = stack.Pop()
+                        
+            blockType := oldType
+            blocks := oldBlocks
+            tokens := oldTokens
+        
+        let rec handleAst (ast : AST) = 
+            let family = ast.first
+
+            let handleFamily (family : Family) = 
+                let familyName = parserSource.leftSides.[family.prod] |> parserSource.numToString
+
+                let handle (node : obj) = 
+                    match node with 
+                    | :? int as t when t < 0 -> () 
+                    | :? int as t -> 
+                        let delimiters = langSource.typeToDelimiters.[!blockType]
+                        let tokNumber = parserSource.tokenToNumber input.[t]
+
+                        if List.exists (fun num -> num = tokNumber) delimiters
+                        then
+                            if not <| tokens.Value.IsEmpty 
+                            then
+                                
+                                let newBlock = createNewBlock <| List.rev !tokens <| !blockType
+                                tokens := []
+                                blocks := (newBlock, newBlock) :: !blocks
+                        else
+                            tokens := t :: !tokens
+
+                        if tokNumber = langSource.elseNumber
+                        then
+                            blocks := List.rev <| !blocks
+                            let head = blocks.Value.Head
+                            let thenBranch = concatBlocks blocks.Value.Tail
+                            blocks := [thenBranch; head;]
+                        elif tokNumber = langSource.endIfNumber
+                        then
+                            blocks := List.rev <| !blocks
+                            let head = blocks.Value.Head
+                            let thenBranch = blocks.Value.Tail.Head
+                            let elseBranch = concatBlocks blocks.Value.Tail.Tail
+
+                            blocks := [head; thenBranch; elseBranch]
+
+                    | :? AST as ast ->  
+                        let pairBlocks = handleAst ast
+                        if fst pairBlocks <> Unchecked.defaultof<Block<_>>
+                        then
+                            blocks := pairBlocks :: !blocks
+                    | _ -> failwithf "Unexpected AST node type in Control-Flow construction"
+
+                if langSource.nodeToType.ContainsKey familyName 
+                then 
+                    match langSource.nodeToType.[familyName] with
+                    | IfStatement -> 
+                        stack.Push (!blockType, !blocks, !tokens)
+                        blockType := IfStatement
+                        blocks := []
+                        tokens := []
+
+                        family.nodes.doForAll(fun node -> handle node)
+
+                        let temp = Array.ofList !blocks
+
+                        let ifBlock = createIfBlock !blocks
+                        restoreContext()
+                        
+                        ifBlock
+
+                    | Assignment -> 
+                        stack.Push (!blockType, !blocks, !tokens)
+                        blockType := Assignment
+                        blocks := []
+                        tokens := []
+
+                        family.nodes.doForAll(fun node -> handle node)
+                        
+                        blocks := List.rev !blocks
+                        let assignBlock = concatBlocks !blocks
+                        
+                        restoreContext()
+
+                        assignBlock
+                    | _ -> 
+                        Unchecked.defaultof<Block<_>>, Unchecked.defaultof<Block<_>>
+
+                else 
+                    family.nodes.doForAll (fun node -> handle node)
+                    Unchecked.defaultof<Block<_>>, Unchecked.defaultof<Block<_>>
+
+            handleFamily family
+
+        handleAst treeRoot |> ignore
+        
+        let result = concatBlocks <| List.rev blocks.Value
+        
+        result
+
+    member this.Start = start
+    member this.Finish = finish
+
+    member this.PrintToDot name = 
+        let count = ref 0
+        let blockToNumber = new System.Collections.Generic.Dictionary<_, _>()
+        let interNodeToNumber = new System.Collections.Generic.Dictionary<_, _>()
+        
+        use out = new System.IO.StreamWriter (name : string)
+        out.WriteLine("digraph AST {")
+
+        let rec printBlock (block : Block<'TokenType>) num = 
+            let isNew = ref true
+            let nodeNumber = 
+                if blockToNumber.ContainsKey block 
+                then 
+                    isNew := false
+                    blockToNumber.[block]
+                else
+                    incr count
+
+                    out.WriteLine (sprintf "%d [label=\"%s\",shape=box]" <| !count <| block.ToString())
+                    blockToNumber.Add (block, !count)
+                    !count
+
+            let interNodeNumber node = 
+                if interNodeToNumber.ContainsKey node 
+                then
+                    isNew := false
+                    interNodeToNumber.[node]
+                else
+                    incr count
+                    out.WriteLine (sprintf "%d [label=\"Node\"]" !count)
+                    interNodeToNumber.Add (node, !count)
+                    !count
+            
+            if num <> -1 
+            then out.WriteLine (sprintf ("%d -> %d") num nodeNumber)
+
+            let child = block.children
+            let childNumber = interNodeNumber child
+            out.WriteLine (sprintf ("%d -> %d") nodeNumber childNumber)
+
+            if !isNew 
+            then
+                for child in child.children do
+                    printBlock child childNumber
+
+        printBlock this.Start -1
+
+        out.WriteLine("}")
+        out.Close()

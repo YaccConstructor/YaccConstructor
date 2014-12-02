e2d61ebd2c7b80dac74f5ce796d35469174a6a06
ragozina.anastasiya <ragozina.anastasiya@gmail.com>
2014-12-02 17:42:06 +0300

    Add lexer and run program to test gll generator2

--- src/GLLGen/Program.fs
+++ src/GLLGen/Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 module GLLGen
+open GLL.Parse.Test
 let run () =
     let fe = new Yard.Frontends.YardFrontend.YardFrontend()
     let gen = new Yard.Generators.GLL2.GLL2()
@@ -8,3 +9,15 @@ let run () =
     gen.Generate(il,"-pos int -token int -module GLL.Parse.Test -o Test.yrd.fs")
 
 run () |> printfn "%A"
+
+let run1 input astBuilder =
+    let tokens = Lexer2.tokens(input)
+    astBuilder tokens
+
+let parser1 = GLL.Parse.Test.buildAst
+//let start = System.DateTime.Now
+//let str = Lexer2.tokens((String.init (1) (fun i -> "A ")).Trim())
+let str = Lexer2.tokens "A D B"
+let r = GLL.Parse.Test.buildAst str 
+r.Start
+

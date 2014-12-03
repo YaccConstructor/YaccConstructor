b9b543e17e76cf4c8e8ae33b4b1381ed4db9ce98
rsdpisuy <rsdpisuy@gmail.com>
2014-12-03 13:07:49 +0300

    Version up.

--- src/FsYacc/AssemblyInfo.fs
+++ src/FsYacc/AssemblyInfo.fs
@@ -57,8 +57,8 @@ open System.Runtime.InteropServices
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.6.106")]
-[<assembly: AssemblyVersion("0.0.6.106")>]
-[<assembly: AssemblyFileVersion("0.0.6.106")>]
+// [assembly: AssemblyVersion("0.0.6.107")]
+[<assembly: AssemblyVersion("0.0.6.107")>]
+[<assembly: AssemblyFileVersion("0.0.6.107")>]
 
 do ()

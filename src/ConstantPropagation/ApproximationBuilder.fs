850d26bab09876b8abf38c751b559116c8fd8c8d
Marat Khabibullin <maratx387@gmail.com>
2015-02-27 17:58:22 +0300

    General control flow graph interface boilplate has been created

---
+++ src/ConstantPropagation/ApproximationBuilder.fs
@@ -0,0 +1,21 @@
+﻿module YC.ReSharper.AbstractAnalysis.LanguageApproximation.ApproximationBuilder
+
+// Type representing control flow graph of source code
+type ControlFlowGraph = NotImplemented
+
+// Type representing control flow graph node 
+type ControlFlowGraphNode = NotImplemented
+
+// Enum representing control flow graph node type
+type ControlFlowGraphNodeType = NotImplemented=0
+
+// Find first node satisfying predicate "pred"
+let findFirst (cfgEntryElem: ControlFlowGraphNode) (pred: ControlFlowGraphNode -> bool)
+    : option<ControlFlowGraphNode> = 
+    None
+    
+// Builds subgraph of "node" node's ancestors omitting nodes not 
+// satisfying "nodePred" predicate (subgraph remains connected)
+let getAncestorsSubgraph (node: ControlFlowGraphNode) (nodePred: ControlFlowGraphNode -> bool)
+    : option<ControlFlowGraph> = 
+    None

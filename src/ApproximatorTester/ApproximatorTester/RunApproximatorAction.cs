2f421e42962223c176545ad959371d2fa7df7c27
Marat Khabibullin <maratx387@gmail.com>
2015-03-14 19:41:41 +0300

    Code to write out plugin test results added (needed to perform tests)

--- src/ApproximatorTester/ApproximatorTester/RunApproximatorAction.cs
+++ src/ApproximatorTester/ApproximatorTester/RunApproximatorAction.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.IO;
+using JetBrains.Application;
 using JetBrains.Application.Progress;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Bulbs;
 using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
 using JetBrains.ReSharper.Intentions.Extensibility;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.Tree;
+using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.TextControl;
 using JetBrains.Util;
 using YC.ReSharper.AbstractAnalysis.LanguageApproximation;
@@ -30,12 +36,39 @@ namespace ApproximatorTester
         {
             var inputFile = _provider.PsiFile;
             Approximator.BuildApproximation(inputFile);
+            OutputResults();
             return null;
         }
 
         public override string Text
         {
             get { return "Run Approximator"; }
+        }
+
+        private void OutputResults()
+        {
+            var firstToken = _provider.TokenAfterCaret;
+            var namespaceDecl = firstToken == null ? null : firstToken.Parent;
+            if (namespaceDecl == null)
+            {
+                const string msg = @"No namespace declaration after caret in test file. 
+                                    Possibly incorrect test input file format,
+                                    it must start with ""{caret}namespace""";
+                throw new Exception(msg);
+            }
+
+            var resultString = ResultToString();
+            var factory = CSharpElementFactory.GetInstance(_provider.PsiModule);
+            var commentWithResult = factory.CreateComment("/*\n" + resultString + "\n*/");
+            WriteLockCookie.Execute(() =>
+            {
+                ModificationUtil.ReplaceChild(namespaceDecl, commentWithResult);
+            });
+        }
+
+        private string ResultToString()
+        {
+            return "result";
         }
     }
 }

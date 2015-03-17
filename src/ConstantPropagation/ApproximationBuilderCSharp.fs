eaa5867c8caf09bb2d280708064923ffff11f26a
Marat Khabibullin <maratx387@gmail.com>
2015-03-17 19:49:06 +0300

    Comments removed

--- src/ConstantPropagation/ApproximationBuilderCSharp.fs
+++ src/ConstantPropagation/ApproximationBuilderCSharp.fs
@@ -70,22 +70,6 @@ let private createControlFlowGraph (hotspot: IInvocationExpression) =
     CSharpControlFlowBuilder.Build methodDeclaration
 
 let createAstCfgMap (cfg: ICSharpControlFlowGraf): Dictionary<ITreeNode, IControlFlowElement> =
-//    let rec dfs (elem: IControlFlowElement) (dict: Dictionary<ITreeNode, IControlFlowElement>) =
-//        if elem = null
-//        then dict
-//        else
-//            if elem.SourceElement <> null
-//            then dict.[elem.SourceElement] <- elem
-//            elem.Exits
-//            |> List.ofSeq
-//            |> List.map (fun rib -> rib.Target) 
-//            |> List.map (fun t -> dfs t dict)
-//            |> List.head
-//        
-//    let dict = new Dictionary<ITreeNode, IControlFlowElement>()
-//    dfs cfg.EntryElement dict
-
-
     let dict = new Dictionary<ITreeNode, IControlFlowElement>()
     cfg.AllElements
     |> List.ofSeq

24fff07fed9dc6ca06dcd2cc4d62ac7e46327558
raymon-02 <2x2.4@mail.ru>
2015-03-24 03:31:32 +0300

    Add simple tests for EBNF Generator

--- src/RNGLR.EBNFParser.SimpleTest/Program.fs
+++ src/RNGLR.EBNFParser.SimpleTest/Program.fs
@@ -1,7 +1,11 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿module RNGLREBNFParserSimpleTest
 
+open Yard.Generators.Common.AST
 open Yard.Generators.RNGLR.EBNF.Parser
+open Yard.Generators.RNGLR.EBNF
+open NUnit.Framework
+open LexCommon
+
 
 let run path astBuilder =
     let tokens = LexCommon.tokens(path)
@@ -10,11 +14,105 @@ let run path astBuilder =
 let dir = @"../../../Tests/RNGLR.EBNF/"
 let inline printErr (num, token : 'a, msg) =
     printfn "Error in position %d on Token %A: %s" num token msg
-    //Assert.Fail(sprintf "Error in position %d on Token %A: %s" num token msg)
+
+
+[<TestFixture>]
+type ``RNGLREBNF parser tests with simple lexer`` () =
+
+//    let translateAndCheck toDot (expected:List<_>) (ast:Tree<_>) file errors = 
+//        ast.PrintAst()
+//        toDot ast (file + ".dot")
+//        let res = translate RNGLR.ParseOmit.translate ast errors
+//        printfn "Result: %A" res
+//        Assert.AreEqual(expected, res)
+
+    let runTest parser file = 
+        let path = dir + file
+        match run path parser with
+        | Error (num, tok, err, _) -> printErr (num, tok, err)
+        | Success (tree, _) -> printfn "Success"
+
+    [<Test>]
+    member test.``CalcEBNF`` () = 
+        let parser = RNGLR.ParseCalcEBNF.buildAst
+        let file = "CalcEBNF.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``ComplexRightNull`` () = 
+        let parser = RNGLR.ParseComplexRightNull.buildAst
+        let file = "ComplexRightNull.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``ComplexRightNull2`` () = 
+        let parser = RNGLR.ParseComplexRightNull.buildAst
+        let file = "ComplexRightNull2.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``ManyAndOne0`` () = 
+        let parser = RNGLR.ParseManyAndOne.buildAst
+        let file = "ManyAndOne0.txt"
+        runTest parser file
+    
+    [<Test>]
+    member test.``ManyAndOne1`` () = 
+        let parser = RNGLR.ParseManyAndOne.buildAst
+        let file = "ManyAndOne1.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``ManyAndOne2`` () = 
+        let parser = RNGLR.ParseManyAndOne.buildAst
+        let file = "ManyAndOne2.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``simpleOneTerm`` () = 
+        let parser = RNGLR.ParseSimpleOpt.buildAst
+        let file = "simpleOneTerm.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``RightNull`` () = 
+        let parser = RNGLR.ParseRightNull.buildAst
+        let file = "RightNull.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``SimpleEpsilon`` () = 
+        let parser = RNGLR.ParseSimpleEpsilon.buildAst
+        let file = "simpleEpsilon.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``SimpleRightNull`` () = 
+        let parser = RNGLR.ParseSimpleRightNull.buildAst
+        let file = "SimpleRightNull.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``StackingConflictWrong`` () = 
+        let parser = RNGLR.ParseStackingConflict.buildAst
+        let file = "StackingConflictWrong.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``TwoEpsilonMiddle`` () = 
+        let parser = RNGLR.ParseTwoEpsilonsMiddle.buildAst
+        let file = "TwoEpsilonsMiddle.txt"
+        runTest parser file
+
+    [<Test>]
+    member test.``TwoEpsilonMiddleWrong`` () = 
+        let parser = RNGLR.ParseTwoEpsilonsMiddle.buildAst
+        let file = "TwoEpsilonsMiddleWrong.txt"
+        runTest parser file
 
 [<EntryPoint>]
 let main argv = 
-    let parser = RNGLR.EBNFParse.SimpleOpt.buildAst
+    let parser = RNGLR.ParseSimpleOpt.buildAst
     let path = dir + "simpleOneTerm.txt"
     match run path parser with
         | Error (num, tok, err, _) -> printErr (num, tok, err)

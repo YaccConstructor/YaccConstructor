17349a91c0932e5c5be7ae7be855e4fe885d14ed
Marat Khabibullin <maratx387@gmail.com>
2015-03-29 14:11:41 +0300

    First version of generic CFG has been created

---
+++ src/ConstantPropagation/GenericCFG.fs
@@ -0,0 +1,51 @@
+﻿module GenericCFG
+
+open QuickGraph
+
+type OperationType =
+| Replace
+| Concat
+| Arbitrary of string
+// and so on ...
+
+type UpdaterType =
+| Assign
+| PlusAssign
+// function with updatable arg passing
+
+type CFGNodeType = 
+| Declaration of string
+| Updater of string * UpdaterType
+| Operation of OperationType * int
+| Literal of string
+| VarRef of string
+| LoopNode of int * int
+
+type CFGNode = {
+    Id: int
+    IsHotspot: bool
+    Type: CFGNodeType
+}
+
+module CFGNodeFuncs =
+    let toString (node: CFGNode) =
+        match node.Type with
+        | Declaration(name) -> sprintf "decl(%s)" name
+        | Updater(target, aType) -> 
+            let typeStr =
+                match aType with
+                | Assign -> "assign"
+                | PlusAssign -> "plusAssign"
+            sprintf "%s(%s)" typeStr target
+        | Operation(oType, operands) -> 
+            match oType with
+            | Replace -> "replace"
+            | Concat -> "concat"
+            | Arbitrary(name) -> sprintf "%s(%d)" name operands
+        | Literal(value) -> sprintf "literal(%s)" value
+        | VarRef(name) -> sprintf "varRef(%s)" name
+        | LoopNode(_,_) -> "loopNode"
+
+type GenericCFG = {
+    Graph: BidirectionalGraph<CFGNode, Edge<CFGNode>>
+}

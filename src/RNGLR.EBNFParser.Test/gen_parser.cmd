a29f5ec1655871e1ba28c4d815dda1f8714dc042
raymon-02 <2x2.4@mail.ru>
2015-04-18 18:24:02 +0300

    Change script for gen pasrer

--- src/RNGLR.EBNFParser.Test/gen_parser.cmd
+++ src/RNGLR.EBNFParser.Test/gen_parser.cmd
@@ -1,4 +1,3 @@
 del log.txt
 
-..\..\Bin\Release\v40\YC.YaccConstructor.exe -i .\..\..\..\YC.Abstract.SQL\src\TSQL\mssqlNonAbstract.yrd -c Linearize -g "RNGLR.Generator -table LALR -o RNGLR.sql.yrd.fs"
-..\..\Bin\Release\v40\YC.YaccConstructor.exe -i .\..\..\..\YC.Abstract.SQL\src\TSQL\mssqlNonAbstract.yrd -c Linearize -g "RNGLR.EBNFGenerator -table LALR -o RNGLREBNF.sql.yrd.fs"
+..\..\Bin\Release\v40\YC.YaccConstructor.exe -i .\..\..\..\YC.Abstract.SQL\src\Grammars\TSQL\mssqlNonAbstract.yrd  -c Linearize -g "RNGLR.EBNFGenerator -table LALR -o RNGLREBNF.sql.yrd.fs"

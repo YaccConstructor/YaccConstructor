daefbca12a79a13e0f377f104b17ca463b6b248b
GrigorevSergey <unstope@gmail.com>
2015-05-18 22:10:08 +0300

    To working state

--- src/Generators.Common/Epsilon.fs
+++ src/Generators.Common/Epsilon.fs
@@ -16,6 +16,7 @@ module Yard.Generators.Common.Epsilon
 
 open Yard.Generators.Common
 open Yard.Generators.Common.AST
+open Yard.Generators.Common.AstNode
 open Yard.Core.IL
 open Yard.Core.IL.Production
 
@@ -23,7 +24,6 @@ let emptyName = "empty"
 let emptyNum (indexator : Indexator) = indexator.nonTermToIndex emptyName
 
 let canInferEpsilon (rules : NumberedRules) (indexator : Indexator) =
-    
     let result : bool[] = Array.zeroCreate indexator.fullCount
     let mutable modified = true
     result.[indexator.errorIndex] <- true
@@ -71,7 +71,7 @@ let getEpsilonCyclicNonTerms (rules : NumberedRules) (indexator : Indexator) (ca
             dfs [i] i
     !result
 
-let epsilonRules (rules : NumberedRules) (indexator : Indexator) (canInferEpsilon : bool[]) =
+let epsilonRules (rules : NumberedRules) (canInferEpsilon : bool[]) =
     let result : bool[] = Array.zeroCreate rules.rulesCount
     for i in 0..rules.rulesCount-1 do
         result.[i] <-
@@ -80,17 +80,10 @@ let epsilonRules (rules : NumberedRules) (indexator : Indexator) (canInferEpsilo
     result
 
 let epsilonTrees (rules : NumberedRules) (indexator : Indexator) (canInferEpsilon : bool[]) =
-    let allEpsilon = epsilonRules rules indexator canInferEpsilon
-    (*
-    printfn "%A" allEpsilon
-    for i in 0..indexator.nonTermCount-1 do
-        printfn "%d %s: %A" i (indexator.indexToNonTerm i) (rules.rulesWithLeftSide i)
-    for i in 0..rules.rulesCount-1 do
-        printfn "%d: %d -> %A" i (rules.leftSide i) (rules.rightSide i)
-    *)
+    let allEpsilon = epsilonRules rules canInferEpsilon
     let result : Tree<_> [] = Array.zeroCreate indexator.nonTermCount
     let pos = Array.zeroCreate indexator.nonTermCount
-    for u = 0 to indexator.nonTermCount-1 do
+    for u = 0 to indexator.nonTermCount - 1 do
         if canInferEpsilon.[u] then
             let order = new ResizeArray<_>()
             let res = new ResizeArray<_>()
@@ -113,7 +106,7 @@ let epsilonTrees (rules : NumberedRules) (indexator : Indexator) (canInferEpsilo
                                         pos.[w] <- order.Count
                                         order.Add w
                                         res.Add (new AST (Unchecked.defaultof<_>, null))
-                                    box res.[pos.[w]])
+                                    res.[pos.[w]] :> AstNode)
                         children.Add <| new Family(rule, new Nodes(nodes))
                 if v = indexator.errorIndex 
                 then
@@ -135,4 +128,6 @@ let epsilonTailStart (rules : NumberedRules) (canInferEpsilon : bool[]) =
             elif not canInferEpsilon.[rules.symbol i pos] then pos+1
             else inner <| pos-1
         result.[i] <- inner <| rules.length i - 1
-    result
+    result    
+
+    

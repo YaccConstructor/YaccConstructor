4e48028d62f5e9c6cf40160117bb719f9a6ae3e3
raymon-02 <2x2.4@mail.ru>
2015-05-18 19:24:16 +0300

    Add unnecessary files to .gitignore

--- src/RNGLR.EBNFParser.SimpleTest/gen_EBNF.cmd
+++ src/RNGLR.EBNFParser.SimpleTest/gen_EBNF.cmd
@@ -4,5 +4,5 @@ for %%i in (CalcEBNF, ComplexRightNull, EpsilonCycle, ManyAndOne, ManyAndOpt, Ri
 		echo . >> log.txt
 		echo %%i >> log.txt
     ..\..\Bin\Release\v40\YC.YaccConstructor.exe -i %%i.yrd -c ExpandMeta ^
-        -g "RNGLR.EBNFGenerator -pos int -token int -module RNGLR.Parse%%i -translate true -table LR -o %%i.yrd.fs" >> log.txt
+        -g "RNGLR.EBNFGenerator -pos int -token int -module RNGLR.Parser%%i -translate true -table LR -o %%i.yrd.fs" >> log.txt
 )

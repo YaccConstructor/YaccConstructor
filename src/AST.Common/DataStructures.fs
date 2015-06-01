da84f1617d5210f33435c4df975679360b18fcc8
RagozinaAnastasiya <ragozina.anastasiya@gmail.com>
2015-06-01 19:16:26 +0300

    works  on home computer

--- src/AST.Common/DataStructures.fs
+++ src/AST.Common/DataStructures.fs
@@ -84,7 +84,9 @@ type ResizableUsualFive<'T when 'T:equality> =
     val mutable third  : 'T
     val mutable fourth : 'T
     val mutable fifth  : 'T
-    val other          : ref<list<'T>>
+    val mutable other  : list<'T>
+    member this.Eq (o : ResizableUsualFive<'T>) =
+        this.second = o.second && this.first = o.first && this.third = o.third && this.fourth = this.fourth && this.fifth = o.fifth
     member this.Add x =
         if this.second <> Unchecked.defaultof<_> 
         then 
@@ -94,7 +96,7 @@ type ResizableUsualFive<'T when 'T:equality> =
                 then
                     if this.fifth <> Unchecked.defaultof<_>
                     then
-                        this.other := x :: !this.other
+                        this.other <- x :: this.other
                     else
                         this.fifth <- x
                 else
@@ -103,8 +105,7 @@ type ResizableUsualFive<'T when 'T:equality> =
                 this.third <- x
         else
             this.second <- x
-        //this.other := x :: !this.other
-
+        
     member this.TryFind f =
         if f this.first
         then Some this.first
@@ -122,8 +123,8 @@ type ResizableUsualFive<'T when 'T:equality> =
                         then
                             Some this.fifth
                         else
-                            List.tryFind f !this.other
-    new (f) = {first = f; second = Unchecked.defaultof<_>; third = Unchecked.defaultof<_>; fourth = Unchecked.defaultof<_>; fifth = Unchecked.defaultof<_>; other = ref []}
+                            List.tryFind f this.other
+    new (f) = {first = f; second = Unchecked.defaultof<_>; third = Unchecked.defaultof<_>; fourth = Unchecked.defaultof<_>; fifth = Unchecked.defaultof<_>; other = []}
     
 
 

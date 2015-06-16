bc8b22d57b6f33b2e450516fa5df433c69514715
raymon-02 <2x2.4@mail.ru>
2015-06-16 19:43:46 +0300

    Delete kernelSets form Lr table

--- src/RNGLR.EBNF.DFAGenerator/Tables.fs
+++ src/RNGLR.EBNF.DFAGenerator/Tables.fs
@@ -12,18 +12,18 @@ type TablesEBNF(grammar : FinalGrammarNFA, states : StatesInterpreterEBNF) =
         let reduces : list<int * ReduceLabel>[,] = Array2D.create states.count symbolCount []
         // первый индекс - сост анализ, второй индекс - символ грамм
         // list<int - куда переходим, два множества
-        let gotos : list<int * (Set<int> * Set<int>)>[,] = Array2D.create states.count symbolCount []
+        let gotos : list<int> [,] = Array2D.create states.count symbolCount []
         let mutable acc = []
         if grammar.canInferEpsilon.[grammar.rules.leftSide grammar.startRule] then acc <- (*startState*)0::acc
         let endRule = KernelInterpreter.toKernel (grammar.startRule, grammar.rules.numberOfStates grammar.startRule - 1)
-        for i = 0 to states.count-1 do
+        for i = 0 to states.count - 1 do
             let vertex = states.vertex i
             let mainKernels, mainLookaheads = states.mainKernels i, states.mainLookaheads i
             let derivedKernels, derivedLookaheads = states.derivedKernels i, states.derivedLookaheads i
 
             for e in vertex.outEdges do
-                let symbol, stackSets = e.label
-                gotos.[i, symbol] <- (e.dest.label, stackSets)::gotos.[i, symbol]
+                let symbol = e.label
+                gotos.[i, symbol] <- (e.dest.label)::gotos.[i, symbol]
                 if gotos.[i, symbol].Length > 1 then
                     eprintfn "Several gotos form state %d on symbol %d: %A" i symbol gotos.[i, symbol]
             

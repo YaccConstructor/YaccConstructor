ea31fbe977a328a36f296fd7dfd54c921efefe12
IvanovAndrew <ivanovandrew2004@gmail.com>
2015-07-06 18:25:07 +0300

    Add SECR demo file to RShT1 solution

---
+++ Tests/ReSharperPluginDemo/RShT1/RShT1/SECRDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RShT1
+{
+    class SECRDemo
+    {
+
+    public static void Execute()
+    {
+        Program.Eval("(1 + 2) * 3");
+        Program.ExecuteImmediate("insert into y (x,v) values (1,2)");
+        string query = "select x from y where";
+        Program.ExecuteImmediate(query + "v > 1");
+    }
+
+    public static void Go(bool cond)
+    {
+        string query = "xxx = 1;";
+        if (cond)
+        {
+            query += "yyy = 2;";
+        }
+        query += "z = xxx + yyy;";
+        Program.ExtEval(query);
+    }
+
+    public static void Insert(bool cond)
+    {
+        string insertQuery = "insert  into y(x";
+
+        if (cond)
+            insertQuery += ", v)";
+        else
+            insertQuery = insertQuery + ", u)";
+        insertQuery += " values (1, 2)";
+        Program.ExecuteImmediate(insertQuery);
+    }
+    }
+}

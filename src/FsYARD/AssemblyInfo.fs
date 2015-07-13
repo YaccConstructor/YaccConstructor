e70b1bc900312a7c4ef68b9c9b561297fcdecf9b
ragozina.anastasiya <ragozina.anastasiya@gmail.com>
2015-07-13 17:00:45 +0300

    Release

--- src/FsYARD/AssemblyInfo.fs
+++ src/FsYARD/AssemblyInfo.fs
@@ -25,7 +25,7 @@ open System.Runtime.InteropServices
 // associated with an assembly.
 [<assembly: AssemblyTitle("YC.FsYARD")>]
 [<assembly: AssemblyDescription("FsYARD is a parsers generator for F#. It is  based on RNGLR algorithm.")>]
-[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyConfiguration("Release")>]
 [<assembly: AssemblyCompany("YaccConstructor Software Foundation")>]
 [<assembly: AssemblyProduct("YC.FsYARD")>]
 [<assembly: AssemblyCopyright("Copyright © 2013. YaccConstructor Software Foundation https://code.google.com/p/recursive-ascent/")>]

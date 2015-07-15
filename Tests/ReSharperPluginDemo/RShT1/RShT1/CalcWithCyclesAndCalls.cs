93a405656cb6db7a9b3c7feea6c0246e1b4e5216
IvanovAndrew <ivanovandrew2004@gmail.com>
2015-07-15 12:12:38 +0300

    Add example with cycle to SECRDemo.cs

--- Tests/ReSharperPluginDemo/RShT1/RShT1/CalcWithCyclesAndCalls.cs
+++ Tests/ReSharperPluginDemo/RShT1/RShT1/CalcWithCyclesAndCalls.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace RShT1
 {
@@ -12,29 +9,25 @@ namespace RShT1
             var expr = "(10";
             for (int i = 0; i < 10; ++i)
             {
-                expr += "+ 1";
+                expr += " + 1";
                 if (cond)
-                {
                     expr += CalcWithCyclesAndCalls.TrueCaseStr();
-                }
                 else
-                {
                     expr += CalcWithCyclesAndCalls.FalseCaseStr();
-                }
             }
-            var logMsg = "Calculaton starts";
+            var logMsg = "Calculation starts";
             Console.Out.WriteLine(logMsg);
-            return Program.Eval(expr + ") / 2");
+            return Program.Eval(expr + ") /2");
         }
 
         static string TrueCaseStr()
         {
-            return "* 3";
+            return "*3";
         }
 
         static string FalseCaseStr()
         {
-            return "* 2";
+            return "*2";
         }
     }
 }
--- Tests/ReSharperPluginDemo/RShT1/RShT1/SECRDemo.cs
+++ Tests/ReSharperPluginDemo/RShT1/RShT1/SECRDemo.cs
@@ -1,42 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace RShT1
 {
     class SECRDemo
     {
 
-    public static void Execute()
-    {
-        Program.Eval("(1 + 2) * 3");
-        Program.ExecuteImmediate("insert into y (x,v) values (1,2)");
-        string query = "select x from y where";
-        Program.ExecuteImmediate(query + "v > 1");
-    }
-
-    public static void Go(bool cond)
-    {
-        string query = "xxx = 1;";
-        if (cond)
+        public static void Execute()
         {
-            query += "yyy = 2;";
+            Program.Eval("(1 + 2) * 3");
+            Program.ExecuteImmediate("insert into y (x,v) values (1,2)");
+            string query = "select x from y where";
+            Program.ExecuteImmediate(query + " v > 1");
         }
-        query += "z = xxx + yyy;";
-        Program.ExtEval(query);
-    }
 
-    public static void Insert(bool cond)
-    {
-        string insertQuery = "insert  into y(x";
+        public static void Go(bool cond)
+        {
+            var query = "varX = 1;";
+            if (cond)
+                query += "varY = 2;";
+            query += "varZ = varX + varY;";
+            Program.ExtEval(query);
+        }
 
-        if (cond)
-            insertQuery += ", v)";
-        else
-            insertQuery = insertQuery + ", u)";
-        insertQuery += " values (1, 2)";
-        Program.ExecuteImmediate(insertQuery);
-    }
+        public static void Insert(bool cond)
+        {
+            string insertQuery = "insert  into y(x";
+
+            if (cond)
+                insertQuery += ", v)";
+            else
+                insertQuery = insertQuery + ", u)";
+            insertQuery += " values (1, 2)";
+            Program.ExecuteImmediate(insertQuery);
+        }
+
+        public static void LexErrors()
+        {
+            Program.Eval("1 + xyz + 3");
+        }
+
+
+        static int Calculate(bool cond)
+        {
+            var expr = "(10";
+            for (int i = 0; i < 10; ++i)
+            {
+                expr += " + 1";
+                if (cond)
+                    expr += SECRDemo.TrueCaseStr();
+                else
+                    expr += SECRDemo.FalseCaseStr();
+            }
+            var logMsg = "Calculation starts";
+            Console.Out.WriteLine(logMsg);
+            return Program.Eval(expr + ") /2");
+        }
+
+        static string TrueCaseStr()
+        {
+            return "*3";
+        }
+
+        static string FalseCaseStr()
+        {
+            return "*2";
+        }
+
     }
 }

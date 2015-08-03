b904356627414981f8e4998c214108a053a5525d
IvanovAndrew <ivanovandrew2004@gmail.com>
2015-08-03 17:12:36 +0300

    Cleanup

--- src/YC.ReSharper.AbstractAnalysis.Plugin.Helper/ReSharperLanguage.fs
+++
@@ -1,9 +0,0 @@
-﻿module ReSharperLanguage
-
-open YC.SDK.CommonInterfaces
-open JetBrains.DocumentModel
-open JetBrains.ReSharper.Psi.Tree
-open JetBrains.ReSharper.Psi.CSharp.Tree
-
-type IReSharperLanguage = 
-    inherit IInjectedLanguageModule<ICSharpLiteralExpression, DocumentRange, ITreeNode>

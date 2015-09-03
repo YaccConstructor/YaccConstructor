2553641c87ee010ec2e3207f1a74f2f8192854a0
IvanovAndrew <ivanovandrew2004@gmail.com>
2015-09-03 13:37:08 +0300

    Add logging into studio with plugin

--- startStudioWithPlugin.bat
+++ startStudioWithPlugin.bat
@@ -2,14 +2,16 @@
 SET pathToVS12=C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe
 SET pathToVS11=C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe
 
+rem info about resharper logging https://www.jetbrains.com/resharper/devguide/Platform/Logging.html
+
 IF EXIST %pathToVS12%(
-"%pathToVS12%" /RootSuffix YC /ReSharper.Internal
+"%pathToVS12%" /RootSuffix YC /ReSharper.Internal /ReSharper.LogLevel TRACE
 Exit /b
 )
 
 IF EXIST %pathToVS11%
 (
-"%pathToVS11%" /RootSuffix YC /ReSharper.Internal
+"%pathToVS11%" /RootSuffix YC /ReSharper.Internal /ReSharper.LogFile /ReSharper.LogLevel TRACE
 Exit /b
 )
 ELSE

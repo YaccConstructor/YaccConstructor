45de916604715ede155d48a5368f3130691a3a98
octomarat <maratx387@gmail.com>
2015-09-06 19:58:10 +0300

    Bug fix: correct hotspots list formed

--- src/SELApproximator/SELApproximator/BuildFsaForCSharp.cs
+++ src/SELApproximator/SELApproximator/BuildFsaForCSharp.cs
@@ -17,7 +17,7 @@ namespace SELApproximator
     public class BuildFsaForCSharp : ContextActionBase
     {
         private readonly ICSharpContextActionDataProvider _provider;
-        private readonly Hotspot.Hotspot _hotspot = new Hotspot.Hotspot("calc", "Program", "Eval", 0, "void");
+        private readonly FSharpList<Tuple<string, Hotspot.Hotspot>> _hotspotsList = CreateHotspotsList();
 
         public BuildFsaForCSharp(ICSharpContextActionDataProvider provider)
         {
@@ -29,22 +29,37 @@ namespace SELApproximator
             return true;
         }
 
+        public override string Text
+        {
+            get { return "BuildFSAForCSharp"; }
+        }
+
         protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
         {
             var inputFile = _provider.PsiFile;
             const int recursionMaxLevel = 3; // 0 for top and 3 level down
-            
-            var tuple = new Tuple<string, Hotspot.Hotspot>(_hotspot.Language, _hotspot);
-            var temp = new FSharpList<Tuple<string, Hotspot.Hotspot>>(tuple, null);
 
-            var fsa = ApproximateCsharp.ApproximateFileWithLogging(inputFile, recursionMaxLevel, temp)[0].Item2;
+            var automata = ApproximateCsharp.ApproximateFileWithLogging(inputFile, recursionMaxLevel, _hotspotsList);
+            if (automata.Count < 1)
+                throw new ArgumentException("File under processing contains no hotspots, but at least one expected");
+            var fsa = automata[0].Item2;
             Utils.OutputCSharpResult(Utils.FsaToTestDot(fsa), _provider);
             return null;
         }
 
-        public override string Text
+        private static FSharpList<Tuple<string, Hotspot.Hotspot>> CreateHotspotsList()
         {
-            get { return "BuildFSAForCSharp"; }
+            var tSqlHotspot = new Hotspot.Hotspot("TSQL", "Program", "ExecuteImmediate", 0, "void");
+            var calcHotspot = new Hotspot.Hotspot("calc", "Program", "Eval", 0, "int");
+            var tSqlTuple = new Tuple<string, Hotspot.Hotspot>(tSqlHotspot.Language, tSqlHotspot);
+            var calcTuple = new Tuple<string, Hotspot.Hotspot>(calcHotspot.Language, calcHotspot);
+            return 
+                FSharpList<Tuple<string, Hotspot.Hotspot>>.Cons(
+                tSqlTuple,
+                FSharpList<Tuple<string, Hotspot.Hotspot>>.Cons(
+                calcTuple,
+                FSharpList<Tuple<string, Hotspot.Hotspot>>.Empty
+                ));
         }
     }
 }

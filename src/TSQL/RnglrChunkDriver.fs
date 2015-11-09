e7ebc169b1e1e6cd46dcac6f8b753ee9113c1bbd
alefirov.aleksei <alefirov93aa@gmail.com>
2015-11-09 15:54:26 +0300

    TqslRnglrChunkDriverTest project added.
    Some test .sql files from old TSQL repo added/

--- src/TSQL/RnglrChunkDriver.fs
+++ src/TSQL/RnglrChunkDriver.fs
@@ -17,7 +17,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-module YC.ReSharper.AbstractAnalysis.Languages.TSQL.RnglrChunkDriver
+module YC.ReSharper.Languages.TSQL.RnglrChunkDriver
 
 open Microsoft.FSharp.Text.Lexing
 open Yard.Examples.MSParser

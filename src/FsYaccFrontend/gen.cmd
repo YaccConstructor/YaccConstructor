55549daca3cdf8d03f12a10faeb69f76780fd22f
nbIxMaN <Shulllik@gmail.com>
2016-01-26 21:50:15 +0300

    remove fsLex

--- src/FsYaccFrontend/gen.cmd
+++ src/FsYaccFrontend/gen.cmd
@@ -1,2 +1,2 @@
-..\..\Bin\Release\v40\YC.FsLex.exe --unicode Lexer.fsl
+..\..\Bin\Release\v40\AbstractLexer.Generator.exe --unicode Lexer.fsl
 ..\..\Bin\Release\v40\YC.FsYacc.exe --module Yard.Frontends.FsYaccFrontend.Parser --open Yard.Core Parser.fsy

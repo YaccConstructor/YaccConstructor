de3c98f38d453d92d5d98ff3ce15a55c790884ef
Vlad Evtushenko <cryptically@yandex.ru>
2016-01-27 12:24:16 +0700

    Issue 42: changed filepaths removed copies of files. Now all are in GrammarZoo

--- src/Calc/gen_highlighting.cmd
+++ src/Calc/gen_highlighting.cmd
@@ -2,6 +2,6 @@ del log.txt
 
 ..\..\Bin\Release\v40\AbstractLexer.Generator.exe Lexer.fsl --unicode -o Lexer.fs --abstract
 
-echo calc.yrd >> log.txt
-    ..\..\Bin\Release\v40\YC.YaccConstructor.exe -f YardFrontend -i calc.yrd -c ExpandEbnf -c ExpandMeta ^
+echo calc_c.yrd >> log.txt
+    ..\..\Bin\Release\v40\YC.YaccConstructor.exe -f YardFrontend -i ..\..\src\YC.GrammarZOO\Calc\calc_c.yrd -c ExpandEbnf -c ExpandMeta ^
         -g "RNGLRGenerator -pos uint64 -module Calc.AbstractParser -translate false -highlighting true -namespace CalcHighlighting -table LALR -abstract true -o Calc.yrd.fs" >> log.txt

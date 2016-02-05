296ad754ff81dd353f8e6d0c906b7a2bb9cdea2e
Lares77 <lares77@yandex.ru>
2016-02-05 10:04:50 +0300

    RIGLR-generator project

---
+++ src/RIGLRGenerator/RIGLRGenerator.fs
@@ -0,0 +1,17 @@
+﻿namespace Yard.Generators.RIGLRGenerator
+
+open Mono.Addins
+open Yard.Core
+open Constraints
+
+[<assembly:Addin>]
+[<assembly:AddinDependency ("YaccConstructor", "1.0")>]
+do()
+
+[<Extension>]
+type RIGLR() = 
+    inherit Generator()
+        override this.Name = "hello"
+        override this.Constraints = [|noEbnf; noMeta; noInnerAlt; (*noLiterals;*) noInnerAlt; noBrackets; needAC; singleModule|]
+        override this.Generate (definition, args) = box ()
+        override this.Generate definition = this.Generate (definition, "")

7751d0f0f75935698f7dc9884e317543759bdc07
Roman Fedorov <Roman2x@yandex.ru>
2016-02-07 16:23:14 +0300

    Refactor for pull request

--- src/Earley/Program.fs
+++ src/Earley/Program.fs
@@ -4,14 +4,14 @@ open Yard.Core.Conversions.Linearize
 open Yard.Core.IL.Production
 open System.Collections.Generic
 
-type EarleyItem(rule, dotPosition : int, startPosition : int) =
-    let rule = rule
-    let dotPosition = dotPosition
-    let startPosition = startPosition
+type EarleyItem = struct
+    val Rule : Yard.Core.IL.Rule.t<Yard.Core.IL.Source.t, Yard.Core.IL.Source.t>
+    val DotPosition : int
+    val StartPosition : int
 
-    member this.getRule = rule
-    member this.getDotPosition = dotPosition
-    member this.getStartPosition = startPosition
+    new (rule, dotPosition, startPosition) = 
+        {Rule = rule; DotPosition = dotPosition; StartPosition = startPosition}
+end
 
 type Grammar(grammarPath : string) = 
     let ilParser = new Yard.Frontends.YardFrontend.YardFrontend()
@@ -29,58 +29,55 @@ type Recognizer(grammar : Grammar) =
         let currentPosition = ref 0
         let stateSet = [|for _ in 0..(input.Length + 1) do yield new ResizeArray<EarleyItem>()|]
         
+        let getRightSide(rule : Yard.Core.IL.Rule.t<_, _>) =
+            match rule.body with 
+                | PSeq(x, _, _) -> x
+                | _ -> []
+            
         // Prediction, Scan and Completion steps as described in the original article
         let prediction (item : EarleyItem) (nonTerminal : Yard.Core.IL.Source.t) =
             let predictions = grammar |> List.filter (fun (rule : Yard.Core.IL.Rule.t<_, _>) -> rule.name.text = nonTerminal.text)
             for prediction in predictions do
-                let newItem = EarleyItem(prediction, 0, !currentPosition)
+                let newItem = new EarleyItem(prediction, 0, !currentPosition)
                 if not (stateSet.[!currentPosition].Contains(newItem)) then
                     stateSet.[!currentPosition].Add(newItem)
             
         let scan (item : EarleyItem) terminal =
             if !currentPosition < input.Length && terminal = input.[!currentPosition] then 
-                stateSet.[(!currentPosition + 1)].Add(EarleyItem(item.getRule, item.getDotPosition + 1, item.getStartPosition))
+                stateSet.[(!currentPosition + 1)].Add(new EarleyItem(item.Rule, item.DotPosition + 1, item.StartPosition))
            
         let completion (item : EarleyItem) =
-            let startOfPartialParse = item.getStartPosition
-            let parsedNonTerminal = item.getRule.name.text
+            let startOfPartialParse = item.StartPosition
+            let parsedNonTerminal = item.Rule.name.text
             for previousItem in stateSet.[startOfPartialParse] do
-                let rightSideOfARule = match previousItem.getRule.body with 
-                                        | PSeq(r, _, _) -> r
-                                        | _ -> []
-                if previousItem.getDotPosition < rightSideOfARule.Length then
-                    match rightSideOfARule.[previousItem.getDotPosition].rule with
+                let rightSideOfARule = getRightSide(previousItem.Rule)
+                if previousItem.DotPosition < rightSideOfARule.Length then
+                    match rightSideOfARule.[previousItem.DotPosition].rule with
                     | PRef(previousNonTerminal, _) when previousNonTerminal.text = parsedNonTerminal ->
-                        stateSet.[!currentPosition].Add(EarleyItem(previousItem.getRule, previousItem.getDotPosition + 1, previousItem.getStartPosition))
+                        stateSet.[!currentPosition].Add(new EarleyItem(previousItem.Rule, previousItem.DotPosition + 1, previousItem.StartPosition))
                     | _ -> ()
 
         let exploreItem (item : EarleyItem) =
-            let currentRule = item.getRule
-            let body = currentRule.body
-            let rightSideOfARule = match body with 
-                                        | PSeq(x, _, _) -> x
-                                        | _ -> []            
-            if rightSideOfARule.Length = item.getDotPosition then
+            let currentRule = item.Rule
+            let rightSideOfARule = getRightSide(currentRule)
+            if rightSideOfARule.Length = item.DotPosition then
                 completion item
             else
-                match rightSideOfARule.[item.getDotPosition].rule with
+                match rightSideOfARule.[item.DotPosition].rule with
                 | PToken(terminal) -> 
                     scan item terminal.text.[0]
                 | PRef(nonTerminal, _) -> 
                     prediction item nonTerminal
                 | _ -> 
-                    printfn "MAYBE ERROR"
+                    raise (System.ArgumentException("Right side of a rule contains unsupported items"))
 
         let satisfiesSuccessfulParse (item : EarleyItem) = 
-            let rule = item.getRule
-            let body = rule.body
-            let ruleLength = match body with 
-                                   | PSeq(x, _, _) -> x.Length
-                                   | _ -> 0
-            rule.isStart && item.getStartPosition = 0 && ruleLength = item.getDotPosition
+            let rule = item.Rule
+            let ruleLength = getRightSide(rule).Length
+            rule.isStart && item.StartPosition = 0 && ruleLength = item.DotPosition
             
         // initialize
-        stateSet.[0] <- new ResizeArray<EarleyItem>([for rule in grammar do if rule.isStart then yield EarleyItem(rule, 0, 0)])
+        stateSet.[0] <- new ResizeArray<EarleyItem>([for rule in grammar do if rule.isStart then yield new EarleyItem(rule, 0, 0)])
         
         // main body: 2 loops
         for _ in 0..input.Length do

3696a29f4ba6085656a2a5631cc9d7b7c8623e6e
Roman Fedorov <Roman2x@yandex.ru>
2016-02-09 19:12:37 +0300

    Earley test complete refactor

--- src/Earley/EarleyTests.fs
+++ src/Earley/EarleyTests.fs
@@ -2,6 +2,7 @@
 
 open Yard.Earley
 open System.IO
+open System.Text.RegularExpressions
 
 [<EntryPoint>]
 let main _ =
@@ -9,4 +10,25 @@ let main _ =
     let r = Recognizer(g)
     assert (r.recognize("AAB") = false)
     assert (r.recognize("AABD") = true)
+
+    let g_json = new Grammar("D:\Y\YaccConstructor\src\JSON_Parser\JSON.yrd")
+    let r_json = Recognizer(g_json)
+    
+    assert(r_json.recognize("false") = true)
+    assert(r_json.recognize("\"str\"") = true)
+    assert(r_json.recognize("-1") = true)
+
+    assert(r_json.recognize("[false]") = true)
+    assert(r_json.recognize("[111,2]") = true)
+    assert(r_json.recognize("[1,2,\"some\"]") = true)
+    
+    assert(r_json.recognize("{null}") = false)
+    assert(r_json.recognize("{1:2}") = false)
+    assert(r_json.recognize("{\"str\":2}") = true)
+    assert(r_json.recognize("{\"str\":2,\"another\":null,\"more\":true,\"again\":\"yes\"}") = true)
+    assert(r_json.recognize(@"{""str"":{""s"":12}}") = true)
+
+    let text = File.ReadAllText("../../../../Tests/Earley/test.json")
+    let complexTest = Regex.Replace(text, @"\s+", "")
+    assert(r_json.recognize(complexTest) = true)
     0

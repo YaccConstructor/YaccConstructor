d2ff3bc2063aec4953105ad7710b5b0afeab368e
Roman Fedorov <Roman2x@yandex.ru>
2016-02-10 14:36:15 +0300

    Add SQL tests and delete JSON tests

--- src/Earley/EarleyTests.fs
+++ src/Earley/EarleyTests.fs
@@ -2,7 +2,6 @@
 
 open Yard.Earley
 open System.IO
-open System.Text.RegularExpressions
 
 [<EntryPoint>]
 let main _ =
@@ -11,24 +10,9 @@ let main _ =
     assert (r.recognize("AAB") = false)
     assert (r.recognize("AABD") = true)
 
-    let g_json = new Grammar("D:\Y\YaccConstructor\src\JSON_Parser\JSON.yrd")
-    let r_json = Recognizer(g_json)
+    let g_sql = new Grammar("../../../YC.GrammarZOO/SQL/TSQL/mssql.yrd")
+    let r_sql = Recognizer(g_sql)
+    assert(r_sql.recognize("selectIdentIdentFromIdentOrderByIdentDecs") = false)
+    assert(r_sql.recognize("selectIdentIdentFromIdentOrderByIdentDesc") = true)
     
-    assert(r_json.recognize("false") = true)
-    assert(r_json.recognize("\"str\"") = true)
-    assert(r_json.recognize("-1") = true)
-
-    assert(r_json.recognize("[false]") = true)
-    assert(r_json.recognize("[111,2]") = true)
-    assert(r_json.recognize("[1,2,\"some\"]") = true)
-    
-    assert(r_json.recognize("{null}") = false)
-    assert(r_json.recognize("{1:2}") = false)
-    assert(r_json.recognize("{\"str\":2}") = true)
-    assert(r_json.recognize("{\"str\":2,\"another\":null,\"more\":true,\"again\":\"yes\"}") = true)
-    assert(r_json.recognize(@"{""str"":{""s"":12}}") = true)
-
-    let text = File.ReadAllText("../../../../Tests/Earley/test.json")
-    let complexTest = Regex.Replace(text, @"\s+", "")
-    assert(r_json.recognize(complexTest) = true)
     0

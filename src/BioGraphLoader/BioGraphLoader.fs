8265cf4da1bc5dd6199dd5b3fb6fd4bbde8e4840
rsdpisuy <rsdpisuy@gmail.com>
2016-02-13 13:05:33 +0300

    Bio graph loader. TMP.

---
+++ src/BioGraphLoader/BioGraphLoader.fs
@@ -0,0 +1,11 @@
+﻿module YC.BIO.BioGraphLoader
+
+open System.IO
+
+let lodGraph fileWithoutExt =
+    let lblsExt = ".sqn"
+    let graphStrauctureExt = ".grp"
+    let lbls = 
+        File.ReadAllLines(fileWithoutExt + lblsExt) 
+        |> 
+

ac57334c4150fc6b9b162c0917a0c81dbee7a049
rsdpisuy <rsdpisuy@gmail.com>
2016-03-22 11:59:22 +0300

    Use Argu for cmd arguments processing.

--- src/RNA.Search/RNA.Search.fs
+++ src/RNA.Search/RNA.Search.fs
@@ -1,7 +1,27 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿module YC.Bio.RNA.Search
+
+open Argu
+
+type CLIArguments =
+    | [<Mandatory>][<AltCommandLine("-i")>] Input of string
+    
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Input _ -> "specify a graph for processing."            
+
+let searchTRNA graphs =
+    ()
+
+let search file =
+    ()
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let parser = ArgumentParser.Create<CLIArguments>()
+    let args = parser.Parse argv
+    let inputGraphPath = 
+        args.GetResult <@Input@>
+        |> System.IO.Path.GetFileNameWithoutExtension
+    0

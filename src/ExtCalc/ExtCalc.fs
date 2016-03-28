f803e0819f7da13aae5ed413b4030c881d349560
IvanovAndrew <ivanovandrew2004@gmail.com>
2016-03-28 23:59:56 +0300

    Add forgotten change

--- src/ExtCalc/ExtCalc.fs
+++ src/ExtCalc/ExtCalc.fs
@@ -11,17 +11,19 @@ open JetBrains.ReSharper.Psi.CSharp.Tree
 open ControlFlowGraph.Common
 open ControlFlowGraph.InputStructures
 
-open Yard.Generators.RNGLR.OtherSPPF
-open ExtCalc.AbstractParser
-open ReSharperExtension
-open Yard.Generators.Common.AST
 open QuickGraph.FSA.GraphBasedFsa
 open QuickGraph.FSA.FsaApproximation
-open YC.FST.AbstractLexing.Interpreter
 open QuickGraph.FST.GraphBasedFst
+
+open Yard.Generators.Common.AST
+open Yard.Generators.RNGLR.OtherSPPF
+
+open YC.FST.AbstractLexing.Interpreter
 open YC.SDK.CommonInterfaces
 open YC.SDK.ReSharper.Helper
 
+open ExtCalc.AbstractParser
+open ReSharperExtension
 
 [<ZoneMarker>]
 type ZoneMarker() = class end
@@ -72,9 +74,8 @@ type ExtCalcInjectedLanguageModule() =
     let semicolonNumber = tokenToNumber <| Token.SEMI (new FSA<_>())
     let eqNumber = tokenToNumber <| Token.EQ (new FSA<_>())
     let isVariable = 
-        fun n -> 
-            let num2 = tokenToNumber <| Token.VARIABLE (new FSA<_>())
-            n = num2
+        let num2 = tokenToNumber <| Token.VARIABLE (new FSA<_>())
+        fun n -> n = num2
 
     let nodeToType = dict["assign", Assignment;]
     let keywordToInt = dict [
@@ -90,9 +91,9 @@ type ExtCalcInjectedLanguageModule() =
         |> List.ofSeq
         |> List.map (fun range -> range.GetText())
         |> List.ofSeq
-        |> List.fold (fun acc elem -> acc + elem) ""
+        |> List.reduce (+)
 
-    let parserSource = new CfgParserSource<Token>(tokenToNumber, numToString, leftSide, tokenData)
+    let parserSource = new GeneratedStuffSource<Token, br>(tokenToNumber, numToString, leftSide, tokenData)
     let semantic = Some <| (parserSource, langSource, tokToSourceString)
 
     let processor =

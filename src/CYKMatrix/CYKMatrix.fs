4c957c2c04b7df4c0f1c457f529d69cf157c34e4
Anya Yaveyn <anya.yaveyn@yandex.ru>
2016-05-01 22:27:28 +0300

    minor changes

--- src/CYKMatrix/CYKMatrix.fs
+++ src/CYKMatrix/CYKMatrix.fs
@@ -3,8 +3,6 @@
     open Util
     open System.Collections.Generic
 
-
-
     let recognize (strToParse: string) 
                   (complexRules: Dictionary<(NonTerminal * NonTerminal), (NonTerminal * double) list>)
                   (simpleRules: Dictionary<char, (NonTerminal * double) list>)
@@ -131,80 +129,3 @@
         compute 0 (roundedSize + 1) |> ignore
 
         tMatrix.Item S
-
-
-////    [<EntryPoint>]
-////    let main args = 
-////        let A = NonTerminal "A"
-////        let B = NonTerminal "B"
-////        let BB = NonTerminal "BB"
-////        let S = NonTerminal "S"
-////        let nonterminals = [|A; B; S; BB|]
-////
-////
-////        let crl = new Dictionary<NonTerminal * NonTerminal, (NonTerminal * double) list>()
-////        [(A, BB), [S, 1.]; (B, B), [BB, 1.; B, 0.2]; (A, A), [A, 0.8]] |> Seq.iter crl.Add
-////        let srl = new Dictionary<char, (NonTerminal * double) list>()
-////        ['a', [(A, 0.2)]; 'b', [B, 0.4]; 'c', [B, 0.4]] |> Seq.iter srl.Add
-////        let erl: NonTerminal list = []
-////
-////    //    S -> A BB, 1.
-////    //
-////    //    BB -> B B, 1.
-////    //
-////    //    A -> A A, 0.8
-////    //    A -> 'a', 0.2
-////    //
-////    //    B -> B B, 0.2
-////    //    B -> 'b', 0.4
-////    //    B -> 'b', 0.4
-////
-////        let printMatrix (matrix: double [,]) strLen searchLen =
-////            let rowLength = matrix.GetLength(0)
-////            let colLength = matrix.GetLength(1)
-////
-////            for i in [0..rowLength-1] do
-////                for j in [0..colLength-1] do
-////                    if i <= strLen && j <= strLen && j > i && j-i <= searchLen then
-////                        printf "%.8f  " matrix.[i, j]
-////                    else
-////                        assert (matrix.[i, j] = 0.)
-////                        printf "----------  "
-////                printfn ""
-////            printfn ""
-////
-////        let isAnswerValid (matrix: double [,]) strLen searchLen = 
-////            let rowLength = matrix.GetLength(0)
-////            let colLength = matrix.GetLength(1)
-////            if rowLength <> colLength || rowLength <> strLen + 1 then
-////                false
-////            else
-////                let notRealCell (i, j) =
-////                        i > strLen 
-////                        || j > strLen 
-////                        || j <= i 
-////                        || j-i > searchLen 
-////
-////                [1..rowLength-1]
-////                |> List.map (fun i -> [1..colLength-1] |> List.map (fun j -> (i,j))) 
-////                |> List.concat
-////                |> List.filter notRealCell
-////                |> List.forall (fun (i, j) -> matrix.[i,j] = 0.)
-////
-////        let check str searchLen = 
-////            let toCheck = recognize str crl srl erl nonterminals S searchLen
-////            assert (isAnswerValid toCheck (String.length str) searchLen)
-////            printMatrix toCheck (String.length str) searchLen 
-////    //        printfn "%.10f" toCheck |> ignore
-////    //        assert (abs(res - toCheck) < 1e-14) 
-////
-////    //    check "abb"     0.032        2 |> ignore    
-////    //    check "aaabbcc" 0.0000524288 3 |> ignore
-////
-////        check "abb"     2 |> ignore    
-////        check "abb"     3 |> ignore    
-////        check "aaabbcc" 3 |> ignore
-////
-////
-////        System.Console.ReadLine() |> ignore
-////        0

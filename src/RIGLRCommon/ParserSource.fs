4072b1b022315ed05bba3364e7691d78f27a299f
Lares77 <lares77@yandex.ru>
2016-05-19 20:26:42 +0300

    Printer + ParserSource

---
+++ src/RIGLRCommon/ParserSource.fs
@@ -0,0 +1,14 @@
+﻿namespace Yard.Generators.RIGLR
+
+type ParserSource<'TokenType> ( table             : (int*int)[][][]
+                                , tokenToNumber   : 'TokenType -> int
+                                , genLiteral      : string -> int -> 'TokenType option
+                                , numToString     : int -> string
+                                , tokenData       : 'TokenType -> obj
+                              ) = 
+    
+    member this.Table              = table
+    member this.TokenToNumber      = tokenToNumber
+    member this.GenLiteral         = genLiteral 
+    member this.TokenData          = tokenData
+    member this.NumToString        = numToString

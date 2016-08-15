8fa62d732fe94d89a0a7a8246a5e1ec4b3d5ab5b
gsv <rsdpisuy@gmail.com>
2016-08-15 22:44:44 +0300

    Some performance tests.

--- tests/GLL.AbstractParser.Simple.Tests/gen.cmd
+++ tests/GLL.AbstractParser.Simple.Tests/gen.cmd
@@ -73,6 +73,11 @@
 ..\..\Bin\Release\v40\YC.YaccConstructor.exe -i Brackets.yrd -c ExpandEbnf -c ExpandMeta ^
         -g "GLLGenerator -pos int -token int -module GLL.Brackets -translate false -o Brackets.yrd.fs -abstract true" >> log.txt
 
+
+..\..\Bin\Release\v40\YC.YaccConstructor.exe -i Brackets2.yrd -c ExpandEbnf -c ExpandMeta ^
+        -g "GLLGenerator -pos int -token int -module GLL.Brackets2 -translate false -o Brackets2.yrd.fs -abstract true" >> log.txt
+
+
 ..\..\Bin\Release\v40\YC.YaccConstructor.exe -i StrangeBrackets.yrd -c ExpandEbnf -c ExpandMeta ^
         -g "GLLGenerator -pos int -token int -module GLL.StrangeBrackets -translate false -o StrangeBrackets.yrd.fs -abstract true" >> log.txt
 

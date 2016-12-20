5e47d6b9221e828cdebb96fc7d620ae206d55402
gsv <rsdpisuy@gmail.com>
2016-12-20 19:23:55 +0300

    Bump version to 0.1.1.0

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+### 0.1.1.0
+* Error recovery based on graph parsing for RNGLR.
+
 ### 0.1.0.2
 * Root of GLL ast is available as field
 
--- src/FsYaccFrontend/AssemblyInfo.fs
+++ src/FsYaccFrontend/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsYaccFrontend")>]
 [<assembly: AssemblyProductAttribute("YaccConstructor")>]
 [<assembly: AssemblyDescriptionAttribute("Platform for parser generators and other grammarware research and development.")>]
-[<assembly: AssemblyVersionAttribute("0.1.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0.2")>]
+[<assembly: AssemblyVersionAttribute("0.1.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0.2"
-    let [<Literal>] InformationalVersion = "0.1.0.2"
+    let [<Literal>] Version = "0.1.1.0"
+    let [<Literal>] InformationalVersion = "0.1.1.0"

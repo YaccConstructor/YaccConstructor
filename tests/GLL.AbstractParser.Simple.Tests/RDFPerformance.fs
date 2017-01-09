b3abdffef78ace0ef66fdfaff6901beec93e2936
rsdpisuy <rsdpisuy@gmail.com>
2017-01-09 17:31:45 +0300

    More RDF data

--- tests/GLL.AbstractParser.Simple.Tests/RDFPerformance.fs
+++ tests/GLL.AbstractParser.Simple.Tests/RDFPerformance.fs
@@ -3,6 +3,10 @@
 open VDS.RDF
 open VDS.RDF.Parsing
 
+let getFragment (g:Graph) =
+    for t in g.Triples do        
+        printfn "%A" ((t.Predicate :?> UriNode).Uri.Fragment)
+
 let loadFromFile (file:string) =
     let g = new Graph()
     if (System.IO.Path.GetExtension file).ToLower() = "ttl"
@@ -10,6 +14,8 @@ let loadFromFile (file:string) =
         let ttlparser = new TurtleParser()
         ttlparser.Load(g, file)
     else
-        FileLoader.Load(g, file)        
+        FileLoader.Load(g, file)   
+    getFragment g     
         
     g
+

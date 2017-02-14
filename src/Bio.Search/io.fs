34c4d010be2780ff5587a2da1c21615b7d311db6
rsdpisuy <rsdpisuy@gmail.com>
2017-02-14 13:23:09 +0300

    Infernal output loader.

--- src/Bio.Search/io.fs
+++ src/Bio.Search/io.fs
@@ -8,99 +8,73 @@ open Microsoft.FSharp.Collections
 open System.IO
 open System.Collections.Generic
 
-//let edgeToString (e:TaggedEdge<_,BioGraphEdgeLbl>) = 
-//    Array.fold (fun string x -> string + numToString x) "" e.Tokens
+[<Struct>]
+type InfernalData =
+    val TargetName : string
+    val Accession1 : Option<string>
+    val QueryName : string
+    val Accession2 : Option<string>
+    val ModelType : string
+    val ModelFrom : int 
+    val ModelTo : int
+    val SeqFrom : int
+    val SeqTo : int
+    val Strand : bool
+    val Trunc : string
+    val Pass : int
+    val Gc : float
+    val Bias : float 
+    val Score : float
+    val E_value : double
+    val Inc : bool
+    val DescriptionOfTarget : Option<string>
 
-///Seq of edges to one string 
-let pathToString (path : ResizeArray<TaggedEdge<_,BioGraphEdgeLbl>>)=
-    ""
-//    path
-//    |> ResizeArray.fold (fun string e -> string + e.Tag.str) ""
+    new (tName, accession1, queryName, accession2, mdl, mdlFrom, mdlTo, seqFrom, seqTo, strand, trunc, pass, gc, bias, score, e_value, inc, descr) =
+        {
+            TargetName = tName
+            Accession1 = accession1
+            QueryName = queryName
+            Accession2 = accession2
+            ModelType = mdl
+            ModelFrom = mdlFrom
+            ModelTo = mdlTo
+            SeqFrom = seqFrom
+            SeqTo = seqTo
+            Strand = strand
+            Trunc = trunc
+            Pass = pass
+            Gc = gc
+            Bias = bias
+            Score = score
+            E_value = e_value
+            Inc = inc
+            DescriptionOfTarget = descr
+        }
 
-let getInfo prefix index = 
-    ">" + prefix + index.ToString()
-
-let printStringsToFASTA path prefix lines =
-    lines
-    |> Seq.mapi (fun i (sourceInfo, line) -> (getInfo prefix i) + " " + sourceInfo + "\n" + line)
-    |> (fun x -> File.AppendAllLines(path, x))
-
-let printEdgesToFASTA path numToString prefix (edges : TaggedEdge<_,BioGraphEdgeLbl> list) =
-    ()
-//    edges
-//    |> List.map (fun e -> e.Tag.str)
-//    |> List.mapi (fun i line -> (getInfo prefix i) + line)
-//    |> (fun x -> File.AppendAllLines(path, x))
-
-let printPathsToFASTA (graph : AdjacencyGraph<int,TaggedEdge<_,BioGraphEdgeLbl>>) path (res:(List<List<TaggedEdge<_,BioGraphEdgeLbl>>> * (int* int * (int * int * int) [])) []) i (shift : int) = 
-    ()
-    //let rec splitLine (line:string) =
-    //    if line.Length <= maxLineLength then [line] else
-    //    (line.Substring (0, maxLineLength))::(splitLine (line.Substring maxLineLength))
-//    let graphEdges = graph.Edges |> Array.ofSeq
-//
-//    let printResult j ((paths:List<List<TaggedEdge<_,BioGraphEdgeLbl>>>), (startE : int, finalE : int, poss)) = 
-//        let prefix = "Graph" + i.ToString() + ".Subgraph" + j.ToString()+ "."
-//        let lenToBegEnd = new Dictionary<int,List<_>>()
-//        
-//        poss
-//        |> Array.iter (fun (startPos, finalPos, length) -> 
-//            let prefixPostfix = 
-//                if startE = finalE && length = 0 && startPos < finalPos
-//                then
-//                    sprintf "%i:%i -> %i:%i"
-//                        (startE+shift) (graphEdges.[startE].Tag.sourceStartPos + startPos)
-//                        (startE+shift) (graphEdges.[startE].Tag.sourceStartPos + finalPos)
-//                    , graphEdges.[startE].Tag.str.[startPos .. (finalPos - startPos + 1)]
-//                    , [||]
-//                else
-//                    sprintf "%i:%i -> %i:%i"
-//                        (startE+shift) (graphEdges.[startE].Tag.sourceStartPos + startPos)
-//                        (finalE+shift) (graphEdges.[finalE].Tag.sourceStartPos + finalPos)
-//                    , graphEdges.[startE].Tag.str.[startPos .. ]
-//                    , graphEdges.[finalE].Tag.str.[0 .. finalPos + 1]
-//
-//            let cond, value = lenToBegEnd.TryGetValue length
-//            if cond
-//            then
-//                value.Add prefixPostfix
-//            else
-//                lenToBegEnd.Add (length, new List<_>([prefixPostfix]))
-//            )
-//        let lines =
-//            if paths.Count = 0 then
-//                lenToBegEnd.Values
-//                |> Seq.collect (fun prefixPostfix -> 
-//                    prefixPostfix
-//                    |> Seq.collect (fun (sourceInfo, prefix, postfix) -> [sourceInfo, prefix + postfix]))
-//                |> Array.ofSeq
-//            else
-//                paths
-//                |> Seq.collect (fun path ->
-//                    let line = pathToString path
-//                    let cond, value = lenToBegEnd.TryGetValue line.Length
-//                    if cond
-//                    then
-//                        value
-//                        |> Seq.map (fun (sourceInfo, prefix, postfix) -> sourceInfo, prefix + line + postfix)
-//                    else
-//                        failwith "some length is not eq res length")
-//                |> Array.ofSeq
-//
-//        printStringsToFASTA path prefix lines
-//    res
-//    |> Array.iteri printResult
-
-(*
-let printResult results graphNumber =
-    let maxLineLength = 80
-    let resultPath = ".\\result.fa"
-    let rec splitLine (line:string) =
-        if line.Length <= maxLineLength then [line] else
-        let buff = line.Substring (0, maxLineLength)
-        buff::(splitLine buff)
-    results
-    |> ResizeArray.iteri (fun subGraphNumber (time,strings) -> let info = ">Graph" + graphNumber.ToString() + ".Subgraph" + subGraphNumber.ToString()+ ". Time: "+ time.ToString()
-                                                                let toPrint = strings |> List.collect (fun (line:string) -> splitLine line)
-                                                                File.AppendAllLines(resultPath, info::toPrint))
-    *)
+let readInfernalout file =
+    let strToOpt s = if s = "-" then None else Some s
+    System.IO.File.ReadAllLines file
+    |> Seq.filter (fun s -> s.StartsWith "#" |> not)
+    |> Seq.map (fun s -> s.Split ' '|> Array.filter ((<>)""))
+    |> Seq.map (fun a -> 
+        new InfernalData (
+            a.[0],
+            strToOpt a.[1],
+            a.[2],
+            strToOpt a.[3],
+            a.[4],
+            int a.[5],
+            int a.[6],
+            int a.[7],
+            int a.[8],
+            a.[9] = "+",
+            a.[10],
+            int a.[11],
+            float a.[12],
+            float a.[13],
+            float a.[14],
+            double a.[15],
+            a.[16] = "!",
+            strToOpt (a.[17..] |> String.concat " ")
+            ) )
+        |> Array.ofSeq

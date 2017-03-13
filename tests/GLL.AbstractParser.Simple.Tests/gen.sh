762aadd86fce82608e30c1cb13139634380303f5
MallabaevAzamat <mallabaev.an@yandex.ru>
2017-03-14 00:09:33 +0300

    add mono prefix

--- tests/GLL.AbstractParser.Simple.Tests/gen.sh
+++ tests/GLL.AbstractParser.Simple.Tests/gen.sh
@@ -1,3 +1,2 @@
-../../Bin/Release/v40/YC.YaccConstructor.exe -i GPPerf1.yrd -c ExpandMeta -g "GLLGenerator -pos int -token int -module GLL.GPPerf1  -o GPPerf1.yrd.fs " >> log.txt
-
-../../Bin/Release/v40/YC.YaccConstructor.exe -i GPPerf2.yrd -c ExpandMeta -g "GLLGenerator -pos int -token int -module GLL.GPPerf2  -o GPPerf2.yrd.fs " >> log.txt
+mono ../../Bin/Release/v40/YC.YaccConstructor.exe -i GPPerf1.yrd -c ExpandMeta -g "GLLGenerator -pos int -token int -module GLL.GPPerf1  -o GPPerf1.yrd.fs " >> log.txt
+mono ../../Bin/Release/v40/YC.YaccConstructor.exe -i GPPerf2.yrd -c ExpandMeta -g "GLLGenerator -pos int -token int -module GLL.GPPerf2  -o GPPerf2.yrd.fs " >> log.txt
--- tests/GLLParser.Simple.Tests/gen.sh
+++ tests/GLLParser.Simple.Tests/gen.sh
@@ -1,4 +1,4 @@
 
 [ -f log.txt ] && rm log.txt
                                                                                                
-..\..\Bin\Release\v40\YC.YaccConstructor.exe -i InfEpsilon.yrd -g "GLLGenerator -pos int -token int -module GLL.ParseInfEpsilon -o InfEpsilon.yrd.fs" >> log.txt
+mono ..\..\Bin\Release\v40\YC.YaccConstructor.exe -i InfEpsilon.yrd -g "GLLGenerator -pos int -token int -module GLL.ParseInfEpsilon -o InfEpsilon.yrd.fs" >> log.txt

52524879a245c6a9d8c3f1ca3f38f52ddeb1c37d
MallabaevAzamat <mallabaev.an@yandex.ru>
2017-03-13 18:05:21 +0300

    add parser.common to yc.minimal

--- src/FsYacc/gen.cmd
+++ src/FsYacc/gen.cmd
@@ -1,2 +1,2 @@
-..\..\lkg\FSharp.PowerPack-1.9.7.7\bin\FsLex.exe --unicode --lexlib Microsoft.FSharp.Text.Lexing fsyacclex.fsl
-..\..\lkg\FSharp.PowerPack-1.9.7.7\bin\FsYacc.exe --internal --module FSharp.PowerPack.FsYacc.Parser --lexlib Microsoft.FSharp.Text.Lexing  --parslib Microsoft.FSharp.Text.Parsing fsyaccpars.fsy
+..\..\lkg\FSharp.PowerPack-1.9.7.7\bin\FsLex.exe --unicode --lexlib Internal.Utilities.Text.Lexing fsyacclex.fsl
+..\..\lkg\FSharp.PowerPack-1.9.7.7\bin\FsYacc.exe --internal --module FSharp.PowerPack.FsYacc.Parser --lexlib Internal.Utilities.Text.Lexing  --parslib Internal.Utilities.Text.Parsing fsyaccpars.fsy

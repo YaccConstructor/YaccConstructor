3f8a98337a51d2438f91cfa7c7594c8895cd426a
MallabaevAzamat <mallabaev.an@yandex.ru>
2017-03-13 20:03:22 +0300

    update gens

--- Tests/RNGLRAbstractParser.Tests/gen.sh
+++ Tests/RNGLRAbstractParser.Tests/gen.sh
@@ -1,10 +1,10 @@
-mono ..\..\bin\Release\v40\YC.AbstractLexer.Generator.exe Lexer.fsl -o Lexer.fs
+mono ../../bin/Release/v40/YC.AbstractLexer.Generator.exe Lexer.fsl -o Lexer.fs
 
 del log.txt
 
-for %%i in (AandB, SimpleCalc, Calc, EpsilonKiller, simpleCalc_with_Nterms, simpleCalc_with_Nterms_2, simpleCalc_with_Nterms_3, simpleCalc_with_Nterms_4, PrettySimpleCalc, NotAmbigousSimpleCalc, NotAmbigousSimpleCalcWith2Ops, Stars, Stars2, Eps, List, FirstEps, CroppedBrackets, Brackets, Brackets1, StrangeBrackets, ErrorSupport) do (
+for i in AandB SimpleCalc Calc EpsilonKiller simpleCalc_with_Nterms simpleCalc_with_Nterms_2 simpleCalc_with_Nterms_3 simpleCalc_with_Nterms_4 PrettySimpleCalc NotAmbigousSimpleCalc NotAmbigousSimpleCalcWith2Ops Stars Stars2 Eps List FirstEps CroppedBrackets Brackets Brackets1 StrangeBrackets ErrorSupport
+do
     echo . >> log.txt
-    echo %%i >> log.txt
-    mono ..\..\Bin\Release\v40\YC.YaccConstructor.exe -i %%i.yrd ^
-        -g "RNGLRGenerator -pos int -token int -module RNGLR.%%i -translate false -o %%i.yrd.fs -abstract true" >> log.txt
-)
+    echo $i >> log.txt
+    mono ../../Bin/Release/v40/YC.YaccConstructor.exe -i $i.yrd -g "RNGLRGenerator -pos int -token int -module RNGLR.$i -translate false -o $i.yrd.fs -abstract true" >> log.txt
+done
--- Tests/RNGLRAbstractParser.Tests/gen_lex.sh
+++ Tests/RNGLRAbstractParser.Tests/gen_lex.sh
@@ -1,1 +1,1 @@
-mono ..\..\bin\Release\v40\YC.AbstractLexer.Generator.exe Lexer.fsl --unicode -o Lexer.fs
+mono ../../bin/Release/v40/YC.AbstractLexer.Generator.exe Lexer.fsl --unicode -o Lexer.fs

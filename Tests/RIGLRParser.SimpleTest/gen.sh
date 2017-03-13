17f2cb843eb1efef6d5aa5d27778e99578ffc739
MallabaevAzamat <mallabaev.an@yandex.ru>
2017-03-13 20:10:20 +0300

    update gens

--- Tests/RIGLRParser.SimpleTest/gen.sh
+++ Tests/RIGLRParser.SimpleTest/gen.sh
@@ -1,8 +1,8 @@
 del log.txt
 
-for %%i in (Chaos, Expr, Brackets, Grammar7) do (
+for i in Chaos Expr Brackets Grammar7
+do
     echo . >> log.txt
-    echo %%i >> log.txt
-    mono ..\..\Bin\Release\v40\YC.YaccConstructor.exe -i %%i.yrd -c ExpandEbnf -c ExpandMeta ^
-        -g "RIGLRGenerator -token int -module RIGLR.%%i" >> log.txt
-)
+    echo $i >> log.txt
+    mono ..\..\Bin\Release\v40\YC.YaccConstructor.exe -i $i.yrd -c ExpandEbnf -c ExpandMeta -g "RIGLRGenerator -token int -module RIGLR.$i" >> log.txt
+done

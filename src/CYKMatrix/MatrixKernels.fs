66360366cc6a3bc48674ba57b260ba6998893706
Rustam <rustam.azimov19021995@gmail.com>
2017-06-21 16:06:26 +0300

    Added first prototype of inner type handler.

--- src/CYKMatrix/MatrixKernels.fs
+++ src/CYKMatrix/MatrixKernels.fs
@@ -21,6 +21,14 @@
 
     type ParsingMatrix<'MatrixType> = Dictionary<NonTerminal, 'MatrixType>
 
+    type IInnerTypeHandler<'InnerType when 'InnerType : comparison> =
+        abstract innerZero : 'InnerType
+        abstract innerOne : 'InnerType
+        abstract innerSum : 'InnerType -> 'InnerType -> 'InnerType
+        abstract innerMult : 'InnerType -> 'InnerType -> 'InnerType
+
+
+    //type IMatrixHandler<'MatrixType, 'InnerType, 'InnerTypeHandler when 'InnerTypeHandler :> IInnerTypeHandler<'InnerType> and 'InnerType : comparison> = 
     type IMatrixHandler<'MatrixType, 'InnerType when 'InnerType : comparison> =
         abstract matrixSize : int
         abstract createEmptyMatrix : int -> 'MatrixType
@@ -29,7 +37,6 @@
         abstract Add : 'MatrixType -> 'MatrixType -> 'MatrixType
         abstract getNonZerosCount : 'MatrixType -> int
          
-        
 
     type MySparseMatrix(size : int, nnz : int, csrVal : float[], csrRow : int[], csrColInd : int[]) =
         let mutable _Nnz = nnz

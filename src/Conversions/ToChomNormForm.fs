70365e0ddffc0b3318d730b4927d0dabfdaf89c5
LuninaPolina <lunina_polina@mail.ru>
2017-07-05 18:23:45 +0300

    1

---
+++ src/Conversions/ToChomNormForm.fs
@@ -0,0 +1,40 @@
+﻿module Yard.Core.Conversions.ToChomNormForm
+
+open System.IO
+open Yard.Core
+open Yard.Core.IL
+open Yard.Core.IL.Production
+open System.Collections.Generic
+open Yard.Core.IL.Rule
+
+
+let deleteLongRules (rules: Rule.t<'a,'b> list) =
+    let rulesNew = ref []
+    for rule in rules do
+        let elements = match rule.body with PSeq(e, a, l) -> e 
+        if elements.Length < 3 then 
+            rulesNew := !rulesNew @ [rule]
+        else 
+            let names = [|for i in 0..elements.Length-3 -> Namer.newSource(rule.name)|]  
+            rulesNew := !rulesNew @ [TransformAux.createRule rule.name rule.args (PSeq([elements.[0]; TransformAux.createDefaultElem (PRef(names.[0], None))], None, None)) false rule.metaArgs              ] 
+            for i in 1..elements.Length-3 do
+                rulesNew := !rulesNew @ [ TransformAux.createRule names.[i-1] rule.args (PSeq([elements.[i]; TransformAux.createDefaultElem (PRef(names.[i], None))], None, None)) false rule.metaArgs] 
+            rulesNew := !rulesNew @ [TransformAux.createRule names.[elements.Length-3] rule.args (PSeq([elements.[elements.Length-2]; elements.[elements.Length-1]], None, None)) false rule.metaArgs            ] 
+    !rulesNew
+
+
+let toChomNormForm (ruleList: Rule.t<_, _> list) = 
+    ruleList
+    |> deleteLongRules
+
+type ToChomNormForm() = 
+    inherit Conversion()
+        override this.Name = "ToChomNormForm"
+        override this.ConvertGrammar (grammar,_) = mapGrammar toChomNormForm grammar
+
+
+
+                
+        
+    
+
---
+++ tests/Conversions.Tests/ToChomNormFormTests.fs
@@ -0,0 +1,41 @@
+﻿module ToChomNormFormTests
+
+open Yard.Core
+open Yard.Core.IL
+open Yard.Core.IL.Production
+open Yard.Core.IL.Definition
+open Yard.Core.Helpers
+open Conversions.TransformAux
+open NUnit.Framework
+open ConversionsTests
+open Yard.Core.Helpers
+
+let basePath = System.IO.Path.Combine(conversionTestPath, "ToChomNormForm")
+let path f = System.IO.Path.Combine(basePath, f)
+      
+let applyConversion (conversion:Conversion) loadIL = 
+    {
+        loadIL
+            with grammar = conversion.ConvertGrammar (loadIL.grammar, [||])                               
+    }
+
+(*let dummyRule : elem<Source.t,Source.t> = {omit=false; binding=None; checker=None; rule=PToken (Source.t "DUMMY")}
+let rules = 
+    (verySimpleRules "e"
+        [{dummyRule with rule = PToken (Source.t "A")}
+         {dummyRule with rule = PRef (Source.t "yard_e_2", None)}]) 
+   @(verySimpleNotStartRules "yard_e_1"
+        [{dummyRule with rule = PRef (Source.t "a", None)}
+         {dummyRule with rule = PToken (Source.t "B")}])
+   @(verySimpleNotStartRules "yard_e_2"
+        [{dummyRule with rule = PRef (Source.t "yard_e_1", None)}
+         {dummyRule with rule = PRef (Source.t "e", None)}])*)
+
+let loadIL = fe.ParseGrammar (path "C:/YaccConstructor/tests/data/Conversions/ToCNF/longrule.yrd") //"C:/YaccConstructor/tests/GraphParsing.Test/GPPerf2_cnf.yrd")//"C:/YaccConstructor/tests/data/Conversions/ToCNF/longrule_0.yrd")
+Namer.initNamer loadIL.grammar
+let result = loadIL |> applyConversion conversionChomNormForm
+printfn "%A" result 
+System.Console.ReadKey() |> ignore
+
+
+

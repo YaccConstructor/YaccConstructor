e607e9c6f7895ffc14945ec518e43a6f2d7da971
rsdpisuy <rsdpisuy@gmail.com>
2017-08-11 09:46:37 +0300

    Time logging in FSA is turned off.

--- src/Generators.Common/FSA.fs
+++ src/Generators.Common/FSA.fs
@@ -17,25 +17,25 @@ type FSA(ruleList : Rule.t<Source.t,Source.t> list) =
         |> convertRulesToFSA
             //|> printDot @".\FSA1initialFSA.dot"
             |> (fun x -> //printfn "Convertion to fsa time: %A" (System.DateTime.Now - !time)
-                        System.IO.File.WriteAllLines(@".\time.txt", [sprintf "Convertion to fsa time: %A" (System.DateTime.Now - !time)])
+                        //System.IO.File.WriteAllLines(@".\time.txt", [sprintf "Convertion to fsa time: %A" (System.DateTime.Now - !time)])
                         time := System.DateTime.Now
                         x)
         |> removeEpsilonEdges
             //|> printDot @".\FSA2withoutEpsilon.dot"
             |> (fun x -> //printfn "Epsilon edges removal time: %A" (System.DateTime.Now - !time)
-                        System.IO.File.AppendAllLines(@".\time.txt", [sprintf "Epsilon edges removal time: %A" (System.DateTime.Now - !time)])
+                        //System.IO.File.AppendAllLines(@".\time.txt", [sprintf "Epsilon edges removal time: %A" (System.DateTime.Now - !time)])
                         time := System.DateTime.Now
                         x)
         |> toDFA              
             //|> printDot @".\FSA3determ.dot"
             |> (fun x -> //printfn "To dfa convetrion time: %A" (System.DateTime.Now - !time)
-                        System.IO.File.AppendAllLines(@".\time.txt", [sprintf "To dfa convetrion time: %A" (System.DateTime.Now - !time)])
+                        //System.IO.File.AppendAllLines(@".\time.txt", [sprintf "To dfa convetrion time: %A" (System.DateTime.Now - !time)])
                         time := System.DateTime.Now
                         x)
         |> minimizeFSA
             //|> printDot @".\FSA4minimized.dot"
             |> (fun x -> //printfn "Minimization time: %A" (System.DateTime.Now - !time)
-                        System.IO.File.AppendAllLines(@".\time.txt", [sprintf "Minimization time: %A" (System.DateTime.Now - !time)])
+                        //System.IO.File.AppendAllLines(@".\time.txt", [sprintf "Minimization time: %A" (System.DateTime.Now - !time)])
                         time := System.DateTime.Now
                         x)
     //let firstSet = genFirstSet fsa

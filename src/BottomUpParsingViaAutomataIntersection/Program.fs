b4b941c4ed58beccc8d65a2d6ad2e8c32b508093
rsdpisuy <rsdpisuy@gmail.com>
2017-10-31 20:42:45 +0300

    Prototype of graph parsing algorithm based on DFA intersection.

---
+++ src/BottomUpParsingViaAutomataIntersection/Program.fs
@@ -0,0 +1,67 @@
+﻿module YC.Parsers.BottomUp
+
+open MathNet.Numerics.LinearAlgebra
+open System.Collections.Generic
+
+[<Struct>]
+type Grammar =
+    val grammar:HashSet<int> [,]
+    val startFinal: ResizeArray<int*HashSet<int>*HashSet<int>>
+    new (grm, strtFnl) = {grammar = grm; startFinal = strtFnl}
+
+let closure (atm:Matrix<_>) =
+    let zeros =  Matrix.foldSkipZeros (fun i j -> i + 1) 0 atm |> ref
+    let mutable res = atm
+    let _go = ref true
+    while !_go do
+        res <- res.Add (res.Multiply res)
+        let newZeros = Matrix.foldSkipZeros (fun i j -> i + 1) 0 res
+        _go := not (!zeros = newZeros )
+        zeros := newZeros
+    res
+
+let main (input:HashSet<int> [,]) (grammar:Grammar) =
+    let inputSize = Array2D.length1 input
+    let grammarSize = Array2D.length1 grammar.grammar    
+    let mutable intersectionResult = 
+        SparseMatrix.create (inputSize * grammarSize) (inputSize * grammarSize) 0.0
+    let stateRemap =
+        let m = new ResizeArray<_>()
+        for i in 0 .. inputSize-1 do
+            for j in 0 .. grammarSize-1 do
+                m.Add((i,j))
+        m
+
+    let getEdgesCount a = 
+        let mutable i = 0
+        Array2D.iter (fun (t:HashSet<_>) -> i <- i + t.Count) a
+        i
+
+    let mutable inputEdges = getEdgesCount input
+    let mutable _go = true
+
+    while _go do
+        intersectionResult 
+        |> Matrix.iteri (fun i j n -> 
+            let _startInput,_startGrammar = stateRemap.[i] 
+            let _endInput,_endGrammar = stateRemap.[j]
+            if input.[_startInput, _endInput].Overlaps grammar.grammar.[_startGrammar, _endGrammar]
+               || (_startInput = _endInput && _startGrammar = _endGrammar)
+            then intersectionResult.[i,j] <- 1.0
+            )
+        let cls = closure intersectionResult
+        cls
+        |> Matrix.iteriSkipZeros (fun i j n ->
+            let _startInput,_startGrammar = stateRemap.[i] 
+            let _endInput,_endGrammar = stateRemap.[j]
+            let startFinal = grammar.startFinal |> Seq.tryFind (fun (_,s,_) -> s.Contains _startGrammar)
+            //printfn "%A %A %A" _startGrammar _endGrammar startFinal
+            match startFinal with
+            | Some (n,s,f) when f.Contains _endGrammar -> input.[_startInput,_endInput].Add n |> ignore
+            | _ -> ()
+            )
+        let newEdgesCount = getEdgesCount input
+        _go <- not (newEdgesCount = inputEdges)
+        inputEdges <- newEdgesCount
+
+    input
---
+++ tests/BottomUpParsingViaAutomataIntersection/Program.fs
@@ -0,0 +1,89 @@
+﻿module BottomUpParsingViaAutomataIntersection.Test
+
+open NUnit.Framework
+open YC.Parsers.BottomUp
+open System.Collections.Generic
+
+[<TestFixture>]
+type ``Tests for bottom-Up parser based on automata intersection``() =
+    
+    // S -> a S b | eps
+    // 1 a
+    // 2 b
+    // 3 S
+    let grammar1 = 
+        let g = Array2D.init 4 4 (fun i j -> new HashSet<_>())
+        g.[0,1].Add 1 |> ignore
+        g.[1,2].Add 3 |> ignore
+        g.[2,3].Add 2 |> ignore
+        let states = [|(3, new HashSet<_>([0]), new HashSet<_>([0;3]))|]
+        new Grammar (g, new ResizeArray<_>(states))
+
+    // E -> E (+|*E) | n
+    // 1 +
+    // 2 *
+    // 3 E
+    // 4 n
+    let grammar2 = 
+        let g = Array2D.init 4 4 (fun i j -> new HashSet<_>())
+        g.[0,1].Add 3 |> ignore
+        g.[1,2].Add 1 |> ignore
+        g.[1,2].Add 2 |> ignore
+        g.[2,3].Add 3 |> ignore
+        g.[0,3].Add 4 |> ignore
+        let states = [|(3, new HashSet<_>([0]), new HashSet<_>([3]))|]
+        new Grammar (g, new ResizeArray<_>(states))    
+
+    [<Test>]
+    member this._01_BracketsLinear() = 
+        let input = Array2D.init 5 5 (fun i j -> new HashSet<_>())
+        input.[0,1].Add 1 |> ignore
+        input.[1,2].Add 1 |> ignore
+        input.[2,3].Add 2 |> ignore
+        input.[3,4].Add 2 |> ignore
+        let output = main input grammar1
+        printfn "_______________"
+        printfn "%A" output
+        Assert.IsTrue(output.[0,0].Contains 3)
+        Assert.IsTrue(output.[1,1].Contains 3)
+        Assert.IsTrue(output.[2,2].Contains 3)
+        Assert.IsTrue(output.[3,3].Contains 3)
+        Assert.IsTrue(output.[4,4].Contains 3)
+        Assert.IsTrue(output.[0,4].Contains 3)
+
+    [<Test>]
+    member this._02_BracketsCycle() = 
+        let input = Array2D.init 2 2 (fun i j -> new HashSet<_>())
+        input.[0,0].Add 1 |> ignore
+        input.[0,1].Add 2 |> ignore
+        input.[1,0].Add 2 |> ignore 
+        let output = main input grammar1
+        printfn "_______________"
+        printfn "%A" output
+        Assert.IsTrue(output.[0,0].Contains 3)
+        Assert.IsTrue(output.[1,1].Contains 3)
+        Assert.IsTrue(output.[0,1].Contains 3)
+        Assert.IsFalse(output.[1,0].Contains 3)
+
+    [<Test>]
+    member this._03_ArithGraph() = 
+        let input = Array2D.init 4 4 (fun i j -> new HashSet<_>())
+        input.[0,1].Add 4 |> ignore
+        input.[1,2].Add 1 |> ignore
+        input.[2,3].Add 4 |> ignore
+        input.[3,0].Add 2 |> ignore 
+        let output = main input grammar2
+        printfn "_______________"
+        printfn "%A" output
+        Assert.IsTrue(output.[0,1].Contains 3)
+        Assert.IsTrue(output.[2,3].Contains 3)
+        Assert.IsTrue(output.[0,3].Contains 3)
+        Assert.IsTrue(output.[2,1].Contains 3)
+        let startNonTermCount = 
+            let i = ref 0
+            output |> Array2D.iter (fun s -> if s.Contains 3 then incr i)
+            !i
+        Assert.AreEqual(4, startNonTermCount)
+
+        
+            

9321e1f066d397256992eb6b4b59b34cb6cdb3be
Misha Kosticyn <mishakosticyn@yandex.ru>
2017-11-25 02:49:36 +0300

    Added function, that gets strongly connected components of graph

--- src/Conversions/RegularApproximation.fs
+++ src/Conversions/RegularApproximation.fs
@@ -9,6 +9,8 @@ let private grammarToGraph (grammar : Grammar<_,_>) =
     let handleOneRule (rule : Rule<_,_>) =
         List.map (fun nonTerm -> SEdge<Source>(rule.name, nonTerm)) <| getAllNonTermOfProd rule.body
 
-    let rules = List.collect (fun module' -> module'.rules) grammar
-    let edges = List.collect handleOneRule rules
+    let edges = List.collect handleOneRule grammar.Head.rules
     edges.ToAdjacencyGraph<Source, SEdge<Source>>()
+
+let inline private getStronglyConnectedComps graph =
+    QuickGraph.Algorithms.ConnectedComponents.StronglyConnectedComponentsAlgorithm(graph).Graphs

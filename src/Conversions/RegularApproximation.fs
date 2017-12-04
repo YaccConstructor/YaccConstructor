afeeb5fd9153ca187c0ad3959f9f03c9821995a4
Misha Kosticyn <mishakosticyn@yandex.ru>
2017-12-05 00:25:15 +0300

    Some bugs were fixed, refactoring - fixed bug - dummy PSeq - fixed bug - wrong start rule

--- src/Conversions/RegularApproximation.fs
+++ src/Conversions/RegularApproximation.fs
@@ -23,37 +23,40 @@ let private getStronglyConnectedComps graph =
     |> List.map (snd >> List.map (fun elem -> elem.Key))
 
 let private approximate (grammar : Grammar<_,_>) =
-    let epsilon = PSeq(List.empty, None, None)
-    let inline newNonTerm nonTerm = new Source(nonTerm + "'")
     let handleSets sets =
         let handleOneSet changed unchanged nonterminals =
+            let inline newNonTerm nonTerm = new Source(nonTerm + "'")
             let epsilonRules =
                 let newNonTerms = List.map newNonTerm nonterminals
+                let epsilon = PSeq(List.empty, None, None)
                 List.map (fun head -> defaultRule head epsilon) newNonTerms
 
             let wrapSeq head elemList =
+                let addToRules isEmpty notEmpty head stock rules =
+                    let rule = if List.isEmpty stock then isEmpty else PSeq(List.append stock [notEmpty], None, None)
+                    List.Cons (defaultRule head rule, rules)
                 let newHead, stock, lst =
                     List.fold (fun (head, stock, rules) elem ->
                         match elem.rule with
-                        | PRef(nonTerm, _) when List.contains nonTerm.text nonterminals ->
-                            newNonTerm nonTerm.text, List.empty, List.Cons (defaultRule head <| PSeq(List.append stock [elem], None, None), rules)
+                        | PRef(nonTerm, _) as ref when List.contains nonTerm.text nonterminals ->
+                            newNonTerm nonTerm.text, List.empty, addToRules ref elem head stock rules
                         | PToken _
                         | PRef _ -> head, List.append stock [elem], rules
                         | _ -> failwith "wrong grammar!")
                         (head, List.empty, List.empty) elemList
                 let head' = PRef(newNonTerm head.text, None)
-                let stock = if stock.IsEmpty then head' else PSeq(List.append stock [createDefaultElem <| head'], None, None)
-                List.Cons(defaultRule newHead stock, lst)
+                addToRules head' (createDefaultElem head') newHead stock lst
 
-            let rec transformOneRule isStart head prod =
-                let x :: xs =
-                    match prod with
-                    | PToken _
-                    | PRef _ as prod -> wrapSeq head [createDefaultElem prod]
-                    | PSeq(elemList, _, _) -> wrapSeq head elemList
-                    | PAlt(prod1, prod2) -> List.append (transformOneRule isStart head prod1) (transformOneRule isStart head prod2)
-                    | _ -> failwith "wrong grammar!"
-                { x with isStart = isStart } :: xs
+            let rec transformOneRule isStart head =
+                let makeStartRule rules =
+                    let x :: xs = List.rev rules
+                    { x with isStart = isStart } :: xs
+                function
+                | PToken _
+                | PRef _ as prod -> makeStartRule <| wrapSeq head [createDefaultElem prod]
+                | PSeq(elemList, _, _) -> makeStartRule <| wrapSeq head elemList
+                | PAlt(prod1, prod2) -> List.append (transformOneRule isStart head prod1) (transformOneRule isStart head prod2)
+                | _ -> failwith "wrong grammar!"
 
             let handleRulesOfNonterm = List.collect (fun rule -> transformOneRule rule.isStart rule.name rule.body)
 

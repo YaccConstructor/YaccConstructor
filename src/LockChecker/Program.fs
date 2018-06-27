023b199264241a6bdc9d8b976db81be57822f7b0
Artem Gorokhov <gorohov.art@gmail.com>
2018-06-27 16:06:59 +0300

    Path extraction

--- src/LockChecker/Program.fs
+++ src/LockChecker/Program.fs
@@ -5,6 +5,7 @@ open AbstractAnalysis.Common
 open Yard.Frontends.YardFrontend
 open YC.API
 open AbstractParser
+open System.Collections.Generic
 
 let loadGraph graphFile tokenizer =
     let data = System.IO.File.ReadAllLines(graphFile)
@@ -44,12 +45,56 @@ let loadGrammar grammarFile =
     let s0Head = "\ns0: {} | ca s0 | ca \n"
     grm + "    |" + sCalls + "\n    |" + sLocks + "\n" + s0Head + "    |" + s0Calls + "\n    |" + s0Locks + "\n"
 
+let singlePathToBadAssertForRoot (root: INode) (badAssertInt : int) (intToString : Dictionary<_,_>) : seq<string> =
+    let results = new Dictionary<INode, _>() 
+    let rec getPath : INode -> seq<string> = function
+        | :? IntermidiateNode as i ->
+            let isGot,value = results.TryGetValue i
+            if isGot
+            then 
+                Seq.empty
+            else
+                results.Add(i, null)
+                getPath i.First
+        | :? TerminalNode as t ->
+            let res = new List<_>()
+            if t.Name <> -1<token> 
+            then 
+                seq{yield intToString.[int t.Name]}
+            else
+                Seq.empty
+        | :? PackedNode as p ->
+            let rightPath = getPath p.Right
+            let leftPath = getPath p.Left
+            Seq.append leftPath rightPath
+        | :? NonTerminalNode as n ->
+            let isGot,value = results.TryGetValue n
+            if isGot
+            then 
+                Seq.empty
+            else
+                results.Add(n, null)
+                if (int n.Name = badAssertInt)
+                then
+                    seq{yield intToString.[int n.Name]}
+                else
+                    getPath n.First
+        | :? EpsilonNode as eps ->
+            Seq.empty
+        | _ -> failwith "Unexpected node type. rly?"
+
+    getPath root
+
 [<EntryPoint>]
 let main argv =
-    let grpahFile = argv.[0] 
+    //let graph = ".\\..\\..\\graph"
+    //let grammarFile = ".\\..\\..\\grammar"
+    let graph = argv.[0]
     let grammarFile = argv.[1]
     
     let grammar = loadGrammar grammarFile    
+    
+    System.IO.File.WriteAllText("resultGrammar.yrd", grammar)
 
     let parserSource =
         let fe = new YardFrontend()
@@ -59,10 +104,21 @@ let main argv =
     let tokenizer str =
         str |> parserSource.StringToToken |> int
 
-    let inputGraph = loadGraph grpahFile tokenizer
+    let inputGraph = loadGraph graph tokenizer
 
-    let ranges = getAllRangesForStartState parserSource inputGraph
+    let treesForEachInitialInputPosition = getAllSPPFRootsAsINodes parserSource inputGraph
+    let badAssertInt = 
+        let k = ref 0
+        for kvp in parserSource.IntToString do
+            if kvp.Value = "ba"
+            then k := kvp.Key
+        !k
 
+    let result = 
+        singlePathToBadAssertForRoot treesForEachInitialInputPosition.[0] badAssertInt parserSource.IntToString
+        |> Array.ofSeq
+    
     let outputFile = argv.[2]
+    System.IO.File.WriteAllLines(outputFile, result)    
     
     0 // return an integer exit code

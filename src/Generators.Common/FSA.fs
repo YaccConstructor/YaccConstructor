50a5b5455d2b48f20f9fa9b94868f1738dcde147
Artem Gorokhov <gorohov.art@gmail.com>
2018-07-14 11:48:49 +0300

    removed paket

--- src/Generators.Common/FSA.fs
+++ src/Generators.Common/FSA.fs
@@ -10,36 +10,7 @@ open Yard.Generators.Common.Epsilon
 open Yard.Generators.Common.SymbolSets
 open Yard.Generators.Common
 
-type FSA(fsa: InternalFSA) =
-    new(ruleList: Rule<Source,Source> list) =
-        let time = ref System.DateTime.Now
-        let rfsa =
-            ruleList
-            |> convertRulesToFSA
-                //|> printDot @".\FSA1initialFSA.dot"
-                |> (fun x -> //printfn "Convertion to fsa time: %A" (System.DateTime.Now - !time)
-                            //System.IO.File.WriteAllLines(@".\time.txt", [sprintf "Convertion to fsa time: %A" (System.DateTime.Now - !time)])
-                            time := System.DateTime.Now
-                            x)
-            |> removeEpsilonEdges
-                //|> printDot @".\FSA2withoutEpsilon.dot"
-                |> (fun x -> //printfn "Epsilon edges removal time: %A" (System.DateTime.Now - !time)
-                            //System.IO.File.AppendAllLines(@".\time.txt", [sprintf "Epsilon edges removal time: %A" (System.DateTime.Now - !time)])
-                            time := System.DateTime.Now
-                            x)
-            |> toDFA              
-                //|> printDot @".\FSA3determ.dot"
-                |> (fun x -> //printfn "To dfa convetrion time: %A" (System.DateTime.Now - !time)
-                            //System.IO.File.AppendAllLines(@".\time.txt", [sprintf "To dfa convetrion time: %A" (System.DateTime.Now - !time)])
-                            time := System.DateTime.Now
-                            x)
-            |> minimizeFSA
-                //|> printDot @".\FSA4minimized.dot"
-                |> (fun x -> //printfn "Minimization time: %A" (System.DateTime.Now - !time)
-                            //System.IO.File.AppendAllLines(@".\time.txt", [sprintf "Minimization time: %A" (System.DateTime.Now - !time)])
-                            time := System.DateTime.Now
-                            x)
-        new FSA(rfsa)
+type FSA(fsa: InternalFSA) =       
 
     member this.States = fsa.States
     //after convertion to dfa each startstates HashSet contains only one state

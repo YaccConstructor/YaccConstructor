15961a0adb03d158f371e7c51c3f26d7d46562b2
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-07-16 22:08:48 +0300

    Resolve cyclic dependency around DotParser
    YaccConstructor depends on YC.QuickGraph which depends on DotParser
    which depends on YaccConstructor (?!!)
    YC.QuickGraph has been repacked without DotParser referencing.

--- src/YardFrontend/gen.cmd
+++ src/YardFrontend/gen.cmd
@@ -1,2 +1,4 @@
 ..\..\tools\fslexyacc\fslex --unicode Lexer.fsl
+cd ../YC.FsYacc
+dotnet run "../YardFrontend/parser.fsy" "-o ../YardFrontend/Parser.fs -module Yard.Frontends.YardFrontend.GrammarParser -pos SourcePosition -token Source
 echo > log.txt

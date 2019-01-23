f2b7dd6927ca1e7726654982ce049a293ea84c70
gsv <rsdpisuy@gmail.com>
2019-01-23 16:44:36 +0300

    Move from directories back...

--- build.cmd
+++ build.cmd
@@ -4,6 +4,7 @@ cls
 nuget restore YaccConstructor.sln
 
 cd packages/FSharp.Compiler.Tools.10.0.2/tools/ 
-fsi.exe "/src/YardFrontend/gen.fsx"
+fsi.exe "../../../src/YardFrontend/gen.fsx"
 
+cd ../../../
 msbuild YaccConstructor.sln

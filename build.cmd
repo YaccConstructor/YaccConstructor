f6e34809b672dcbc78fc3ee83f270e9fe4d1ba23
gsv <rsdpisuy@gmail.com>
2019-01-23 16:39:14 +0300

    install dotnet for ubuntu. Try to run fsi on Win

--- build.cmd
+++ build.cmd
@@ -3,6 +3,7 @@ cls
 
 nuget restore YaccConstructor.sln
 
-START "packages/FSharp.Compiler.Tools.10.0.2/tools/fsi.exe" "/src/YardFrontend/gen.fsx"
+cd "/packages/FSharp.Compiler.Tools.10.0.2/tools/" 
+fsi.exe "/src/YardFrontend/gen.fsx"
 
 msbuild YaccConstructor.sln

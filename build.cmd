2a7ee8cc37d71126bce6724e0bc216e10d49daa8
gsv <rsdpisuy@gmail.com>
2019-01-23 16:21:37 +0300

    Start fsi in Win

--- build.cmd
+++ build.cmd
@@ -3,6 +3,6 @@ cls
 
 nuget restore YaccConstructor.sln
 
-packages/FSharp.Compiler.Tools.10.0.2/tools/fsi.exe src/YardFrontend/gen.fsx
+START "packages/FSharp.Compiler.Tools.10.0.2/tools/fsi.exe" /src/YardFrontend/gen.fsx
 
 msbuild YaccConstructor.sln

dd6d8f163baddad12697113c7886f7965b332ef2
gsv <rsdpisuy@gmail.com>
2019-01-23 17:14:10 +0300

    Yet another version of build scripts

--- build.cmd
+++ build.cmd
@@ -3,6 +3,6 @@ cls
 
 nuget restore YaccConstructor.sln
 
-start \w "/packages/FSharp.Compiler.Tools.10.0.2/tools/fsi.exe " "/src/YardFrontend/gen.fsx"
+start \w \d "/packages/FSharp.Compiler.Tools.10.0.2/tools/fsi.exe " "/src/YardFrontend/gen.fsx"
 
 msbuild YaccConstructor.sln

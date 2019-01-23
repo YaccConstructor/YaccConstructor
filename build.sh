0995e3835b92d691a85c22fd3fa4f48a8423413f
gsv <rsdpisuy@gmail.com>
2019-01-23 17:26:02 +0300

    Yet another version of build scripts

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@
 set -eu
 set -o pipefail
 
-dotnet restore YaccConstructor.sln
-
 mono ./packages/FSharp.Compiler.Tools.10.0.2/tools/fsi.exe ./src/YardFrontend/gen.fsx
 
 msbuild YaccConstructor.sln

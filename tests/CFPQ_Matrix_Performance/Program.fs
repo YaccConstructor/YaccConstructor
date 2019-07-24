3b18b5b6baf7cdbf9d8a0f050b390aa19bc0c032
rustam-azimov <rustam.azimov19021995@gmail.com>
2019-07-24 17:33:49 +0300

    CFPQ now allows to use epsilon rules.

--- tests/CFPQ_Matrix_Performance/Program.fs
+++ tests/CFPQ_Matrix_Performance/Program.fs
@@ -1,22 +1,11 @@
 ﻿module YC.GraphParsing.Tests.FormatPerformance
 
+open System.Collections.Generic
 open Util
 open YC.Parsing.Common.GraphInput
 open ImplementationTests
 open MatrixKernels
 open MySparseGraphParsingImpl
-
-let tokenizer str =
-    match str with
-    | "A" -> 1<token>
-    | "B" -> 2<token>
-    | "AR" -> 3<token>
-    | "SCOR" -> 4<token>
-    | "TR" -> 5<token>
-    | "OTHER" -> 6<token>
-    | "SCO" -> 7<token>
-    | "T" -> 8<token>
-    | _ -> -1<token>
 
 
 let parseGPU (graph:YC.Parsing.Common.GraphInput.SimpleInputGraph<int<token>>)
@@ -43,16 +32,21 @@ let testGPU cnt graph allRules nonterminals S tokenizer =
         let countOfPairs = mySparseAnalyzer root.[0] !S1
         root.[0], time, countOfPairs
 
-let processFile file grammarFile cnt=
+let processFile file grammarFile cnt =
     let edges = new ResizeArray<int*int<token>*int>()
+    let tokenToInt = new Dictionary<string, int>()
     let lines = System.IO.File.ReadLines(file)
 
     for line in lines do
         let cur = line.Split [|' '|]
         let f = cur.[0] |> int
-        let l = tokenizer cur.[1]
+        let l = cur.[1]
+        if not <| tokenToInt.ContainsKey l
+        then
+            tokenToInt.Add(l, tokenToInt.Count+1)
+        
         let t = cur.[2] |> int
-        edges.Add(f,l,t)
+        edges.Add(f,tokenToInt.[l]*1<token>,t)
 
     let edgs = edges.ToArray()
     let allVs = edgs |> Array.collect (fun (f,l,t) -> [|f * 1<positionInInput>; t * 1<positionInInput>|]) |> Set.ofArray |> Array.ofSeq
@@ -68,13 +62,20 @@ let processFile file grammarFile cnt=
     let nonterminals = new ResizeArray<NonTerminal>()
     let crl = new ResizeArray<(NonTerminal*Probability.T)*((NonTerminal*NonTerminal*bool)[])>()
     let srl = new ResizeArray<(NonTerminal*Probability.T)*int<token>>()
-    let erl: NonTerminal [] = [||]
+    let erl = new ResizeArray<NonTerminal>()
     let grammarlines = System.IO.File.ReadLines(grammarFile)
     let S = ref (NonTerminal "")
     for line in grammarlines do
-        let cur = line.Split [|' '|]
-        if cur.Length = 2 then
-            srl.Add((NonTerminal cur.[0], Probability.create 1.0), tokenizer cur.[1])
+        let cur = line.Trim().Split [|' '|]
+        if cur.Length = 1 then
+            erl.Add(NonTerminal cur.[0])
+            if not <| nonterminals.Contains(NonTerminal cur.[0]) then
+                nonterminals.Add(NonTerminal cur.[0])
+        elif cur.Length = 2 then
+            if not <| tokenToInt.ContainsKey cur.[1]
+            then
+                tokenToInt.Add(cur.[1], tokenToInt.Count+1)
+            srl.Add((NonTerminal cur.[0], Probability.create 1.0), tokenToInt.[cur.[1]]*1<token>)
             if not <| nonterminals.Contains(NonTerminal cur.[0]) then
                 nonterminals.Add(NonTerminal cur.[0])
         else
@@ -88,8 +89,13 @@ let processFile file grammarFile cnt=
     
     S := nonterminals.[0]
 
-    let allRules = new BooleanRulesHolder(crl.ToArray(), srl.ToArray(), erl)
-
+    let allRules = new BooleanRulesHolder(crl.ToArray(), srl.ToArray(), erl.ToArray())
+    let tokenizer str =
+        if tokenToInt.ContainsKey(str)
+        then
+            tokenToInt.[str] * 1<token>
+        else
+            -1<token>
     let root, time, countOfPairs = testGPU cnt graph allRules nonterminals S tokenizer
 
     System.IO.Path.GetFileNameWithoutExtension file, time, countOfPairs
